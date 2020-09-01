cb5cab36d8b15d978d25e28fb77383259b3eec20
SGrade <sgrade@users.noreply.github.com>
2020-09-01 16:36:26 +0200

    codeforces

---
+++ codeforces/1003A.cpp
@@ -0,0 +1,35 @@
+// A. Polycarp's Pockets
+
+#include <stdio.h>
+#include <algorithm>
+#include <set>
+
+
+int main(){
+
+    int n;
+    scanf("%d\n", &n);
+
+    int a[n];
+    for (int i=0; i<n; ++i){
+        scanf("%d", &(a[i]));
+    }
+    
+    int minPockets;
+    if (n==1){
+        minPockets = 1;
+    }
+    else {
+        std::sort(a, a + n);
+        std::set<int> st(a, a+n);
+
+        for (auto e: st){
+            int count = std::count(a, a+n, e);
+            if (count > minPockets) minPockets = count;
+        }
+    }
+
+    printf("%d\n", minPockets);
+
+    return 0;
+}
