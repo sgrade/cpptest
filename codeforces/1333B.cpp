282fedb557d2fd012f052f2525b8ab069e2d6194
Roman K <sgrade@users.noreply.github.com>
2020-10-03 21:20:00 +0200

    codeforces

---
+++ codeforces/1333B.cpp
@@ -0,0 +1,64 @@
+// B. Kind Anton
+
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+
+// https://stackoverflow.com/questions/1903954/is-there-a-standard-sign-function-signum-sgn-in-c-c
+int signOf (int x){
+    return (x > 0) - (x < 0);
+}
+
+int main(){
+
+    int t;
+    cin >> t;
+
+    while(t--){
+
+        int n;
+        cin >> n;
+
+        int a[n], b[n];
+        for (int i=0; i<n; ++i) cin >> a[i];
+        for (int i=0; i<n; ++i) cin >> b[i];
+
+        bool ans = true;
+
+        // first element
+        if (a[0] != b[0]) ans = false;
+
+        // second and following elements
+        map<int, bool> mp = {
+            {-1, false},
+            {0, true},
+            {1, false}
+        };
+
+        for (int i=1; i<n && ans; ++i){
+
+            // Check if there is no need to continue search 
+            // (-1, 0, 1 are found - everything can be constructed from them)
+            if (mp[-1] && mp[1]) break;
+
+            for (int j=0; j<i; ++j){
+
+                mp[a[j]] = true;
+
+                if (b[i] < a[i] && mp[-1]) break;
+                else if (b[i] > a[i] && mp[1]) break;
+                else if (b[i] == a[i]) break;
+                else if (j == i-1){
+                    ans = false;
+                }
+            }
+        }
+
+        cout << (ans ? "YES" : "NO") << endl;
+
+    }
+
+    return 0;
+}
