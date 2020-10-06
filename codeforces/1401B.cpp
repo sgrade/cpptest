aba06cb973c58cd169f2f9d881071b1a01e6ac42
Roman K <sgrade@users.noreply.github.com>
2020-10-06 21:11:11 +0200

    codeforces

---
+++ codeforces/1401B.cpp
@@ -0,0 +1,2 @@
+// B. Ternary Sequence
+
--- codeforces/651A.cpp
+++ codeforces/651A.cpp
@@ -13,11 +13,16 @@ int main(){
     if (a1 > a2) std::swap(a1, a2);
 
     int ans = 0;
-    while ((a1 >= 1 && a2 >= 1) && (a1 >= 2 || a2 >=2) ) {
+
+    // Hence, if at the beginning of minute some joystick is charged by 1 percent, it has to be connected to a charger, otherwise the game stops.
+    // (a2 >= 2)
+    // If some joystick completely discharges (its charge turns to 0), the game also stops.
+    // (a1 > 0  && a2 > 0)
+    while ((a1 > 0  && a2 > 0) && (a2 >= 2)) {
         a1 += 1;
         a2 -= 2;
         ++ans;
-        if (a2 <= 1) std::swap(a1, a2);
+        if (a2 <= 2) std::swap(a1, a2);
     }
 
     printf("%d\n", ans);
