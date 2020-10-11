f109519cc8a355219e5c67c5bb76323d07138086
Roman K <sgrade@users.noreply.github.com>
2020-10-11 18:49:08 +0200

    codeforces

--- codeforces/1108B.cpp
+++ codeforces/1108B.cpp
@@ -21,6 +21,7 @@ int main(){
 
     int x = 0;
     int y = all[0];
+
     for (int i=1; i<n; ++i){
         if (y % all[i] != 0){
             x = all[i];
@@ -28,9 +29,17 @@ int main(){
         }
     }
     if (x == 0){
-        x = all[1];
+        if (all[0] == all[1]){
+            x = all[1];
+        }
+        else if (all[n-1] == 1 && all[n-2] == 1){
+            x = all[0];
+        }
+        else {
+            x = 1;
+        }
     }
-
+    
     cout << y << ' ' << x << endl;
 
     return 0;
