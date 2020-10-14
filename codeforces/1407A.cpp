a236f0ffbd42c82ea355a0588966b860581a4031
Roman K <sgrade@users.noreply.github.com>
2020-10-14 15:39:28 +0200

    codeforces

--- codeforces/1407A.cpp
+++ codeforces/1407A.cpp
@@ -17,38 +17,30 @@ int main(){
         cin >> n;
 
         vector<int> v(n);
-        int odd=0, even=0;
+        
+        // Editorial - https://codeforces.com/blog/entry/82417
+        int zeros = 0, ones = 0;
         for(int i=0; i<n; ++i){
             cin >> v[i];
-            if(v[i]){
-                if(i%2 == 0) ++even;
-                else ++ odd;
-            }
+            if(v[i]) ++ones;
+            else ++ zeros;
         }
         
-        int diff = abs(odd-even);
-        while (diff > 0 ){
-            bool removeOdd = odd > even ? true : false;
-            for(int i=0; i<n; ++i){
-                if (removeOdd && i%2 != 0) {
-                    if (v[i] == 1) {
-                        v.erase(v.begin()+i);
-                        break;
-                    }
-                }
-                else if (!removeOdd && i%2 == 0){
-                    if (v[i] == 1) {
-                        v.erase(v.begin()+i);
-                        break;
-                    }
-                }
+        if (zeros >= n/2){
+            // we remove all ones
+            cout << zeros << endl;
+            for (int i=0; i<zeros; ++i) {
+                cout << 0 << ' ';
             }
-            --diff;
+        }
+        else {
+            // we remove all zeros
+            cout << ones - ones%2 << endl;
+            for (int i=0; i<ones-ones%2; ++i){
+                cout << 1 << ' ';
+            }
         }
 
-        for(int i=0; i<n; ++i){
-            cout << v[i] << ' ';
-        }
         cout << endl;
     
     }
