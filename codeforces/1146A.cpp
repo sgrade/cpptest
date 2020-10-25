639856cb39457c694613a0fa9e6c6b68a4a42d39
Roman K <sgrade@users.noreply.github.com>
2020-10-25 16:27:31 +0100

    codeforces

---
+++ codeforces/1146A.cpp
@@ -0,0 +1,30 @@
+// A. Love "A"
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+
+int main(){
+
+    string s;
+    getline(cin, s);
+
+    int ans = 1;
+    int numOfA = count(s.begin(), s.end(), 'a');
+
+    int sHalf = s.size()/2;
+
+    if (numOfA > sHalf){
+        ans = s.size();
+    }
+    else{
+        ans = numOfA*2 -1;
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
