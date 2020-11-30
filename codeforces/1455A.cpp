9d420ace0bb946ade8312efd0217d7779b8db8dd
Roman K <sgrade@users.noreply.github.com>
2020-11-30 19:36:03 +0100

    Educational Codeforces Round 99 (Rated for Div. 2)

---
+++ codeforces/1455A.cpp
@@ -0,0 +1,25 @@
+// A. Strange Functions
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+
+int main(){
+
+    int t;
+    cin >> t;
+
+    while(t--){
+
+        string s;
+        getline(cin >> ws, s);
+
+        cout << s.size() << endl;
+
+    }
+                                    
+    return 0;
+}
