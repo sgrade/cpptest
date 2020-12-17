81976d4361e66f3ef08b41db484f3f0d473747a0
Roman K <sgrade@users.noreply.github.com>
2020-12-17 23:28:55 +0100

    codeforces

--- codeforces/115A.cpp
+++ codeforces/115A.cpp
@@ -1,25 +1,34 @@
 // A. Party
 
 #include <iostream>
-#include <set>
+#include <algorithm>
 
 using namespace std;
 
 
 int main(){
 
-    int t;
-    cin >> t;
+    int n;
+    cin >> n;
 
-    set<int> st;
+    // Editorial - https://codeforces.com/blog/entry/2584
+    // Solution based on https://codeforces.com/contest/115/submission/57934041
     
-    int tmp;
-    while(t--){
-        cin >> tmp;
-        st.insert(tmp);
+    int p[n];
+    for (auto &el: p) cin >> el;
+
+    int ans = 0;
+    for (int i = 0; i < n; ++i){
+        int current = 0;
+        int j = i;
+        while (j >= 0){
+            j = p[j] - 1;
+            current += 1;
+        }
+        ans = max(ans, current);
     }
 
-    cout << st.size() << endl;
+    cout << ans << endl;
 
     return 0;
 }
