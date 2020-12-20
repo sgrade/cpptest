228cd0fda829d69d2bc2c496aac2b8055d54d208
Roman K <sgrade@users.noreply.github.com>
2020-12-20 20:09:42 +0100

    codeforces

--- codeforces/1459A.cpp
+++ codeforces/1459A.cpp
@@ -1,5 +1,4 @@
 // A. Red-Blue Shuffle
-// NOT FINISHED
 
 #include <iostream>
 #include <algorithm>
@@ -32,30 +31,17 @@ int main(){
             b[i] = (int)s[i] - 48;
         }
 
-        sort(r, r+n, greater<int>());
-        sort(b, b+n, greater<int>());
-
         int rans = 0;
         int bans = 0;
-        for (int ri = 0; ri < n; ++ri){
-            for (int bi = 0; bi < n; ++bi){
-                if (r[ri] > b[bi]){
-                    ++rans;
-                }
-                else if (r[ri] > b[bi]){
-                    ++bans;
-                }
-            }
+        for (int i = 0; i < n; ++i){
+            if (r[i] > b[i]) ++rans;
+            else if (r[i] < b[i]) ++bans;
         }
 
         if (rans > bans) cout << "RED";
         else if (rans < bans) cout << "BLUE";
         else cout << "EQUAL";
         cout << endl;
-
-        int ans = 0;
-
-
     }
 
     return 0;
