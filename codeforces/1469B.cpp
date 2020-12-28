e98e6bf49db4d5843e4c91be576387848f71a1ba
Roman K <sgrade@users.noreply.github.com>
2020-12-28 17:25:32 +0100

    Educational Codeforces Round 101 (Rated for Div. 2

---
+++ codeforces/1469B.cpp
@@ -0,0 +1,47 @@
+// 
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <numeric>
+
+using namespace std;
+
+
+int main(){
+
+    int t;
+    cin >> t;
+
+    while(t--){
+
+        int n;
+        cin >> n;
+        vector<int> r(n);
+        for (auto &el: r) cin >> el;
+
+        int m;
+        cin >> m;
+        vector<int> b(m);
+        for (auto &el: b) cin >> el;
+       
+        int rMax = 0;
+        int rSum = 0;
+        for (int i = 0; i < n; ++i){
+            rSum += r[i];
+            rMax = max(rMax, rSum);
+        }
+
+        int bMax = 0;
+        int bSum = 0;
+        for (int i = 0; i < m; ++i){
+            bSum += b[i];
+            bMax = max(bMax, bSum);
+        }
+
+        cout << rMax + bMax << endl;
+
+    }
+
+    return 0;
+}
