98875d171f7738a51bc11d229dbc20a387321d65
Roman K <sgrade@users.noreply.github.com>
2021-01-01 13:13:19 +0100

    codeforces

---
+++ codeforces/964A.cpp
@@ -0,0 +1,15 @@
+// A. Splits
+
+#include <iostream>
+
+
+int main(){
+
+    int n;
+    std::cin >> n;
+
+    // Editorial - https://codeforces.com/blog/entry/58991
+    std::cout << n / 2 + 1 << std::endl;
+
+    return 0;
+}
