08a0c1fd6dfc32f1718ba895c90b64ae9a4fd762
Roman K <sgrade@users.noreply.github.com>
2021-02-10 20:45:27 +0100

    codeforces

--- codeforces/1407B.cpp
+++ codeforces/1407B.cpp
@@ -1,5 +1,4 @@
 // B. Big Vova
-// NOT FINISHED: wrong answer Participant's answer is worse (test case 51)
 
 #include <iostream>
 #include <algorithm>
@@ -27,31 +26,28 @@ int main(){
 
         vector<int> a(n);
         for (auto &el: a) cin >> el;
-        sort(a.begin(), a.end(), greater<int>());
-
+  
         vector<int> output;
-        vector<bool> chosen(n, false);
-        output.push_back(a[0]);
-        chosen[0] = true;
+       
+        int max_gcd = 0, cur_gcd;
+        int max_index;
         
-        for (int i = 1; i < n; ++i) {
-            int max_gcd = 0;
-            int max_j;
-            for (int j = 1; j < n && max_gcd < a[j]; ++j) {
-                if (chosen[j]) continue;
-                int cur_gcd = numeric_limits<int>::max();
-                for (int k = 0; k < output.size(); ++k) {
-                    cur_gcd = min(cur_gcd, gcd(output[k], a[j]));
-                }
-                if (cur_gcd > max_gcd) {
-                    max_gcd = cur_gcd;
-                    max_j = j;
-                }
+        while (!a.empty()) {
+
+            // Idea from - https://codeforces.com/contest/1407/submission/92313307
+            
+            vector<pair<int, int>> v;
+            
+            for (int i = 0; i < a.size(); ++i) {
+                cur_gcd = gcd(max_gcd, a[i]);
+                v.emplace_back(make_pair(cur_gcd, i));
             }
 
-            output.push_back(a[max_j]);
-            chosen[max_j] = true;
+            pair<int, int> m = *(max_element(v.begin(), v.end()));
 
+            max_gcd = m.first;
+            output.push_back(a[m.second]);
+            a.erase(a.begin() + m.second);
         }
 
         for (auto el: output) cout << el << ' ';
