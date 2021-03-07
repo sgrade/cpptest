5905dd106c76be64dbf1d68ef480c72bf523b7ac
Roman Klyuev <sgrade@users.noreply.github.com>
2021-03-07 19:21:30 +0100

    codeforces

--- codeforces/455A.cpp
+++ codeforces/455A.cpp
@@ -1,8 +1,8 @@
 // A. Boredom
-// NOT FINISHED
 
 #include <iostream>
 #include <map>
+#include <vector>
 #include <algorithm>
 
 using namespace std;
@@ -15,13 +15,25 @@ int main(){
 
     int tmp;
     map<int, int> mp;
+    vector<int> a(n);
     for (int i = 0; i < n; ++i)
     {
         cin >> tmp;
         ++mp[tmp];
+        a[i] = tmp;
     }
 
+    vector<unsigned long long> dp;
+    // Key ideas - https://www.youtube.com/watch?v=vGTzawku3lM
 
+    dp.push_back(0);
+    dp.push_back(mp[1]);
+    for (int i = 2; i <= 100000; ++i)
+    {
+        dp.push_back(max(dp[i-1], 1ULL * i * mp[i] + dp[i-2]));
+    }
+    
+    cout << *dp.rbegin() << endl;
 
     return 0;
 }
