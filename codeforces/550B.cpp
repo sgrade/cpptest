bc9bf44207cdb82ca404356ae4a7a612e3eaa24c
Roman Klyuev <sgrade@users.noreply.github.com>
2021-03-16 12:46:27 +0100

    codeforces

--- codeforces/550B.cpp
+++ codeforces/550B.cpp
@@ -1,10 +1,7 @@
 // B. Preparing Olympiad
-// Wrong answer on test 4
 
 #include <iostream>
 #include <algorithm>
-#include <set>
-#include <map>
 #include <vector>
 #include <numeric>
 
@@ -15,48 +12,23 @@ vector<int> people;
 vector<int> combination;
 
 int l, r, x, ans = 0;
-map<int, int> mp;
 
 void check_combination(vector<int> v)
-{
-    int current_ans;
-    
+{   
     int sum = accumulate(v.begin(), v.end(), 0);
     if (sum >= l && sum <= r)
     {
-        bool flag = true;
-        for (int i = 1; i < v.size(); ++i)
+        auto mn_mx = minmax_element(combination.begin(), combination.end());
+        if (*mn_mx.second - *mn_mx.first >= x)
         {
-            if (v[i] - v[i-1] < x)
-            {
-                flag = false;
-                break;
-            }
-        }
-        if (flag)
-        {
-            current_ans = 1;
-            for (auto &el: combination)
-                current_ans *= mp[el];
-            ans += current_ans;
+            ++ans;
         }
     }
 }
 
-
 // https://stackoverflow.com/questions/12991758/creating-all-possible-k-combinations-of-n-items-in-c/28698654
-/*
-void pretty_print(const vector<int>& v) {
-  static int count = 0;
-  cout << "combination no " << (++count) << ": [ ";
-  for (int i = 0; i < v.size(); ++i) { cout << v[i] << " "; }
-  cout << "] " << endl;
-}
-*/
-
 void go(int offset, int k) {
     if (k == 0) {
-    // pretty_print(combination);
     check_combination(combination);
     return;
     }
@@ -71,19 +43,13 @@ void go(int offset, int k) {
 int main(){
 
     int n;
-    // int l, r, x;
     cin >> n >> l >> r >> x;
 
-    set<int> c;
-    int tmp;
-    for (int i = 0; i < n; ++i)
+    vector<int> c(n);
+    for (auto &el: c)
     {
-        cin >> tmp;
-        c.insert(tmp);
-        ++mp[tmp];
+        cin >> el;
     }
-
-    // int ans = 0;
 
     for (int i = 2; i < c.size()+1; ++i)
     {
