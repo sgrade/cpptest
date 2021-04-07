47939e4a61826989639fc5188c3ca6898f5336e1
Roman Klyuev <sgrade@users.noreply.github.com>
2021-04-07 11:22:19 +0200

    codeforces

--- codeforces/1485B.cpp
+++ codeforces/1485B.cpp
@@ -1,13 +1,8 @@
 // B. Replace and Keep Sorted
-// Time limit exceeded on test 3
 
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#include <numeric>
 
 using namespace std;
-using ull = unsigned long long;
 
 
 int main() {
@@ -15,33 +10,18 @@ int main() {
     int n, q, k;
     cin >> n >> q >> k;
 
-    vector<int> a(n+2);
-    a[0] = 0;
-    for (int i = 1; i < n+1; ++i) {
-        cin >> a[i];
-    }
-    a[n+1] = k;
-
-    vector<int> dp(n+2);
-    dp[0] = 0;
-    for (int i = 1; i < n+1; ++i) {
-        dp[i] = max(0, a[i+1] - a[i-1] - 2);
+    int a[n];
+    for (auto &el: a) {
+        cin >> el;
     }
     
-    int l, r, ans;
+    int l, r;
+    // Editorial - https://codeforces.com/blog/entry/87470
     for (int i = 0; i < q; ++i) {
         cin >> l >> r;
-        // l and r are 1-indexed, but we already compensated it before by inserting dp[0] = 0
-        ans = accumulate(dp.begin()+l, dp.begin()+r+1, 0ULL);
-        
-        // Correction for range from 1 to a[l-1], which are not included in dp
-        ans += a[l-1];
-        // Correction for range from a[r] to a[r+1], which are included in dp, but should not
-        ans -= max(0, a[r+1] - a[r] - 1);
-        // Correction for range from a[r] to k
-        ans += max(0, k - a[r]);
-
-        cout << ans << endl;
+        --l;
+        --r;
+        cout << (a[l] - 1) + (k - a[r]) + 2 * ((a[r] - a[l]) - (r - l)) << endl;
     }
 
     return 0;
