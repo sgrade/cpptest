201dc32e21e4f25aee057dd2edf1cf33b4112e09
Roman Klyuev <sgrade@users.noreply.github.com>
2021-04-08 16:17:11 +0200

    codeforces

--- codeforces/1498B.cpp
+++ codeforces/1498B.cpp
@@ -3,29 +3,17 @@
 #include <iostream>
 #include <algorithm>
 #include <map>
-#include <numeric>
-#include <vector>
 
 using namespace std;
 
-
-map<int, int, greater <int>> mp;
-int W;
-
-// w - width, q - quantity
-pair<int, int> place(const int w, const int q) {
-    int cur_w, cur_h;
-    cur_w = W / w;
-    cur_h = q / cur_w;
-    mp[w] = q % cur_w;
-
-    return pair(cur_h, cur_w);
-}
 
 int main() {
 
     int t;
     cin >> t;
+
+    map<int, int, greater <int>> mp;
+    int W;
 
     while (t--) {
 
@@ -38,35 +26,24 @@ int main() {
             ++mp[tmp];
         }
 
-        int ans = 0;
-
-        // To store current 
-        vector<int> box(100001, 0);
-        vector<int>::iterator it_box;
+        int ans = 1;
 
         map<int, int>::iterator it_mp = mp.begin();
-        int w, h, idx;
-        int current_ans;
-        while (it_mp != mp.end()) {
-            w = it_mp->first;
-
-            it_box = find_if(box.begin(), box.end(), [w](int w_left){
-                return W - w >= w_left;
+        int w_left = W, i = 0;
+        while (i < n) {
+            it_mp = find_if(mp.begin(), mp.end(), [w_left](pair<int, int> p){
+                return p.first <= w_left && p.second > 0;
             });
-            idx = distance(box.begin(), it_box);
-            
-            pair<int, int> p = place(W - box[idx], mp[w]);
-
-            for (int i = idx; p.first > 0; ++i, --h) {
-                box[i] += p.second;
+            if (it_mp == mp.end()) {
+                ++ans;
+                w_left = W;
+            }
+            else {
+                --it_mp->second;
+                w_left -= it_mp->first;
+                ++i;
             }
         }
-
-        auto ans_it = find_if(box.begin(), box.end(), [](int x){
-            return x == 0;
-        });
-        int anss = distance(box.begin(), ans_it) - 1;
-        ans = max(1, anss);
 
         cout << ans << endl;
     }
