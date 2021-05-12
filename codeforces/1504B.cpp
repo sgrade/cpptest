a8af66415919356a576b80cc84c18f572e8085b2
Roman Klyuev <sgrade@users.noreply.github.com>
2021-05-12 20:27:02 +0000

    codeforces

--- codeforces/1504B.cpp
+++ codeforces/1504B.cpp
@@ -1,5 +1,4 @@
 // B. Flip the Bits
-// NOT FINISHED
 
 #include <iostream>
 #include <string>
@@ -22,47 +21,30 @@ int main() {
         getline(cin >> ws, b);
 
         bool ans = true;
-
-        if (n % 2 != 0 && a[n-1] != b[n-1]) {
-            ans = false;
-        }
-        else {
-            if (n % 2 != 0) {
-                --n;
+        
+        int i = 0;
+        int cnt = 0;
+        for (; i < n; ++i) {
+            if (a[i] == '1') {
+                ++cnt;
+            }
+            else {
+                --cnt;
             }
 
-            int zeroes_a = 0, zeroes_b = 0, ones_a = 0, ones_b = 0;
-            int cnt = 0;
-            for (int i = 0; i < n; i+=2) {
-
-                
-                cnt += 2;
-
-                if (a[i] == '0')
-                    ++zeroes_a;
-                if (a[i+1] == '0')
-                    ++zeroes_a;
-                if (b[i] == '0')
-                    ++zeroes_b;
-                if (b[i+1] == '0')
-                    ++zeroes_b;
-                
-                if (zeroes_a % 2 != zeroes_b % 2 || zeroes_a != zeroes_b) {
+            if ((a[i] == b[i]) != (a[i+1] == b[i+1])) {
+                // Editorial - https://codeforces.com/blog/entry/89319
+                if (cnt != 0) {
                     ans = false;
                     break;
-                }
-                
-                ones_a = cnt - zeroes_a;
-                ones_b = cnt - zeroes_b;
-
-                if (zeroes_a == ones_a && zeroes_a == zeroes_b) {
-                    zeroes_a = 0;
-                    zeroes_b = 0;
-                    cnt = 0;
                 }
             }
         }
         
+        if (n % 2 != 0 && a[n-1] != b[n-1]) {
+            ans = false;
+        }
+    
         cout << (ans ? "YES" : "NO") << endl;
     }
 
