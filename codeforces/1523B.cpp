73a3eccbdbf6d30b3749b734b361f1f2d1c1fbbf
Roman Klyuev <sgrade@users.noreply.github.com>
2021-06-03 17:59:28 +0000

    codeforces

--- codeforces/1523B.cpp
+++ codeforces/1523B.cpp
@@ -1,10 +1,11 @@
-// 
+// B. Lord of the Values
 
 #include <iostream>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
-
+using ll = long long;
 
 int main() {
 
@@ -13,7 +14,34 @@ int main() {
 
     while (t--) {
 
+        int n;
+        cin >> n;
 
+        vector<ll> a(n);
+        for (auto &el: a) {
+            cin >> el;
+        }
+
+        // Editorial - https://codeforces.com/blog/entry/91271
+        int i;
+        vector<vector<ll>> output;
+        for (int j = 1; j < n; j += 2) {
+            i = j-1;
+            for (int m = 0; m < 3; ++m) {
+                output.push_back({1, i+1, j+1});
+                a[i] += a[j];
+                output.push_back({2, i+1, j+1});
+                a[j] -= a[j];
+            }
+        }
+
+        cout << output.size() << endl;
+        for (auto &v: output) {
+            for (auto &el: v) {
+                cout << el << ' ';
+            }
+            cout << endl;
+        }
         
     }
 
