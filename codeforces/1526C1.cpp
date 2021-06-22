dc4013a08a4dbec21f2c15ffdc6f4246c687fca0
Roman Klyuev <sgrade@users.noreply.github.com>
2021-06-22 14:55:12 +0000

    codeforces

---
+++ codeforces/1526C1.cpp
@@ -0,0 +1,45 @@
+// C1. Potions (Easy Version)
+
+#include <iostream>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+
+    multiset<int> mst;
+
+    long long health = 0LL;
+    int tmp;
+    for (int i = 0; i < n; ++i) {
+        cin >> tmp;
+        if (health + tmp < 0) {
+            if (mst.empty()) {
+                continue;
+            }
+            else {
+                if (*mst.begin() < tmp) {
+                    health -= *mst.begin();
+                    mst.erase(mst.begin());
+                }
+                else {
+                    continue;
+                }
+            }
+        }
+
+        health += tmp;
+        mst.insert(tmp);
+    }
+
+    cout << mst.size() << endl;
+
+    return 0;
+}
