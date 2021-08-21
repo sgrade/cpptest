8e4724c77aa154cb6cc354cf1d87b487d64136f3
Roman Klyuev <sgrade@users.noreply.github.com>
2021-08-21 18:25:10 +0000

    codeforces

---
+++ codeforces/1549A.cpp
@@ -0,0 +1,23 @@
+// A. Gregor and Cryptography
+
+#include <iostream>
+
+
+int main() {
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+
+        int P;
+        std::cin >> P;
+
+        // Editorial - https://codeforces.com/blog/entry/93586
+
+        int a = 2, b = P - 1;
+        std::cout << a << ' ' << b << std::endl;
+    }
+
+    return 0;
+}
