fb2cc1bdcd7faff8929c6ce5378fdf65afbb9ebb
Roman Klyuev <sgrade@users.noreply.github.com>
2021-09-12 17:19:36 +0000

    templates/number_theory/prime_factors_int

---
+++ templates/number_theory/prime_factors_int.cpp
@@ -0,0 +1,51 @@
+// All prime factors (divisors) of a number
+
+// Algorithm used in this file
+// https://www.geeksforgeeks.org/print-all-prime-factors-of-a-given-number/
+
+// Alternative algorithm (not used in this file)
+// https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+
+#include <iostream>
+#include <math.h>
+#include <vector>
+
+using namespace std;
+
+
+vector<int> allPrimeFactors (int n){
+    // Source - // https://www.geeksforgeeks.org/print-all-prime-factors-of-a-given-number/
+    
+    vector<int> v;
+    if (n == 0 || n == 1) return v;
+
+    while(n % 2 == 0){
+        v.push_back(2);
+        n /= 2;
+    }
+
+    for (int i=3; i <= sqrtl(n); i += 2){
+        while (n % i == 0){
+            v.push_back(i);
+            n = n/i;
+        }
+    }
+    
+    if (n > 2) v.push_back(n);
+
+    return v;
+
+}
+
+
+int main(){
+
+    int n = 60;
+
+    for (auto el: allPrimeFactors(n)){
+        cout << el << ' ';
+    }
+    cout << endl;
+
+    return 0;
+}
