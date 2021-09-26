dac2f7c4762b9884bcd4031a709ea0d3014c0179
Roman Klyuev <sgrade@users.noreply.github.com>
2021-09-26 19:08:56 +0000

    fast IO

---
+++ codeforces/fast_io.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// The explanation is here - 
+// https://www.geeksforgeeks.org/fast-io-for-competitive-programming/
+
+
+int main() {
+
+    // The below speeds up cin/cout greatly
+    // E.g. in 1574C.cpp this is allowed me to reduce time greatly (avoid TLE). 
+    // No other ways happened.
+    // https://codeforces.com/contest/1574/my
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    // Also this
+    cout << "USE '\n' instead of endl" << 'n';
+
+    return 0;
+}
