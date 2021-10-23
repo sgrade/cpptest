db85b61a52591b8234f25bb22cbf92a6c2c08d77
Roman K <sgrade@users.noreply.github.com>
2021-10-23 21:51:13 +0000

    codeforces

--- codeforces/1594B.cpp
+++ codeforces/1594B.cpp
@@ -1,3 +1,38 @@
 // B. Special Numbers
 
 #include <iostream>
+#include <math.h>
+
+using namespace std;
+
+long long MOD = 1e9 + 7;
+
+
+int main() {
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        
+        long long n, k;
+        cin >> n >> k;
+
+        long long ans = 0, power = 1;
+
+        // Explanation - https://www.youtube.com/watch?v=X_kVGgHcfQs
+
+        for (long long i = 0; i < 31; ++i) {
+            if (k & (1 << i)) {
+                ans = (ans + power) % MOD;
+            }
+            
+            power *= n;
+            power %= MOD;
+        }
+        
+        cout << ans << endl;
+    }
+
+    return 0;
+}
