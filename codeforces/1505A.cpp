fafbaa726d358b4ac68b50eafa6ed3fa2481cef8
Roman K <sgrade@users.noreply.github.com>
2021-12-09 15:43:28 +0000

    codeforces

---
+++ codeforces/1505A.cpp
@@ -0,0 +1,20 @@
+// A. Is it rated - 2
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main() {
+
+    string tmp;
+
+    // Idea - https://codeforces.com/contest/1505/submission/111654105
+
+    while(cin >> tmp) {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
