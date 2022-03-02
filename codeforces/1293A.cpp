55d598a3d6cef8791071fc139094d59d41f0b9be
Roman Klyuev <rklyuev@subspace.com>
2022-03-02 21:19:33 +0000

    codeforces

---
+++ codeforces/1293A.cpp
@@ -0,0 +1,49 @@
+// A. ConneR and the A.R.C. Markland-N
+
+#include <iostream>
+#include <set>
+#include <algorithm>
+
+using namespace std;
+
+
+int main() {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+
+        int n, s, k;
+        cin >> n >> s >> k;
+
+        set<int> a;
+        int tmp;
+        for (int i = 0; i < k; ++i) {
+            cin >> tmp;
+            a.insert(tmp);
+        }
+
+        int ans = 0;
+
+        ++n;
+
+        // Idea - https://codeforces.com/contest/1293/submission/69125876
+        while (true) {
+            if (s - ans > 0 && a.find(s - ans) == end(a)) {
+                break;
+            }
+            if (s + ans < n && a.find(s + ans) == end(a)) {
+                break;
+            }
+            ++ans;
+        }
+
+        cout << ans << '\n';
+    }
+
+    return 0;
+}
