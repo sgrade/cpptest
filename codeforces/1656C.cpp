9fe8c4bacd345e29d13d47cfe196641d75725c71
Roman K <sgrade@users.noreply.github.com>
2022-04-10 15:30:52 +0000

    codeforces

---
+++ codeforces/1656C.cpp
@@ -0,0 +1,47 @@
+// C. Make Equal With Mod
+
+#include <iostream>
+#include <set>
+#include <algorithm>
+
+using namespace std;
+
+
+int main() {
+
+    int t;
+    cin >> t;
+
+    while (t--) 
+    {
+        int n;
+        cin >> n;
+
+        set<int> a;
+        int tmp;
+        for (int i = 0; i < n; ++i) 
+        {
+            cin >> tmp;
+            a.insert(tmp);
+        }
+
+        bool ans = true;
+
+        if (a.find(1) != a.end() && a.size() > 1) 
+        {
+            // Editorial - https://codeforces.com/blog/entry/101211
+            set<int>::iterator it = a.begin();
+            int prev = *it, cur;
+            while (it++ != a.end()) 
+            {
+                cur = *it;
+                if (cur - prev == 1) ans = false;
+                prev = cur;
+            }
+        }
+
+        cout << (ans ? "YES\n" : "NO\n");
+    }
+
+    return 0;
+}
