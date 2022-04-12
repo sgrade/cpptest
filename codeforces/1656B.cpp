8d8cf3958a57fb9f8b071793a5ad32b882c20bae
Roman K <sgrade@users.noreply.github.com>
2022-04-12 20:44:43 +0000

    codeforces

---
+++ codeforces/1656B.cpp
@@ -0,0 +1,53 @@
+// B. Subtract Operation
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+int main() {
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+
+        int n, k;
+        cin >> n >> k;
+
+        vector<int> a(n);
+        for (int &el: a) {
+            cin >> el;
+        }
+        sort(begin(a), end(a));
+
+        bool ans = false;
+
+        // Editorial - https://codeforces.com/blog/entry/101211
+
+        int l = 0, r = 0;
+        for (; l < n; ++l) {
+
+            // Iteration ideas are from
+            // https://codeforces.com/contest/1656/submission/150731872
+
+            while (r < l) ++r;
+
+            for (; r < n - 1; ++r) {
+                if (a[r] - a[l] < k) continue;
+                break;
+            }
+
+            if (a[r] - a[l] == k) {
+                ans = true;
+                break;
+            }
+        }
+
+        cout << (ans ? "YES\n" : "NO\n");
+    }
+
+    return 0;
+}
