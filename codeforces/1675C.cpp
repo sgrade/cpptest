4c3a0538e4989dd5c928d9b4b635a24663446f3e
Roman K <sgrade@users.noreply.github.com>
2022-05-07 16:20:36 +0000

    codeforces

--- codeforces/1675C.cpp
+++ codeforces/1675C.cpp
@@ -1,9 +1,7 @@
 // C. Detective Task
-// NOT FINISHED
 
 #include <iostream>
 #include <string>
-#include <vector>
 #include <algorithm>
 
 using namespace std;
@@ -26,34 +24,41 @@ int main() {
 
         int ans = 0;
 
-        if (n == 1 || s[0] == 0) {
+        if (n == 1 || s[0] == '0') {
             ans = 1;
         }
 
         else {
 
-            size_t first_not_one = s.find_first_not_of('1');
-            
-            if (first_not_one == 0) ans = 0;
-            else ans = 1;
-            
-            size_t next_zero = s.find_first_of('0', first_not_one + 1);
-            if (next_zero != string::npos) {
-                int cnt = 0;
-                for (int i = next_zero; i > -1; --i) {
-                    ++cnt;
-                    if (s[i] != '?') break;
+            int zeroes = count(s.begin(), s.end(), '0');
+            size_t idx;
+
+            if (zeroes >= 1) {
+                
+                idx = s.find_first_of('0');
+
+                idx = s.find('0');
+
+                int i;
+                for (i = idx; i > -1; --i) {
+                    if (s[i] == '1') {
+                        break;
+                    }
                 }
-                ans += cnt;
+                ans = idx - i;
+                if (i != -1) ++ans;
             }
 
             else {
-                int cnt = 0;
-                for (int i = n-1; i > -1; --i) {
-                    ++cnt;
-                    if (s[i] != '?') break;
+                idx = s.find_last_of('1');
+
+                if (idx == string::npos) {
+                    ans = n;
                 }
-                ans += cnt;
+
+                else {
+                    ans = n - idx;
+                }
             }
         }
 
