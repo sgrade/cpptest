61bcb5e03eab59ba35a26cc1391290dc62d13d19
Roman K <sgrade@users.noreply.github.com>
2022-05-19 06:36:31 +0000

    codeforces

--- codeforces/1490D.cpp
+++ codeforces/1490D.cpp
@@ -1,5 +1,4 @@
 // D. Permutation Transformation
-// NOT FINISHED
 
 #include <iostream>
 #include <vector>
@@ -10,44 +9,25 @@ using namespace std;
 
 int n;
 vector<int> a;
-vector<vector<int>> adj;
-vector<bool> visited;
 int d;
 vector<int> ans(n + 1);
 
-void add_edge (int &cur_idx, int &mx_idx) {
-    visited[cur_idx] = true;
-    adj[cur_idx].push_back(mx_idx);
-    adj[mx_idx].push_back(cur_idx);
-}
 
-int find_mx_idx (int l, int r) {
+void explore (int l, int r) {
 
     if (l > r) {
         return;
     }
 
-    if (l == r) {
-        ans[l] = d;
-        --d;
-        return;
-    }
-
-    int cur = 0, cur_idx;
-
-    for (int i = l; i < r + 1; ++i) {
-        if (a[i] > cur) {
-            cur = a[i];
-            cur_idx = i;
-        }
-    }
-
-    return cur_idx;
-} 
-
-void explore (int mx_idx, int l, int r) {
-
+    int mx_idx = distance(a.begin(), max_element(a.begin() + l, a.begin() + r + 1));
     
+    ++d;
+    explore(l, mx_idx - 1);
+    explore(mx_idx + 1, r);
+    --d;
+
+    ans[mx_idx] = d;
+    return;
 }
 
 
@@ -65,21 +45,11 @@ int main() {
         for (int i = 1; i < n + 1; ++i) {
             cin >> a[i];
         }
-
-        adj.clear();
-        adj.resize(n + 1);
-        visited.clear();
-        visited.resize(n + 1);
-        
-        vector<int>::iterator it = max_element(a.begin(), a.end());
-        int mx_idx = distance(a.begin(), it);
-        int mx = a[mx_idx];
-        visited[mx_idx] = true;
         
         d = 0;
         ans.clear();
         ans.resize(n + 1);
-        explore(mx_idx, 1, n);
+        explore(1, n);
 
         for (int i = 1; i < n + 1; ++i) {
             cout << ans[i] << ' ';
