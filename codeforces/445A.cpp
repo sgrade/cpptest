8f8578409b85af36630604cd469fb59d982974a7
Roman K <sgrade@users.noreply.github.com>
2022-05-20 18:06:19 +0000

    codeforces

--- codeforces/445A.cpp
+++ codeforces/445A.cpp
@@ -1,5 +1,4 @@
 // A. DZY Loves Chessboard
-// Wrong answer on test 5
 
 #include <iostream>
 #include <vector>
@@ -15,30 +14,10 @@ void explore(const int &r, const int &c) {
     
     if (a[r][c] != '.') {
         ans[r][c] = '-';
-        return;
     }
 
-    bool b = true;
-    bool w = true;
-
-    if (r - 1 >= 0) {
-        if (ans[r-1][c] == 'B') b = false;
-        if (ans[r-1][c] == 'W') w = false;
-    }
-
-    if (c - 1 >= 0) {
-        if (ans[r][c-1] == 'B') b = false;
-        if (ans[r][c-1] == 'W') w = false;
-    }
-
-    if (!b) {
-        ans[r][c] = 'W';
-    }
-    else if (!w) {
-        ans[r][c] = 'B';
-    }
     else {
-        if (c % 2 == 0) {
+        if ((r + c) % 2 == 0) {
             ans[r][c] = 'B';
         }
         else {
@@ -61,6 +40,8 @@ int main() {
     for (int i = 0; i < n; ++i) cin >> a[i];
 
     ans.resize(n, vector<char>(m));
+
+    // Editorial - https://codeforces.com/blog/entry/12959
 
     int d = 0;
     while (d < max(n, m)) {
