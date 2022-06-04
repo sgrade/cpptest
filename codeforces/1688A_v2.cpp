58601c377167a60bfd50b5d27c80101a7b2620b7
Roman K <sgrade@users.noreply.github.com>
2022-06-04 18:52:47 +0000

    codeforces

--- codeforces/1688A_v2.cpp
+++ codeforces/1688A_v2.cpp
@@ -1,10 +1,7 @@
 // A. Cirno's Perfect Bitmasks Classroom
-// Time limit exceeded on test 4
 
 #include <iostream>
-#include <algorithm>
-#include <vector>
-#include <string>
+
 
 using namespace std;
 
@@ -22,12 +19,16 @@ int main() {
         int x;
         cin >> x;
 
-        int y = 1;
-        while (true) {
-            if ((x & y) && (x ^ y)) {
-                break;
-            }
-            ++y;
+        // Idea - https://codeforces.com/contest/1688/submission/159352249
+
+        int y = x & -x;
+
+        if (y == x) {
+            y++;
+        }
+
+        if (x == 1) {
+            y = 3;
         }
 
         cout << y << '\n';
