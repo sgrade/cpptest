a9b574c2afe9841da88bf86eb5ffcaee6331eae7
Roman K <sgrade@users.noreply.github.com>
2022-06-24 16:00:39 +0000

    codeforces

--- codeforces/1695C.cpp
+++ codeforces/1695C.cpp
@@ -1,5 +1,4 @@
 // C. Zero Path
-// Time limit exceeded on pretest 4
  
 #include <iostream>
 #include <algorithm>
@@ -11,25 +10,6 @@ using namespace std;
 int n, m;
  
 bool ans;
- 
-void dfs(const vector<vector<int>>& a, int i, int j, int cost) {
-    
-    cost += a[i][j];
-    if (i == n - 1 && j == m - 1) {
-        if (cost == 0) {
-            ans = true;
-        }
-    }
- 
-    if (i + 1 < n) {
-        dfs(a, i + 1, j, cost);
-    }
-    if (j + 1 < m) {
-        dfs(a, i, j + 1, cost);
-    }
-}
- 
- 
  
  
 int main() {
@@ -52,7 +32,32 @@ int main() {
  
         ans = false;
  
-        dfs(a, 0, 0, 0);
+        // Editorial - https://codeforces.com/blog/entry/103996
+        if ((n + m) % 2 != 0) {
+
+            // https://www.youtube.com/watch?v=90YSRIreNLY
+            vector<vector<pair<int, int>>> dp(n, vector<pair<int, int>>(m));
+            dp[0][0].first = a[0][0];
+            dp[0][0].second = a[0][0];
+            for (int j = 1; j < m; ++j) {
+                dp[0][j].first = dp[0][j - 1].first + a[0][j];
+                dp[0][j].second = dp[0][j - 1].second + a[0][j];
+            }
+            for (int i = 1; i < n; ++i) {
+                dp[i][0].first = dp[i - 1][0].first + a[i][0];
+                dp[i][0].second = dp[i - 1][0].first + a[i][0];
+            }
+            for (int i = 1; i < n; ++i) {
+                for (int j = 1; j < m; ++j) {
+                    dp[i][j].first = min(dp[i][j - 1].first, dp[i - 1][j].first) + a[i][j];
+                    dp[i][j].second = max(dp[i][j - 1].second, dp[i - 1][j].second) + a[i][j];
+                }
+            }
+
+            if (dp[n - 1][m - 1].first <= 0 && dp[n - 1][m - 1].second >= 0) {
+                ans = true;
+            }
+        }
  
         cout << (ans ? "YES\n" : "NO\n");
     }
