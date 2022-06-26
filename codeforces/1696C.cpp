0d01425e16bbd0e700765e4f42c0b74225a87a99
Roman K <sgrade@users.noreply.github.com>
2022-06-26 08:09:21 +0000

    codeforces

--- codeforces/1696C.cpp
+++ codeforces/1696C.cpp
@@ -1,14 +1,32 @@
 // C. Fishingprince Plays With Array
-// NOT FINISHED
 
 #include <iostream>
 #include <algorithm>
 #include <vector>
-#include <numeric>
 
 using namespace std;
 using ll = long long;
 
+
+// Ideas for implementation - https://codeforces.com/contest/1696/submission/161736565
+vector<pair<int, ll>> tf(const vector<int> &v, const int &m) {
+    vector<pair<int, ll>> output;
+    int y;
+    for (int x: v) {
+        y = 1;
+        while (x % m == 0) {
+            x /= m;
+            y *= m;
+        }
+        if (!output.empty() && x == output.back().first) {
+            output.back().second += y;
+        }
+        else {
+            output.emplace_back(x, y);
+        }
+    }
+    return output;
+}
 
 int main() {
 
@@ -31,12 +49,13 @@ int main() {
 
         bool ans = false;
 
-        ll sum1 = accumulate(a.begin(), a.end(), 0);
-        ll sum2 = accumulate(b.begin(), b.end(), 0);
+        // Editorial - https://codeforces.com/blog/entry/103479
+        vector<pair<int, ll>> transformed_a = tf(a, m);
+        vector<pair<int, ll>> transformed_b = tf(b, m);
 
-        if (sum1 % sum2 == 0) ans = true;
+        ans = (transformed_a == transformed_b ? true : false);
 
-        cout << (ans ? "YES\n" : "NO\n");
+        cout << (ans ? "Yes\n" : "No\n");
     }
 
     return 0;
