aa998a5c2b9bb3b64a0d7327986e7993e2dbc98d
Roman K <sgrade@users.noreply.github.com>
2022-07-17 10:17:04 +0000

    leetcode

--- leetcode/moving-average-from-data-stream.cpp
+++ leetcode/moving-average-from-data-stream.cpp
@@ -2,30 +2,30 @@
 // https://leetcode.com/problems/moving-average-from-data-stream/
 
 #include <iostream>
-#include <numeric>
-#include <vector>
+#include <queue>
 
 using namespace std;
 
 class MovingAverage {
 public:
-    MovingAverage(int size) 
-    : size_(size)
-    {
-        
+    MovingAverage(int size) {
+        size_ = size;
+        sm = 0.0;
+        ans = 0.0;
     }
     
     double next(int val) {
-        a.push_back(val);
-        cnt_ = min(int(a.size()), size_);
-        sm = accumulate(a.end() - cnt_, a.end(), 0.0);
-        ans = sm / cnt_;
-        return ans;
+        if (q.size() == size_) {
+            sm -= q.front();
+            q.pop();
+        }
+        sm += val;
+        q.emplace(val);
+        return sm / q.size();
     }
+    
 private:
     int size_;
-    vector<int> a;
-    double cnt_ = 1.0;
-    double ans = 0;
-    double sm = 0;
+    queue<int> q;
+    double sm, ans;
 };
