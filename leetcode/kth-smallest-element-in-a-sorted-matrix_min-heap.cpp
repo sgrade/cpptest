5a979cab6ccc8bf8422b983323a201c4921a3700
Roman K <sgrade@users.noreply.github.com>
2022-08-03 20:52:22 +0000

    leetcode

--- leetcode/kth-smallest-element-in-a-sorted-matrix_min-heap.cpp
+++ leetcode/kth-smallest-element-in-a-sorted-matrix_min-heap.cpp
@@ -1,6 +1,10 @@
 // 378. Kth Smallest Element in a Sorted Matrix
 // https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/
 
+#include <bits/stdc++.h>
+
+using namespace std;
+
 
 class Solution {
 public:
