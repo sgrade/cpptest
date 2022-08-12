5259813f6941ad14078a1475fb184426e6546f9a
Roman K <sgrade@users.noreply.github.com>
2022-08-12 18:47:09 +0000

    codeforces

---
+++ codeforces/1234B1.cpp
@@ -0,0 +1,44 @@
+// B1. Social Network (easy version)
+
+#include <iostream>
+#include <deque>
+#include <set>
+
+using namespace std;
+
+
+int main() {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, k;
+    cin >> n >> k;
+
+    deque<int> q;
+    set<int> st;
+
+    int tmp;
+
+    for (int i = 0; i < n; ++i) {
+        cin >> tmp;
+        if (st.find(tmp) == st.end()) {
+            st.emplace(tmp);
+            if (q.size() == k) {
+                st.erase(q.front());
+                q.pop_front();
+            }
+            q.push_back(tmp);
+        }
+    }
+
+    cout << q.size() << '\n';
+
+    while (!q.empty()) {
+        cout << q.back() << ' ';
+        q.pop_back();
+    }
+    cout << '\n';
+
+    return 0;
+}
