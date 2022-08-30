7e96b96841aa6dc679b17df9834343f381b799a9
sgrade <sgrade@users.noreply.github.com>
2022-08-30 18:02:29 +0000

    leetcode

---
+++ leetcode/pascals-triangle-ii_recursive_TLE.cpp
@@ -0,0 +1,42 @@
+// 119. Pascal's Triangle II
+// https://leetcode.com/problems/pascals-triangle-ii/
+// 119. Pascal's Triangle II
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<int> ans(rowIndex + 1);
+        for (int i = 0; i <= rowIndex; ++i) {
+            ans[i] = getNum(rowIndex, i);
+        }
+        return ans;
+    }
+    
+    int getNum(int row, int col) {
+        if (row == 0 || col == 0 || row == col) {
+            return 1;
+        }
+        return getNum(row - 1, col - 1) + getNum (row - 1, col);
+    }
+};
+
+
+
+int main() {
+
+    cout << "HELLO!\n";
+
+    Solution sol;
+    vector<int> a = sol.getRow(3);
+    for (int &el: a) {
+        cout << el << ' ';
+    }
+    cout << '\n';
+
+    return 0;
+}
