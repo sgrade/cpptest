2adc938999fb47617d45fddca7f6edd5e70e7ba8
sgrade <sgrade@users.noreply.github.com>
2022-10-03 13:12:46 +0000

    codeforces

--- codeforces/1735A.cpp
+++ codeforces/1735A.cpp
@@ -1,5 +1,4 @@
 // A. Working Week
-// Wrong answer on pretest 2
 
 #include <iostream>
 #include <vector>
@@ -21,26 +20,7 @@ int main() {
         int n;
         cin >> n;
 
-        n -= 4;
-        
-        int l1 = 1, l2 = 1, l3 = n - l2;
-
-        int ans = min({abs(l1 - l2), abs(l2 - l3), abs(l1 - l3)});
-        
-        int current_ans, mid;
-
-        int diff = n / 3;
-        if (diff > 0) {
-            diff += n % 3;
-        }
-
-        int x;
-        for (int i = 0; i <= diff; ++i) {
-            x = l2 + i;
-            l3 = n - x;
-            current_ans = min(abs(l1 - x), abs(x - l3));
-            ans = max(ans, current_ans);
-        }
+        int ans = (n - 3) / 3 - 1;
 
         cout << ans << '\n';
     }
