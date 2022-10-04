6e9b6174f3d15f38a0cf6f94fe47d80d1f3312eb
sgrade <sgrade@users.noreply.github.com>
2022-10-04 21:41:52 +0000

    codeforces

--- codeforces/1735C.cpp
+++ codeforces/1735C.cpp
@@ -1,9 +1,7 @@
 // C. Phase Shift
-// NOT FINISHED (cannot figure out the second test)
 
 #include <iostream>
 #include <string>
-#include <map>
 #include <vector>
 
 using namespace std;
@@ -29,31 +27,52 @@ int main() {
 
         string ans;
 
-        int idx = 0;
-        map<char, char> mp;
-        vector<bool> chosen(26);
+        // prev - prev letter in the circle
+        // nxt - next letter in the circle
+        vector<int> prev(26, -1), nxt(26, -1);
 
         int i = 0;
-        for (; i < n; ++i) {
-            if (idx >= 26) 
-                break;
+        for (; i < n ; ++i) {
+
+            char ch = s[i];
+            int ch_idx = s[i] - 'a';
+
+            if (prev[ch_idx] != -1) {
+                ans += alpha[prev[ch_idx]];
+                continue;
+            }
             
-            if (mp.find(s[i]) == mp.end()) {
-                int temp_idx = idx;
-                while (s[i] == alpha[temp_idx] || chosen[temp_idx]) {
-                    ++temp_idx;
-                }
-                mp[s[i]] = alpha[temp_idx];
-                chosen[temp_idx] = true;
-                if (temp_idx == idx) {
-                    ++idx;
+            // Some ideas are from - https://codeforces.com/contest/1735/submission/174417757
+            for (int j = 0; j < 26; ++j) {
+                char candidate = alpha[j];
+                if (ch_idx != j && nxt[j] == -1) {
+                    prev[ch_idx] = j;
+                    nxt[j] = ch_idx;
+
+                    int cnt = 1;
+                    int tmp_idx = j;
+                    bool cycle_is_too_short = false;
+                    while (tmp_idx != -1) {
+                        tmp_idx = nxt[tmp_idx];
+                        ++cnt;
+                        if (tmp_idx == j) {
+                            if (cnt <= 26) {
+                                cycle_is_too_short = true;
+                            }
+                            break;
+                        }
+                    }
+
+                    if (cycle_is_too_short) {
+                        prev[ch_idx] = -1;
+                        nxt[j] = -1;
+                    }
+                    else {
+                        ans += alpha[j];
+                        break;
+                    }
                 }
             }
-            ans += mp[s[i]];
-        }
-
-        for (; i < n; ++i) {
-            ans += mp[s[i]];
         }
 
         cout << ans << '\n';
