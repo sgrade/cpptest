cfa43cf578965b502a3be80603ce91d103ee5782
sgrade <sgrade@users.noreply.github.com>
2022-10-06 16:17:03 +0000

    leetcode

---
+++ leetcode/perfect-squares.cpp
@@ -0,0 +1,45 @@
+// 279. Perfect Squares
+// https://leetcode.com/problems/perfect-squares/
+// Based on Leetcode's Approach 4: Greedy + BFS (Breadth-First Search)
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+class Solution {
+public:
+    int numSquares(int n) {
+        vector<int> squares;
+        for (int i = 1; i * i <= n; ++i) {
+            squares.emplace_back(i * i);
+        }
+        
+        deque<int> q;
+        q.emplace_back(n);
+        
+        int level = 0;
+        while (!q.empty()) {
+            ++level;
+            deque<int> next_q;
+            
+            for (const int &remainder: q) {
+                for (const int square: squares) {
+                    if (remainder == square) {
+                        return level;
+                    }
+                    else if (remainder < square) {
+                        break;
+                    }
+                    else {
+                        next_q.emplace_back(remainder - square);
+                    }
+                }
+            }
+            
+            q = next_q;
+        }
+        
+        return level;
+    }
+};
