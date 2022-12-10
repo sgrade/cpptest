7b7247cf67e219993f95a16545e6d201cb871468
sgrade <sgrade@users.noreply.github.com>
2022-12-10 20:11:04 +0000

    leetcode

---
+++ leetcode/majority-element.cpp
@@ -0,0 +1,29 @@
+// 169. Majority Element
+// https://leetcode.com/problems/majority-element/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// Based on Leetcode's Approach 7: Boyer-Moore Voting Algorithm
+// Optimization is based on https://leetcode.com/problems/majority-element/submissions/857759012/
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count = 0, candidate = -1;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (count == 0) {
+                candidate = nums[i];
+                count = 1;
+            }
+            else if (nums[i] == candidate) {
+                ++count;
+            }
+            else {
+                --count;
+            }
+        }
+        return candidate;
+    }
+};
