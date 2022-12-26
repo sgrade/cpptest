b04a0d8bab8377b522844d31f3f896b9b8789886
sgrade <sgrade@users.noreply.github.com>
2022-12-26 15:08:25 +0000

    leetcode

---
+++ leetcode/minimum-cost-to-connect-sticks.cpp
@@ -0,0 +1,25 @@
+// 1167. Minimum Cost to Connect Sticks
+// https://leetcode.com/problems/minimum-cost-to-connect-sticks/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+class Solution {
+public:
+    int connectSticks(vector<int>& sticks) {
+        long long ans = 0;
+        priority_queue<int, vector<int>, greater<int>> min_heap(sticks.begin(), sticks.end());
+        int nxt;
+        while (min_heap.size() > 1) {
+            nxt = min_heap.top();
+            min_heap.pop();
+            nxt += min_heap.top();
+            min_heap.pop();
+            ans += nxt;
+            min_heap.emplace(nxt);
+        }
+        return ans;
+    }
+};
