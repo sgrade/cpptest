6d1f5c10caf3c13302d1bfb6a2ea34a4cd51cda7
sgrade <sgrade@users.noreply.github.com>
2023-01-06 16:43:50 +0000

    leetcode;

---
+++ leetcode/maximum-ice-cream-bars.cpp
@@ -0,0 +1,21 @@
+// 1833. Maximum Ice Cream Bars
+// https://leetcode.com/problems/maximum-ice-cream-bars/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+class Solution {
+public:
+    int maxIceCream(vector<int>& costs, int coins) {
+        sort(costs.begin(), costs.end());
+        int ans = 0;
+        for (const int& cost: costs) {
+            coins -= cost;
+            if (coins < 0) break;
+            else ++ans;
+        }
+        return ans;
+    }
+};
