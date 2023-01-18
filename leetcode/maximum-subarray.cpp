d668f7ff0c37d90b98c39c1654c7bc788a2f6678
sgrade <sgrade@users.noreply.github.com>
2023-01-18 18:54:18 +0000

    leetcode

--- leetcode/maximum-subarray.cpp
+++ leetcode/maximum-subarray.cpp
@@ -6,13 +6,14 @@
 using namespace std;
 
 
+// Based on Leetcode's Approach 2: Dynamic Programming, Kadane's Algorithm
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int ans = nums[0], cur_ans = nums[0];
+        int ans = nums[0], current_ans = nums[0];
         for (int i = 1; i < nums.size(); ++i) {
-            cur_ans = max(nums[i], cur_ans + nums[i]);
-            ans = max(ans, cur_ans);
+            current_ans = max(nums[i], current_ans + nums[i]);
+            ans = max(current_ans, ans);
         }
         return ans;
     }
