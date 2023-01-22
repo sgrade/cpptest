871f134f96f36319671c68e24f2680566572ac61
sgrade <sgrade@users.noreply.github.com>
2023-01-22 20:45:37 +0000

    leetcode

--- leetcode/restore-ip-addresses.cpp
+++ leetcode/restore-ip-addresses.cpp
@@ -5,40 +5,64 @@
 
 using namespace std;
 
-
-// NOT FINISHED
+// SLOW
+// Some ideas are based on https://leetcode.com/problems/restore-ip-addresses/solutions/2868540/restore-ip-addresses/comments/1767097
 class Solution {
 public:
     vector<string> restoreIpAddresses(string s) {
-       vector<string> ans; 
+       vector<string> ans;
+       vector<string> octets;
+       Backtrack(s, ans, octets, 0);
+       return ans;
     }
 private:
-    bool IsValid (string s) {
-        if (s[0] == '0') {
-            if (s.size() == 1) {
+    bool IsValidOctet (string octet) {
+        if (octet[0] == '0') {
+            if (octet.size() == 1) {
                 return true;
             }
             return false;
-        int octet = stoi(s);
-        if (octet < 256) {
-            return true;
         }
-        return false;
+        return stoi(octet) <= 255;
     }
-    void Backtrack (const string& s, vector<string>& ans, string current, string last, int dots, int idx) {
-        if (current.size() - 3 == s.size()) {
-            if (IsValid(last)) {
-                ans.emplace(current);
+
+    void Backtrack (const string& s, vector<string>& ans, vector<string>& octets, int idx) {
+        if (octets.size() == 4 && idx == s.size()) {
+            string ip;
+            for (const string& octet: octets) {
+                ip += octet;
+                ip += '.';
             }
+            ip.pop_back();
+            ans.emplace_back(ip);
             return;
         }
-        if (last.size() < 3) {
-            last += s[idx];
-            backtrack(s, ans, current, last, idx + 1);
-            last.pop_back();
+
+        for (int len = 1; len < 4 && idx + len <= s.size(); ++len) {
+            string octet = s.substr(idx, len);
+            if (!IsValidOctet(octet)) {
+                break;
+            }
+            if (octets.size() < 4) {
+                octets.emplace_back(octet);
+                Backtrack (s, ans, octets, idx + len);
+                octets.pop_back();
+            }
         }
-        else if (last.size() == 3) {
-            current
-        }
-    }
+   }
 };
+
+int main() {
+
+    string str;
+    cin >> str;
+
+    Solution sol;
+    vector<string> output = sol.restoreIpAddresses(str);
+
+    for (const string& o: output) {
+        cout << o << endl;
+    }
+
+    return 0;
+}
