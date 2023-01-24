8b72cedefa13b56eeaeee6977d40f06d1f112bc2
sgrade <sgrade@users.noreply.github.com>
2023-01-24 16:49:05 +0000

    leetcode

--- leetcode/h-index-ii.cpp
+++ leetcode/h-index-ii.cpp
@@ -6,22 +6,24 @@
 using namespace std;
 
 
+// The optmimizations are based on one of the Leetcode's accepted solutions
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
         int n = citations.size();
         int left = 0, right = citations.size() - 1, mid;
-        int ans = 0;
         while (left <= right) {
             mid = left + (right - left) / 2;
-            if (citations[mid] >= n - mid) {
-                ans = max(ans, n - mid);
+            if (citations[mid] == n - mid) {
+                return citations[mid];
+            }
+            else if (citations[mid] > n - mid) {
                 right = mid - 1;
             }
             else {
                 left = mid + 1;
             }
         }
-        return ans;
+        return n - left;
     }
 };
