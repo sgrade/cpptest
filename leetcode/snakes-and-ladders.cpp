92c1ca2cbfc2efa7b0354a33a8e7213664d9d0ae
sgrade <sgrade@users.noreply.github.com>
2023-01-25 17:37:21 +0000

    leetcode

--- leetcode/snakes-and-ladders.cpp
+++ leetcode/snakes-and-ladders.cpp
@@ -15,16 +15,32 @@ public:
         cols = board[0].size();
         max_idx = rows * rows;
 
+        vector<pair<int, int>> indexes(max_idx + 1);
+        int idx = 1;
+        for (int row = rows - 1; row >= 0; --row) {
+            if (row % 2 != rows % 2) {  // left to right
+                for (int col = 0; col < cols; ++col) {
+                    indexes[idx] = {row, col};
+                    ++idx;
+                }
+            }
+            else {                      // right to left
+                for (int col = cols - 1; col >= 0; --col) {
+                    indexes[idx] = {row, col};
+                    ++idx;
+                }
+            }
+        }
+
         vector<int> dist(max_idx + 1, -1);  // distance from cell 1 to a cell with index i
         dist[1] = 0;
-        vector<bool> dont_come_back(max_idx + 1);
 
         int current_cell_idx = 1;
         q.emplace(current_cell_idx);
         while (!q.empty()) {
             current_cell_idx = q.front();
             q.pop();
-            move (board, dist, dont_come_back, current_cell_idx);
+            move (board, dist, indexes, current_cell_idx);
         }
 
         return dist[rows * rows];
@@ -33,27 +49,14 @@ public:
 private:
     int rows, cols, max_idx;
     queue<int> q;
-    void move (const vector<vector<int>>& board, vector<int>& dist, vector<bool>& dont_come_back, int& current_cell_idx) {
+    void move (const vector<vector<int>>& board, vector<int>& dist, vector<pair<int, int>>& indexes, int& current_cell_idx) {
         for (int i = current_cell_idx + 1; i < min(current_cell_idx + 7, max_idx + 1); ++i) {
-            auto [row, col] = getRowCol(i);
+            auto [row, col] = indexes[i];
             int next_cell_idx = board[row][col] != -1 ? board[row][col] : i;
             if (dist[next_cell_idx] == -1) {
                 dist[next_cell_idx] = dist[current_cell_idx] + 1;
                 q.emplace(next_cell_idx);
             }
         }
-    }
-
-    pair<int, int> getRowCol (int& current_cell_idx) {
-        int rem = current_cell_idx % rows;
-        int row = rows - (current_cell_idx - 1) / rows - 1;
-        int col;
-        if (row % 2 != rows % 2) {  // left to right
-            col = rem == 0 ? cols - 1 : rem - 1;
-        }
-        else {                      // right to left
-            col = rem == 0 ? 0 : cols - rem;
-        }
-        return pair<int, int>(row, col);
     }
 };
