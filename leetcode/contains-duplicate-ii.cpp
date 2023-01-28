339c9ffdf2d341afd445b8e9a0fe0dc5e5f0b7e0
sgrade <sgrade@users.noreply.github.com>
2023-01-28 11:59:57 +0000

    leetcode

--- leetcode/contains-duplicate-ii.cpp
+++ leetcode/contains-duplicate-ii.cpp
@@ -9,19 +9,14 @@ using namespace std;
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        ios::sync_with_stdio(false); cin.tie(0);
         unordered_map<int, int> indexes;
         for (int i = 0; i < nums.size(); ++i) {
             if (indexes.find(nums[i]) != indexes.end()) {
-                if (i - indexes[nums[i]] <= k) {
-                    return true;
-                }
-                else {
-                    indexes[nums[i]] = i;
-                }
+                if (i - indexes[nums[i]] <= k) return true;
+                else indexes[nums[i]] = i;
             }
-            else {
-                indexes[nums[i]] = i;
-            }
+            else indexes[nums[i]] = i;
         }
         return false;
     }
