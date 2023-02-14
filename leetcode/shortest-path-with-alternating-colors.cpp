e1aa0bd74ff089377ce6fac2f266478c5e60e946
sgrade <sgrade@users.noreply.github.com>
2023-02-14 19:08:21 +0000

    leetcode

--- leetcode/shortest-path-with-alternating-colors.cpp
+++ leetcode/shortest-path-with-alternating-colors.cpp
@@ -7,29 +7,38 @@ using namespace std;
 
 
 // Based on Leetcode's Shortest Path with Alternating Colors
-// NOT FINISHED
 class Solution {
 public:
     vector<int> shortestAlternatingPaths(int n, vector<vector<int>>& redEdges, vector<vector<int>>& blueEdges) {
         // pair.first = source node; pair.second = color (0 - red, 1 - blue)
         vector<vector<pair<int, int>>> adj(n);
-        for (const vector<int>& v: redEdges) adj[redEdges[0]].emplace_back(redEdges[1], 0);
-        for (const vector<int>& v: blueEdges) adj[blueEdges[0]].emplace_back(blueEdges[1], 1);
+        for (vector<int>& v: redEdges) 
+            adj[v[0]].emplace_back(pair<int, int>(v[1], 0));
+        for (vector<int>& v: blueEdges) 
+            adj[v[0]].emplace_back(pair<int, int>(v[1], 1));
 
         vector<int> ans(n, -1);
-        vector<pair<bool, bool>> visited(n, pair<bool, bool>(false, false));
+        vector<vector<bool>> visited(n, vector<bool>(2));
         
         // v[0] = node, v[1] = number of steps, v[2] = color (see above)
         queue<vector<int>> q;
-        q.emplace({0, 0, -1});
+        q.emplace(vector<int>{0, 0, -1});
         visited[0][0] = visited[0][1] = true;
         ans[0] = 0;
 
         while (!q.empty()) {
-            auto[node, element, prev_color]& = q.front();
+            int node = q.front()[0], steps = q.front()[1], prev_color = q.front()[2];
             q.pop();
 
-            for (const auto)
+            for (auto& [neighbor, color] : adj[node]) {
+                if (!visited[neighbor][color] && color != prev_color) {
+                    visited[neighbor][color] = true;
+                    q.emplace(vector<int>{neighbor, steps + 1, color});
+                    if (ans[neighbor] == -1) ans[neighbor] = steps + 1;
+                }
+            }
         }
+
+        return ans;
     }
 };
