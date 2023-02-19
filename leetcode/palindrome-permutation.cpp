88b24e086a6326f02a4b61c0f67f384bfa304340
sgrade <sgrade@users.noreply.github.com>
2023-02-19 22:00:16 +0000

    leetcode

---
+++ leetcode/palindrome-permutation.cpp
@@ -0,0 +1,26 @@
+// 266. Palindrome Permutation
+// https://leetcode.com/problems/palindrome-permutation/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// SLOW
+class Solution {
+public:
+    bool canPermutePalindrome(string s) {
+        vector<char> counter(26);
+        for (const char& ch: s) ++counter[ch - 'a'];
+        int odd = 0;
+        for (int i = 0; i < 26; ++i) {
+            if (counter[i] != 0) {
+                if (counter[i] % 2 != 0) {
+                    ++odd;
+                    if (odd > 1) return false;
+                }
+            }
+        }
+        return true;
+    }
+};
