9d992489b5b5a72ea3fe2669a31665d8bdf3c519
sgrade <sgrade@users.noreply.github.com>
2023-03-02 19:57:49 +0000

    leetcode

--- leetcode/string-compression.cpp
+++ leetcode/string-compression.cpp
@@ -9,29 +9,23 @@ using namespace std;
 class Solution {
 public:
     int compress(vector<char>& chars) {
-        prev = chars[0], len = 0, idx = 0;
-        for (int i = 0; i < chars.size(); ++i) {
-            if (chars[i] == prev)
-                ++len;
-            else {
-                append(chars);
-                len = 1;
-                prev = chars[i];
+        int len = 0, idx = 0;
+        size_t i = 0, j, n = chars.size();
+        while (i < n) {
+            j = i + 1;
+            while (j < n && chars[j] == chars[i]) 
+                ++j;
+            
+            chars[idx++] = chars[i];
+            len = j - i;
+            if (len > 1) {
+                string len_s = to_string(len);
+                for (const char& ch: len_s)
+                    chars[idx++] = ch;
             }
+
+            i = j;
         }
-        append(chars);
         return idx;
-    }
-private:
-    char prev;
-    int len, idx;
-    string len_s;
-    void append(vector<char>& chars) {
-        chars[idx++] = prev;
-        if (len == 1) return;
-        len_s = to_string(len);
-        for (const char& ch: len_s) {
-            chars[idx++] = ch;
-        }
     }
 };
