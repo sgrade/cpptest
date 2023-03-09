5cd9db747d36a935b2c558b2059a202f0446eb17
sgrade <sgrade@users.noreply.github.com>
2023-03-09 20:31:19 +0000

    leetcode

--- leetcode/linked-list-cycle-ii.cpp
+++ leetcode/linked-list-cycle-ii.cpp
@@ -6,8 +6,6 @@
 using namespace std;
 
 
-// Based on Leetcode's Approach 2: Floyd's Tortoise and Hare
-
 // Definition for singly-linked list.
 struct ListNode {
     int val;
@@ -15,42 +13,19 @@ struct ListNode {
     ListNode(int x) : val(x), next(NULL) {}
 };
 
+
+// SLOW
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        if (head == nullptr) {
-            return nullptr;
+        while (head) {
+            if (nodes.find(head) != nodes.end())
+                return head;
+            nodes.emplace(head);
+            head = head->next;
         }
-        
-        ListNode* intersect = GetIntersect(head);
-        if (intersect == nullptr) {
-            return nullptr;
-        }
-        
-        ListNode* ptr1 = head;
-        ListNode* ptr2 = intersect;
-        
-        while (ptr1 != ptr2) {
-            ptr1 = ptr1->next;
-            ptr2 = ptr2->next;
-        }
-        
-        return ptr1;
-    }
-
-private:
-    ListNode* GetIntersect(ListNode* head) {
-        ListNode* hare = head;
-        ListNode* tortoise = head;
-        
-        while (hare && hare->next) {
-            tortoise = tortoise->next;
-            hare = hare->next->next;
-            if (tortoise == hare) {
-                return tortoise;
-            }
-        }
-        
         return nullptr;
     }
+private:
+    unordered_set<ListNode*> nodes;
 };
