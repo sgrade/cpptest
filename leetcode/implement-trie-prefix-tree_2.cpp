c1448d428f9f278837bc7ebc30325f45adaebe87
sgrade <sgrade@users.noreply.github.com>
2023-03-17 21:02:04 +0000

    leetcode

--- leetcode/implement-trie-prefix-tree_2.cpp
+++ leetcode/implement-trie-prefix-tree_2.cpp
@@ -5,21 +5,25 @@
 
 using namespace std;
 
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
 
 // NOT FINISHED
 class TrieNode {
 public:
-    TreeNode(){
-        this->is_end = false;
-        children.resize(26, nullptr);
-    }
+    TrieNode() : children(26, nullptr) {}
     bool ContainsKey(char ch) {
         return children[ch - 'a'];
     }
     TrieNode* Get(char ch) {
         return children[ch - 'a'];
     }
-    void Put(char ch, TrieNode*) {
+    void Put(char ch, TrieNode* node) {
         children[ch - 'a'] = node;
     }
     void SetEnd() {
@@ -29,39 +33,35 @@ public:
         return is_end;
     }
 private:
-    vector<TrieNode*> children;
-    bool is_end = false;
+    vector<TrieNode*> children(26, nullptr);
+    bool is_end;
 };
 
 
 class Trie {
-public:
-    Trie() {
-        
-    }
-    
+public:    
     void insert(string word) {
         TrieNode* node = root;
         for (size_t i = 0; i < word.size(); ++i) {
-            if (!node.ContainsKey(word[i])) {
-                node.Put(word[i], new TrieNode());
+            if (!node->ContainsKey(word[i])) {
+                node->Put(word[i], new TrieNode());
             }
-            node = node.Get(word[i]);
+            node = node->Get(word[i]);
         }
-        node.SetEnd();
+        node->SetEnd();
     }
     
     bool search(string word) {
         TrieNode* node = root;
         for (size_t i = 0; i < word.size(); ++i) {
-            if (node.ContainsKey(word[i])) {
-                node = node.Get(word[i]);
+            if (node->ContainsKey(word[i])) {
+                node = node->Get(word[i]);
             }
             else {
                 return false;
             }
         }
-        return node.IsEnd();
+        return node->IsEnd();
     }
     
     bool startsWith(string prefix) {
