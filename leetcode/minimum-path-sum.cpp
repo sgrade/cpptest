9478d808013ecaf5660ee3568112cb696a170a9e
sgrade <sgrade@users.noreply.github.com>
2023-03-27 05:53:30 +0000

    leetcode

--- leetcode/minimum-path-sum.cpp
+++ leetcode/minimum-path-sum.cpp
@@ -9,19 +9,14 @@ using namespace std;
 class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
-        int rows = grid.size();
-        int cols = grid[0].size();
-        for (int col = 1; col < cols; ++col) {
-            grid[0][col] = grid[0][col - 1] + grid[0][col];
-        }
-        for (int row = 1; row < rows; ++row) {
-            grid[row][0] = grid[row - 1][0] + grid[row][0];
-        }
-        for (int row = 1; row < rows; ++row) {
-            for (int col = 1; col < cols; ++col) {
-                grid[row][col] = grid[row][col] + min(grid[row-1][col], grid[row][col - 1]);
-            }
-        }
+        size_t rows = grid.size(), cols = grid[0].size();
+        for (size_t col = 1; col < cols; ++col) 
+            grid[0][col] += grid[0][col - 1];
+        for (size_t row = 1; row < rows; ++row)
+            grid[row][0] += grid[row - 1][0];
+        for (size_t row = 1; row < rows; ++row)
+            for (size_t col = 1; col < cols; ++col)
+                grid[row][col] += min(grid[row - 1][col], grid[row][col - 1]);
         return grid[rows - 1][cols - 1];
     }
 };
