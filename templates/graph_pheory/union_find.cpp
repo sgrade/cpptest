5b1d015ed61ee579098133f6565c4e3594eec0f1
sgrade <sgrade@users.noreply.github.com>
2023-03-31 12:54:09 +0000

    templates

--- templates/graph_pheory/union_find.cpp
+++ templates/graph_pheory/union_find.cpp
@@ -6,29 +6,29 @@ using namespace std;
 class UnionFind {
 public:
     UnionFind (int size): parent(size), rank(size) {
-        iota (parent.begin(), parent.end(), 0);
+        iota(parent.begin(), parent.end(), 0);
     }
     int Find (int x) {
-        if (parent[x] != x)
+        if (x != parent[x])
             parent[x] = Find(parent[x]);
         return parent[x];
     }
     void UnionSet (int x, int y) {
-        int xset = Find(x), yset = Find(y);
-        if (xset == yset)
+        int root_x = Find(x); 
+        int root_y = Find(y);
+        if (root_x == root_y)
             return;
-        else if (rank[xset] < rank[yset])
-            parent[xset] = yset;
-        else if (rank[xset] > rank[yset])
-            parent[yset] = xset;
+        if (rank[root_x] < rank[root_y])
+            parent[root_x] = root_y;
+        else if (rank[root_y] < rank[root_x])
+            parent[root_y] = root_x;
         else {
-            parent[yset] = xset;
-            ++rank[xset];
+            parent[root_x] = root_y;
+            ++rank[root_y];
         }
     }
     bool Connected (int x, int y) {
         return Find(x) == Find(y);
     }
-private:
     vector<int> parent, rank;
 };
