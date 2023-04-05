dc20ce3c8374cd681b93999f4435ca11885e580d
sgrade <sgrade@users.noreply.github.com>
2023-04-05 15:16:46 +0000

    leetcode

--- leetcode/best-time-to-buy-and-sell-stock.cpp
+++ leetcode/best-time-to-buy-and-sell-stock.cpp
@@ -9,8 +9,9 @@ using namespace std;
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int ans = 0;
-        int mn = prices[0];
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        int ans = 0, mn = prices[0];
         for (int i = 1; i < prices.size(); ++i) {
             ans = max(ans, prices[i] - mn);
             mn = min(mn, prices[i]);
