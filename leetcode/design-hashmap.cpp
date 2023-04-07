f78af9579a5eedd89008714f3d53ff35ecbbd295
sgrade <sgrade@users.noreply.github.com>
2023-04-07 11:06:12 +0000

    leetcode

--- leetcode/design-hashmap.cpp
+++ leetcode/design-hashmap.cpp
@@ -6,26 +6,45 @@
 using namespace std;
 
 
+// Combined ideas from the Leetcode's Editorial and Sample 90 ms submission
 class MyHashMap {
 public:
     MyHashMap() {
-        storage.resize(1000001, -1);
+        storage.resize(1024);
     }
     
     void put(int key, int value) {
-        storage[key] = value;
+        int bucket = key % (sizeof(storage) / sizeof(storage[0]));
+        for (auto& [pkey, pvalue]: storage[bucket])
+            if (pkey == key) {
+                pvalue = value;
+                return;
+            }
+        storage[bucket].emplace_back(pair<int, int>(key, value));
     }
     
     int get(int key) {
-        return storage[key];
+        int bucket = key % (sizeof(storage) / sizeof(storage[0]));
+        for (auto& [pkey, pvalue]: storage[bucket])
+            if (pkey == key)
+                return pvalue;
+        return -1;
     }
     
     void remove(int key) {
-        storage[key] = -1;
+        int bucket = key % (sizeof(storage) / sizeof(storage[0]));
+        vector<pair<int, int>>::iterator it = storage[bucket].begin();
+        while (it != storage[bucket].end()) {
+            if ((*it).first == key) {
+                storage[bucket].erase(it);
+                return;
+            }
+            ++it;
+        }
     }
 
 private:
-    vector<int> storage;
+    vector<vector<pair<int, int>>> storage;
 };
 
 /**
