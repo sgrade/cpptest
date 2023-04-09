aee7722e8b8ac5fc30d70cd1319298d2ecd814cd
sgrade <sgrade@users.noreply.github.com>
2023-04-09 19:01:28 +0000

    leetcode

---
+++ leetcode/design-compressed-string-iterator.cpp
@@ -0,0 +1,45 @@
+// 604. Design Compressed String Iterator
+// https://leetcode.com/problems/design-compressed-string-iterator/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+class StringIterator {
+public:
+    StringIterator(string compressedString) {
+        s = compressedString;
+    }
+    
+    char next() {
+        if (cnt > 0) {
+            --cnt;
+            return s[idx];
+        }
+        if  (next_idx == s.size())
+            return ' ';
+        cnt = 0;
+        idx = next_idx;
+        ++next_idx;
+        while (next_idx < s.size() && isdigit(s[next_idx]))
+            cnt = cnt * 10 + (s[next_idx++] - '0');
+        --cnt;
+        return s[idx];
+    }
+    
+    bool hasNext() {
+        return cnt > 0 || next_idx < s.size();
+    }
+
+private:
+    string s;
+    int idx = 0, cnt = 0, next_idx = 0;
+};
+
+/**
+ * Your StringIterator object will be instantiated and called as such:
+ * StringIterator* obj = new StringIterator(compressedString);
+ * char param_1 = obj->next();
+ * bool param_2 = obj->hasNext();
+ */
