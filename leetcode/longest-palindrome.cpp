8b0003963602147196b3e155c8fd97d26e7d9693
sgrade <sgrade@users.noreply.github.com>
2023-04-15 20:10:14 +0000

    leetcode

--- leetcode/longest-palindrome.cpp
+++ leetcode/longest-palindrome.cpp
@@ -1,2 +1,31 @@
 // 409. Longest Palindrome
 // https://leetcode.com/problems/longest-palindrome/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        for (const char& ch: s)
+            ++counter[ch];
+        int ans = 0, odd = 0;
+        for (const auto& [_, cnt]: counter) {
+            if (cnt % 2 == 0) {
+                ans += cnt;
+            }
+            else {
+                ans += cnt / 2 * 2;
+                ++odd;
+            }
+        }
+        if (odd > 0) 
+            ++ans;
+        return ans;
+    }
+
+private:
+    map<char, int> counter;
+};
