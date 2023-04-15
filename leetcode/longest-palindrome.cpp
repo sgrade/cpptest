708461cd0bdedabbfb89200dffc471e5c72e5a44
sgrade <sgrade@users.noreply.github.com>
2023-04-15 20:19:04 +0000

    leetcode

--- leetcode/longest-palindrome.cpp
+++ leetcode/longest-palindrome.cpp
@@ -9,13 +9,13 @@ using namespace std;
 class Solution {
 public:
     int longestPalindrome(string s) {
+        vector<int> counter(123);
         for (const char& ch: s)
             ++counter[ch];
         int ans = 0, odd = 0;
-        for (const auto& [_, cnt]: counter) {
-            if (cnt % 2 == 0) {
+        for (const int& cnt: counter) {
+            if (cnt % 2 == 0)
                 ans += cnt;
-            }
             else {
                 ans += cnt / 2 * 2;
                 ++odd;
@@ -25,7 +25,4 @@ public:
             ++ans;
         return ans;
     }
-
-private:
-    map<char, int> counter;
 };
