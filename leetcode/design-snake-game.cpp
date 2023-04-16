12ac2cda168d603b8aec5735578a76674a3b0e97
sgrade <sgrade@users.noreply.github.com>
2023-04-16 21:00:05 +0000

    leetcode

--- leetcode/design-snake-game.cpp
+++ leetcode/design-snake-game.cpp
@@ -6,23 +6,19 @@
 using namespace std;
 
 
-// Time Limit Exceeded
-// 79 / 446 testcases passed
 class SnakeGame {
 public:
     SnakeGame(int width, int height, vector<vector<int>>& food) {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
         rows = height;
         cols = width;
-        snake.resize(height, vector<string>(width));
-        fd = food;
-        f_row = food[0][0];
-        f_col = food[0][1];
+        for (int i = 0; i < food.size(); ++i)
+            this->food.emplace(food[i]);
+        body.emplace_front(0, 0);
     }
     
     int move(string direction) {
-        // Direction from the old head to new head (to be used later for tail moves)
-        snake[s_head_row][s_head_col] = direction;
-
         // Move head
         s_head_row += directions[direction].first;
         s_head_col += directions[direction].second;
@@ -31,40 +27,33 @@ public:
         if (s_head_row < 0 || s_head_row == rows || s_head_col < 0 || s_head_col == cols)
             return -1;
         
-        // Head eats food
-        // f_row = -1 when there is no more food
-        if (f_row != - 1 && s_head_row == f_row && s_head_col == f_col) {
+        pair<int, int> new_head(s_head_row, s_head_col);
+        
+        // Snake eats food
+        if (!food.empty() && s_head_row == food.front()[0] && s_head_col == food.front()[1]) {
             ans += 1;
-            if (++f_idx < fd.size()) {
-                f_row = fd[f_idx][0];
-                f_col = fd[f_idx][1];
-                return ans;
-            }
-            else 
-                f_row = -1;
+            food.pop();
+            body.emplace_front(new_head);
+            return ans;
         }
+         
+        body.pop_back();        
 
-        // Tail moves
-        string s_tail_direction = snake[s_tail_row][s_tail_col];
-        snake[s_tail_row][s_tail_col] = "";
-        s_tail_row += directions[s_tail_direction].first;
-        s_tail_col += directions[s_tail_direction].second;
-
-        // Is head on a cell occupied by snake's body?
-        if (!snake[s_head_row][s_head_col].empty())
+        // Snake eats itself
+        if (find(body.begin(), body.end(), new_head) != body.end())
             return -1;
         
+        body.emplace_front(new_head);
+
         return ans;
     }
 
 private:
     int ans = 0;
     int cols, rows;
-    vector<vector<int>> fd;
-    vector<vector<string>> snake;
-    int s_head_row = 0, s_head_col = 0, s_tail_row = 0, s_tail_col = 0;
-    int f_idx = 0;
-    int f_row, f_col;
+    queue<vector<int>> food;
+    deque<pair<int, int>> body;
+    int s_head_row = 0, s_head_col = 0;
     unordered_map<string, pair<int, int>> directions {
         {"U", {-1, 0}},
         {"D", {1, 0}},
