481dded611b8d6db73fe109f2ed5685448291c1b
sgrade <sgrade@users.noreply.github.com>
2023-04-27 17:53:11 +0000

    leetcode

---
+++ leetcode/bulb-switcher.cpp
@@ -0,0 +1,16 @@
+// 319. Bulb Switcher
+// https://leetcode.com/problems/bulb-switcher/
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// Based on Editorial's Approach 1: Math
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        return sqrt(n);
+    }
+};
