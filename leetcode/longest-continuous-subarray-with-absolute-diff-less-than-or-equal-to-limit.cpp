1384e976ff1c144c49ae6a23357ac8814a03d20d
sgrade <sgrade@users.noreply.github.com>
2023-04-29 16:38:56 +0000

    leetcode

--- leetcode/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
+++ leetcode/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -6,24 +6,33 @@
 using namespace std;
 
 
+// Key idea is based on a Leetcode's Sample 87 ms submission
 class Solution {
 public:
-    // Some ideas are from https://leetcode.com/problems/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/discuss/609969/C%2B%2B-solution-with-detailed-explanation
     int longestSubarray(vector<int>& nums, int limit) {
-        if (nums.size() == 0) return 0;
-        int n = nums.size();
-        int ans = 1;
-        int left = 0, right = 0;
-        multiset<int> sorted;
-        for (; right < n; ++right) {
-            sorted.emplace(nums[right]);
-            while (!sorted.empty() && *sorted.rbegin() - *sorted.begin() > limit) {
-                multiset<int>::iterator nxt = sorted.find(nums[left]);
-                sorted.erase(nxt);
+        int ans = 0;
+        int left = 0;
+        for (int right = 0; right < nums.size(); ++right) {
+            while (!minq.empty() && minq.back() > nums[right])
+                minq.pop_back();
+            minq.emplace_back(nums[right]);
+            while (!maxq.empty() && maxq.back() < nums[right])
+                maxq.pop_back();
+            maxq.emplace_back(nums[right]);
+            if (maxq.front() - minq.front() <= limit) {
+                ans = max(ans, right - left + 1);
+            }
+            else {
+                if (minq.front() == nums[left])
+                    minq.pop_front();
+                if (maxq.front() == nums[left])
+                    maxq.pop_front();
                 ++left;
             }
-            ans = max(ans, right - left + 1);
         }
         return ans;
     }
+
+private:
+    deque<int> minq, maxq;
 };
