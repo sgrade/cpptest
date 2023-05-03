40e40ef83ad8a1a93bcf12f728fafa4091c48aac
sgrade <sgrade@users.noreply.github.com>
2023-05-03 19:33:36 +0000

    leetcode

--- leetcode/roman-to-integer.cpp
+++ leetcode/roman-to-integer.cpp
@@ -6,33 +6,22 @@
 using namespace std;
 
 
-// Based on Editorial's Approach 3: Right-to-Left Pass
 class Solution {
 public:
     int romanToInt(string s) {
-        ios::sync_with_stdio(false);
-        cin.tie(nullptr);
-        int ans = 0;
+        int current, prev = 0, ans = 0;
         for (int i = s.size() - 1; i >= 0; --i) {
-            current = chars[s[i]];
-            if (current < prev)
-                ans -= current;
-            else 
-                ans += current;
+            if (s[i] == 'M') current = 1000;
+            else if (s[i] == 'D') current = 500;
+            else if (s[i] == 'C') current = 100;
+            else if (s[i] == 'L') current = 50;
+            else if (s[i] == 'X') current = 10;
+            else if (s[i] == 'V') current = 5;
+            else current = 1;
+            if (current < prev) ans -= current;
+            else ans += current;
             prev = current;
         }
         return ans;
     }
-
-private:
-    int current, prev = 0;
-    unordered_map<char, int> chars {
-        {'M', 1000},
-        {'D', 500},
-        {'C', 100},
-        {'L', 50},
-        {'X', 10},
-        {'V', 5},
-        {'I', 1}
-    };
 };
