50bd041f949fa96573594290ab4205bca62370f4
sgrade <sgrade@users.noreply.github.com>
2023-05-05 21:30:53 +0000

    leetcode

--- leetcode/stock-price-fluctuation.cpp
+++ leetcode/stock-price-fluctuation.cpp
@@ -6,21 +6,19 @@
 using namespace std;
 
 
+// Based on Editorial's Approach 2: Hashmap and Heaps
 class StockPrice {
 public:
     StockPrice() {}
     
     void update(int timestamp, int price) {
-        auto it = ts_to_price.find(timestamp);
-        if (it != ts_to_price.end()) {
-            price_to_ts.erase(pair<int, int>(it->second, it->first));
-        }
-        price_to_ts.emplace(price, timestamp);
         ts_to_price[timestamp] = price;
         if (timestamp >= current_timestamp) {
             current_price = price;
             current_timestamp = timestamp;
         }
+        max_heap.emplace(price, timestamp);
+        min_heap.emplace(price, timestamp);
     }
     
     int current() {
@@ -28,16 +26,28 @@ public:
     }
     
     int maximum() {
-        return price_to_ts.rbegin()->first;
+        auto& [price, timestamp] = max_heap.top();
+        while (ts_to_price[timestamp] != price) {
+            max_heap.pop();
+            auto& [price, timestamp] = max_heap.top();
+        }
+        return price;
     }
     
     int minimum() {
-        return price_to_ts.begin()->first;
+        auto& [price, timestamp] = min_heap.top();
+        while (ts_to_price[timestamp] != price) {
+            min_heap.pop();
+            auto& [price, timestamp] = min_heap.top();
+        }
+        return price;
     }
 
 private:
     unordered_map<int, int> ts_to_price;
-    set<pair<int, int>> price_to_ts;
+    // pair<timestamp, price>
+    priority_queue<pair<int, int>> max_heap;
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> min_heap;
     int current_price = numeric_limits<int>::max();
     int current_timestamp = 0;
 };
