59f1e08af75d7df88904d8928901295be7fe4bf5
sgrade <sgrade@users.noreply.github.com>
2023-05-08 19:58:02 +0000

    leetcode

--- leetcode/minesweeper_dfs.cpp
+++ leetcode/minesweeper_dfs.cpp
@@ -6,59 +6,50 @@
 using namespace std;
 
 
+// Modified with ideas from Leetcode's Sample 7 ms submission
 class Solution {
 public:
     vector<vector<char>> updateBoard(vector<vector<char>>& board, vector<int>& click) {
+        if (board[click[0]][click[1]] == 'M') {
+            board[click[0]][click[1]] = 'X';
+            return board;
+        }
         rows = board.size();
         cols = board[0].size();
-        visited.resize(rows, vector<bool>(cols));
-
         dfs(board, click[0], click[1]);
         return board;
     }
 
 private:
     int rows, cols;
+    vector<pair<int, int>> directions {
+        {-1, 0}, {1, 0}, {0, -1}, {0, 1}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1}
+    };
     
-    // 0 - not visited, 1 - visited, 2 - mine
-    vector<vector<bool>> visited;
-
     void dfs(vector<vector<char>>& board, int row, int col) {
-        if (row < 0 || row == rows || col < 0 || col == cols || visited[row][col]) 
+        if (row < 0 || row == rows || col < 0 || col == cols || board[row][col] != 'E') 
             return;
-
-        visited[row][col] = true;
-
-        if (board[row][col] == 'M') {
-            board[row][col] = 'X';
-            return;
-        }
-
-        int adjacent_mines = 0;
-        adjacent_mines += (row - 1 >= 0) && board[row - 1][col] == 'M';
-        adjacent_mines += (row + 1 < rows) && board[row + 1][col] == 'M';
-        adjacent_mines += (col - 1 >= 0) && board[row][col - 1] == 'M';
-        adjacent_mines += (col + 1 < cols) && board[row][col + 1] == 'M';
-        adjacent_mines += row - 1 >= 0 && col - 1 >= 0 && board[row - 1][col - 1] == 'M';
-        adjacent_mines += row - 1 >= 0 && col + 1 < cols && board[row - 1][col + 1] == 'M';
-        adjacent_mines += row + 1 < rows && col - 1 >= 0 &&  board[row + 1][col - 1] == 'M';
-        adjacent_mines += row + 1 < rows && col + 1 < cols && board[row + 1][col + 1] == 'M';
-
+        
+        int adjacent_mines = count_mines(board, row, col);
         if (adjacent_mines != 0) {
             board[row][col] = adjacent_mines + '0';
             return;
         }
 
-        dfs(board, row - 1, col);
-        dfs(board, row + 1, col);
-        dfs(board, row, col - 1);
-        dfs(board, row, col + 1);
-        dfs(board, row - 1, col - 1);
-        dfs(board, row + 1, col - 1);
-        dfs(board, row - 1, col + 1);
-        dfs(board, row + 1, col + 1);
         board[row][col] = 'B';
+        for (const auto& [r, c]: directions)
+            dfs(board, row + r, col + c);
+    }
 
-        return;
+    int count_mines (vector<vector<char>>& board, const int row, const int col) {
+        int adjacent_mines = 0;
+        for (const auto& [r, c]: directions) {
+            int rr = row + r;
+            int cc = col + c;
+            if (rr < 0 || rr == rows || cc < 0 || cc == cols) 
+                continue;
+            adjacent_mines += board[rr][cc] == 'M';
+        }
+        return adjacent_mines;
     }
 };
