b64e777f24969efbb4a2e563300b8b4a89dd4efa
sgrade <sgrade@users.noreply.github.com>
2023-05-14 19:09:54 +0000

    leetcode

--- leetcode/sparse-matrix-multiplication.cpp
+++ leetcode/sparse-matrix-multiplication.cpp
@@ -9,12 +9,14 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int>> multiply(vector<vector<int>>& mat1, vector<vector<int>>& mat2) {
-        int m = mat1.size(), k = mat1[0].size(), n = mat2[0].size();
-        vector<vector<int>> ans(m, vector<int>(n));
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                for (int kk = 0; kk < k; ++kk) {
-                    ans[i][j] += mat1[i][kk] * mat2[kk][j];
+        int rows = mat1.size(), kk = mat1[0].size(), cols = mat2[0].size();
+        vector<vector<int>> ans(rows, vector<int>(cols));
+        for (int row = 0; row < rows; ++row) {
+            for (int k = 0; k < kk; ++k) {
+                if (mat1[row][k] != 0) {
+                    for (int col = 0; col < cols; ++col) {
+                        ans[row][col] += mat1[row][k] * mat2[k][col];
+                    }
                 }
             }
         }
