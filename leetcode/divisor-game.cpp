9f58f613eed108ba641e08fbba371db572955146
sgrade <sgrade@users.noreply.github.com>
2023-06-02 19:25:28 +0000

    leetcode

---
+++ leetcode/divisor-game.cpp
@@ -0,0 +1,14 @@
+// 1025. Divisor Game
+// https://leetcode.com/problems/divisor-game/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+class Solution {
+public:
+    bool divisorGame(int n) {
+        return n % 2 == 0;
+    }
+};
