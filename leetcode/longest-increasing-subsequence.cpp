516de2f71d0151208cd649073d4b9d37697cc3b5
sgrade <sgrade@users.noreply.github.com>
2023-06-11 16:27:16 +0000

    leetcode

--- leetcode/longest-increasing-subsequence.cpp
+++ leetcode/longest-increasing-subsequence.cpp
@@ -9,16 +9,15 @@ using namespace std;
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
         int n = nums.size();
         vector<int> dp(n, 1);
-        for (int i = 1; i < n; ++i) {
+        for (int i = 1; i < n; ++i)
             for (int j = 0; j < i; ++j) {
-                if (nums[i] > nums[j]) {
+                if (nums[i] > nums[j])
                     dp[i] = max(dp[i], dp[j] + 1);
-                }
             }
-        }
-        int ans = *max_element(dp.begin(), dp.end());
-        return ans;
+        return *max_element(dp.begin(), dp.end());
     }
 };
