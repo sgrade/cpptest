20766f20e76c2ed562d8bce041b9ae8736db68b7
sgrade <sgrade@users.noreply.github.com>
2023-06-27 21:16:55 +0000

    leetcode

--- leetcode/find-k-pairs-with-smallest-sums.cpp
+++ leetcode/find-k-pairs-with-smallest-sums.cpp
@@ -12,25 +12,27 @@ public:
     vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
         ios::sync_with_stdio(false);
         cin.tie(nullptr);
-        auto comp = [](vector<int>& v1, vector<int>& v2) {
-            return v1[0] + v1[1] < v2[0] + v2[1];
+        auto comp = [](pair<int, vector<int>>& p1, pair<int, vector<int>>& p2) {
+            return p1.first < p2.first;
         };
-        priority_queue< vector <int>, vector<vector<int>>, decltype(comp) > pq(comp);
+        priority_queue< pair<int, vector<int>>, vector<pair<int, vector<int>>>, decltype(comp) > pq(comp);
         
+        int cur_sum;
         for (int i = 0; i < nums1.size(); ++i) {
             for (int j = 0; j < nums2.size(); ++j) {
+                cur_sum = nums1[i] + nums2[j];
                 if (pq.size() == k) {
-                    if (nums1[i] + nums2[j] > pq.top()[0] + pq.top()[1])
+                    if (cur_sum > pq.top().first)
                         continue;
                     pq.pop();
                 }
-                pq.emplace(vector<int>{nums1[i], nums2[j]});
+                pq.emplace(cur_sum, vector<int>{nums1[i], nums2[j]});
             }
         }
 
         vector<vector<int>> ans(pq.size());
         for (int i = 0; i < ans.size(); ++i) {
-            ans[i] = pq.top();
+            ans[i] = pq.top().second;
             pq.pop();
         }
         return ans;
