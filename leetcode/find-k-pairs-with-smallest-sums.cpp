ffe1e8d72b5879900d046fce8663bcec8c447de3
sgrade <sgrade@users.noreply.github.com>
2023-06-28 09:26:39 +0000

    leetcode

--- leetcode/find-k-pairs-with-smallest-sums.cpp
+++ leetcode/find-k-pairs-with-smallest-sums.cpp
@@ -6,35 +6,41 @@
 using namespace std;
 
 
-// Time Limit Exceeded
+// Based on Editorial's Approach: Using Heap
 class Solution {
 public:
     vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
         ios::sync_with_stdio(false);
         cin.tie(nullptr);
-        auto comp = [](pair<int, vector<int>>& p1, pair<int, vector<int>>& p2) {
-            return p1.first < p2.first;
-        };
-        priority_queue< pair<int, vector<int>>, vector<pair<int, vector<int>>>, decltype(comp) > pq(comp);
+
+        priority_queue< pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, 
+            greater<pair<int, pair<int, int>>> > min_heap;
+        min_heap.emplace(nums1[0] + nums2[0], pair<int, int>(0, 0));
+
+        vector<vector<int>> ans;
         
-        int cur_sum;
-        for (int i = 0; i < nums1.size(); ++i) {
-            for (int j = 0; j < nums2.size(); ++j) {
-                cur_sum = nums1[i] + nums2[j];
-                if (pq.size() == k) {
-                    if (cur_sum > pq.top().first)
-                        continue;
-                    pq.pop();
-                }
-                pq.emplace(cur_sum, vector<int>{nums1[i], nums2[j]});
+        set<pair<int, int>> visited;
+        visited.emplace(0, 0);
+        int idx1, idx2, n1 = nums1.size(), n2 = nums2.size();
+
+        while (k-- && !min_heap.empty()) {
+            auto [_, i] = min_heap.top();
+            min_heap.pop();
+            idx1 = i.first;
+            idx2 = i.second;
+
+            ans.emplace_back(vector<int>{nums1[idx1], nums2[idx2]});
+            
+            if (idx1 + 1 < n1 && visited.find({idx1 + 1, idx2}) == visited.end()) {
+                min_heap.emplace(nums1[idx1 + 1] + nums2[idx2], pair<int, int>{idx1 + 1, idx2});
+                visited.insert({idx1 + 1, idx2});
+            }
+            if (idx2 + 1 < n2 && visited.find({idx1, idx2 + 1}) == visited.end()) {
+                min_heap.emplace(nums1[idx1] + nums2[idx2 + 1], pair<int, int>{idx1, idx2 + 1});
+                visited.insert({idx1, idx2 + 1});
             }
         }
-
-        vector<vector<int>> ans(pq.size());
-        for (int i = 0; i < ans.size(); ++i) {
-            ans[i] = pq.top().second;
-            pq.pop();
-        }
+       
         return ans;
     }
 };
