7a74743e2498d00493bc82b90bfd6207b3e1683b
sgrade <sgrade@users.noreply.github.com>
2023-07-28 20:00:42 +0000

    leetcode

---
+++ leetcode/add-two-integers.cpp
@@ -0,0 +1,14 @@
+// 2235. Add Two Integers
+// https://leetcode.com/problems/add-two-integers/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+class Solution {
+public:
+    int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+};
