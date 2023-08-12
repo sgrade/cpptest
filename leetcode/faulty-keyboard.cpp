3d83128a8d4e9126a5de9818d6c23a88f4a119e3
RomAn <sgrade@users.noreply.github.com>
2023-08-12 22:01:30 +0500

    leetcode

--- leetcode/faulty-keyboard.cpp
+++ leetcode/faulty-keyboard.cpp
@@ -9,26 +9,19 @@ using namespace std;
 class Solution {
 public:
     string finalString(string s) {
-        string ans, tmp;
-        size_t prev = 0;
-        size_t pos = s.find('i');
-        size_t d;
-        size_t cnt;
-        while (pos != string::npos) {
-            d = pos - prev;
-            ans += s.substr(prev, d);
-            cnt = 1;
-            while (pos != string::npos && s[++pos] == 'i')
-                ++cnt;
-            if (cnt % 2 != 0)
-                reverse(ans.begin(), ans.end());
-            prev = pos;
-            pos = s.find('i', prev);
-        }
-        if (prev != pos) {
-            d = pos - prev;
-            ans += s.substr(prev, d);
-            reverse(ans.rbegin(), ans.rbegin() + d);
+        int n = s.size();
+        string ans;
+        for (int i = 0; i < n; ++i) {
+            if (s[i] != 'i')
+                ans += s[i];
+            else {
+                int cnt = 1;
+                while (i + cnt != n && s[i + cnt] == 'i')
+                    ++cnt;
+                if (cnt % 2 != 0)
+                    reverse(ans.begin(), ans.end());
+                i += cnt - 1;
+            }
         }
         return ans;
     }
