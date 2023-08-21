eb026f2bd20d18da22a93fea710ae2c749bf1918
RomAn <sgrade@users.noreply.github.com>
2023-08-21 20:24:56 +0200

    leetcode

--- leetcode/positions-of-large-groups.cpp
+++ leetcode/positions-of-large-groups.cpp
@@ -10,17 +10,14 @@ class Solution {
 public:
     vector<vector<int>> largeGroupPositions(string s) {
         vector<vector<int>> ans;
-        int start = 0, end = 1;
-        for (; end < s.size(); ++end) {
-            if (s[start] != s[end]) {
-                if (end - start > 2) {
+        int start = 0, end = 1, n = s.size();
+        for (; end <= n; ++end) {
+            if (end == n || s[start] != s[end]) {
+                if (end - start > 2)
                     ans.emplace_back(vector<int>{start, end - 1});
-                }
                 start = end;
             }
         }
-        if (end == s.size() && end - start > 2)
-            ans.emplace_back(vector<int>{start, end - 1});
         return ans;
     }
 };
