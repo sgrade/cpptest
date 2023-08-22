4d35b33472609505d64a98e9c4a28bd3dc0e0e50
RomAn <sgrade@users.noreply.github.com>
2023-08-22 14:58:31 +0200

    leetcode

--- leetcode/01-matrix.cpp
+++ leetcode/01-matrix.cpp
@@ -6,43 +6,44 @@
 using namespace std;
 
 
-// Based on Editorial's Approach 1: Breadth-First Search (BFS)
+// Based on Editorial's Approach 2: Dynamic Programming
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
         rows = mat.size(), cols = mat[0].size();
-        vector<vector<int>> ans(rows, vector<int>(cols));
-        vector<vector<bool>> visited(rows, vector<bool>(cols));
-        queue<vector<int>> q;
+        vector<vector<int>> dp = mat;
 
         for (int row = 0; row < rows; ++row) {
             for (int col = 0; col < cols; ++col) {
-                if (mat[row][col] == 0) {
-                    ans[row][col] = mat[row][col];
-                    q.emplace(vector<int>{row, col, 0});
-                    visited[row][col] = true;
-                }
-            }
-        }
-
-        while (!q.empty()) {
-            vector<int> current = q.front();
-            q.pop();
-            int row = current[0], col = current[1], dist = current[2];
-            for (auto& [row_diff, col_diff]: directions) {
-                int next_row = row + row_diff, next_col = col + col_diff;
-                if (next_row < 0 || next_row == rows || next_col < 0 || next_col == cols || visited[next_row][next_col])
+                if (dp[row][col] == 0)
                     continue;
-                visited[next_row][next_col] = true;
-                q.emplace(vector<int>{next_row, next_col, dist + 1});
-                ans[next_row][next_col] = dist + 1;
+                int min_neighbor = rows * cols;
+                if (row > 0)
+                    min_neighbor = min(min_neighbor, dp[row - 1][col]);
+                if (col > 0)
+                    min_neighbor = min(min_neighbor, dp[row][col - 1]);
+                dp[row][col] = min_neighbor + 1;
             }
         }
 
-        return ans;
+        for (int row = rows - 1; row >= 0; --row) {
+            for (int col = cols - 1; col >= 0; --col) {
+                if (dp[row][col] == 0)
+                    continue;
+                int min_neighbor = rows * cols;
+                if (row < rows - 1)
+                    min_neighbor = min(min_neighbor, dp[row + 1][col]);
+                if (col < cols - 1)
+                    min_neighbor = min(min_neighbor, dp[row][col + 1]);
+                dp[row][col] = min(dp[row][col], min_neighbor + 1);
+            }
+        }
+
+        return dp;
     }
 
 private:
     int rows, cols;
-    vector<pair<int, int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 };
