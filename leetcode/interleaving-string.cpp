9c25e99f3bd810099b40d922a97a2f79b7e9f6c8
RomAn <sgrade@users.noreply.github.com>
2023-08-25 22:00:27 +0200

    leetcode

--- leetcode/interleaving-string.cpp
+++ leetcode/interleaving-string.cpp
@@ -6,36 +6,29 @@
 using namespace std;
 
 
-// Based on Leetcode's Approach 2: Recursion with memoization
+// Based on Editorial's Approach 3: Using 2D Dynamic Programming
 class Solution {
 public:
-    int n, m;
     bool isInterleave(string s1, string s2, string s3) {
-        n = s1.size(), m = s2.size();
-        if (n + m != s3.size()) {
+        int n1 = s1.size(), n2 = s2.size(), n3 = s3.size();
+        if (n1 + n2 != n3)
             return false;
+        vector<vector<bool>> dp(n1 + 1, vector<bool>(n2 + 1));
+        for (int i = 0; i <= n1; ++i) {
+            for (int j = 0; j <= n2; ++j) {
+                if (i == 0 && j == 0)
+                    dp[i][j] = true;
+                else if (i == 0)
+                    dp[i][j] = dp[i][j - 1] && s2[j - 1] == s3[i + j - 1];
+                else if (j == 0)
+                    dp[i][j] = dp[i - 1][j] && s1[i - 1] == s3[i + j - 1];
+                else
+                    dp[i][j] = (
+                        dp[i - 1][j] && s1[i - 1] == s3[i + j - 1] ||
+                        dp[i][j - 1] && s2[j - 1] == s3[i + j - 1]
+                    );
+            }
         }
-        vector<vector<int>> processed(n, vector<int>(m, -1));
-        return IsInterleave(s1, 0, s2, 0, s3, 0, processed);
-    }
-
-private:
-    bool IsInterleave(const string &s1, int i, const string &s2, int j, const string &s3, int k, vector<vector<int>> &processed) {
-        if (i == n) {
-            return s2.substr(j) == s3.substr(k);
-        }
-        if (j == m) {
-            return s1.substr(i) == s3.substr(k);
-        }
-        if (processed[i][j] >= 0) {
-            return processed[i][j] == 1 ? true : false;
-        }
-        bool ans = false;
-        if (s3[k] == s1[i] && IsInterleave(s1, i + 1, s2, j, s3, k + 1, processed) || 
-            s3[k] == s2[j] && IsInterleave(s1, i, s2, j + 1, s3, k + 1, processed)) {
-            ans = true;
-        }
-        processed[i][j] = ans ? 1 : 0;
-        return ans;
+        return dp[n1][n2];
     }
 };
