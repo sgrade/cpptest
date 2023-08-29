aed1bbbcdc0bac7a6c6663d1435333b459c77923
RomAn <sgrade@users.noreply.github.com>
2023-08-29 17:56:47 +0200

    leetcode

--- leetcode/check-if-there-is-a-valid-partition-for-the-array.cpp
+++ leetcode/check-if-there-is-a-valid-partition-for-the-array.cpp
@@ -6,33 +6,24 @@
 using namespace std;
 
 
-// Based on Editorial's Approach 1: Top-Down Dynamic Programming
+// Based on Editorial's Approach 2: Bottom-Up Dynamic Programming
 class Solution {
 public:
     bool validPartition(vector<int>& nums) {
-        n = nums.size();
-        memo.resize(n + 1, -1);
-        memo[0] = 1;
-        return PrefixIsValid(nums, n - 1);
-    }
+        int n = nums.size();
+        vector<int> dp(n + 1);
+        dp[0] = 1;
 
-private:
-    int n;
-    vector<int> memo;
-    bool PrefixIsValid (vector<int>& nums, int i) {
-        if (memo[i + 1] != -1) 
-            return memo[i + 1];
-        
-        bool ans = false;
+        for (int i = 0; i < n; ++i) {
+            int dp_idx = i + 1;
+            if (i > 0 && nums[i] == nums[i - 1])
+                dp[dp_idx] |= dp[dp_idx - 2];
+            if (i > 1 && nums[i] == nums[i - 1] && nums[i] == nums[i - 2])
+                dp[dp_idx] |= dp[dp_idx - 3];
+            if (i > 1 && nums[i] == nums[i - 1] + 1 && nums[i] == nums[i - 2] + 2)
+                dp[dp_idx] |= dp[dp_idx - 3];
+        }
 
-        if (i > 0 && nums[i] == nums[i - 1])
-            ans |= PrefixIsValid(nums, i - 2);
-        if (i > 1 && nums[i] == nums[i - 1] && nums[i - 1] == nums[i - 2])
-            ans |= PrefixIsValid(nums, i - 3);
-        if (i > 1 && nums[i] == nums[i - 1] + 1 && nums[i - 1] == nums[i - 2] + 1)
-            ans |= PrefixIsValid(nums, i - 3);
-        
-        memo[i + 1] = ans;
-        return ans;
-    }
+        return dp[n];
+    };
 };
