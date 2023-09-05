2755e88e779e7eb5d333fb7a54ca92456cad7fb1
SGrade <sgrade@users.noreply.github.com>
2023-09-05 18:28:08 +0000

    leetcode

--- coin-change-ii.cpp
+++ coin-change-ii.cpp
@@ -6,30 +6,26 @@
 using namespace std;
 
 
-// Based on Editorial's Approach 1: Top-Down Dynamic Programming
+// Based on Editorial's Approach 2: Bottom-Up Dynamic Programming
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
-        n = coins.size();
-        memo.resize(n, vector<int>(amount + 1, -1));
-        return NumberOfWays(0, amount, coins);
-    }
+        int n = coins.size();
 
-private:
-    int n;
-    vector<vector<int>> memo;
-
-    int NumberOfWays(int i, int amount, vector<int>& coins) {
-        if (amount == 0)
-            return 1;
-        if (i == n)
-            return 0;
-        if (memo[i][amount] != -1)
-            return memo[i][amount];
-        if (coins[i] > amount)
-            return memo[i][amount] = NumberOfWays(i + 1, amount, coins);
+        // i - current coin, j - amount to get
+        vector<vector<int>> dp(n + 1, vector<int>(amount + 1));
+        for (vector<int>& num_of_ways: dp)
+            num_of_ways[0] = 1;
         
-        memo[i][amount] = NumberOfWays(i, amount - coins[i], coins) + NumberOfWays(i + 1, amount, coins);
-        return memo[i][amount];
+        for (int i = n - 1; i >= 0; --i) {
+            for (int j = 1; j <= amount; ++j) {
+                if (coins[i] > j)
+                    dp[i][j] = dp[i + 1][j];
+                else
+                    dp[i][j] = dp[i + 1][j] + dp[i][j - coins[i]];
+            }
+        }
+
+        return dp[0][amount];
     }
 };
