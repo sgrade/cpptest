98bac9d52ebf7d6bcb8c983e2a243f2949f06d9e
SGrade <sgrade@users.noreply.github.com>
2023-10-09 09:02:59 +0000

    leetcode

--- leetcode/find-first-and-last-position-of-element-in-sorted-array_lower_bound.cpp
+++ leetcode/find-first-and-last-position-of-element-in-sorted-array_lower_bound.cpp
@@ -1,6 +1,6 @@
 // 34. Find First and Last Position of Element in Sorted Array
 // https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
-// implementation with lower_bound
+
 
 #include <bits/stdc++.h>
 
@@ -9,19 +9,21 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {        
-        vector<int>::iterator it;
-        it = lower_bound(nums.begin(), nums.end(), target);
-        
-        if (it == nums.end() || *it != target) {
-            return {-1, -1};
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> ans = {-1, -1};
+        int left = 0, n = nums.size(), right = n - 1, mid;
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            if (nums[mid] < target)
+                left = mid + 1;
+            else if (nums[mid] > target)
+                right = mid - 1;
+            else {
+                ans[0] = distance(nums.begin(), lower_bound(nums.begin() + left, nums.begin() + mid + 1, target));
+                ans[1] = distance(nums.begin(), upper_bound(nums.begin() + mid, nums.begin() + right + 1, target)) - 1;
+                break;
+            }
         }
-        
-        int left = distance(nums.begin(), it);
-        int right = left;
-        while (right < nums.size() - 1 && nums[right + 1] == target) {
-            ++right;
-        }
-        return  {left, right};
+        return ans;
     }
 };
