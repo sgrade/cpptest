c42e7a927dc7b09f11aa615acc069edac9e2d637
SGrade <sgrade@users.noreply.github.com>
2023-10-12 16:50:54 +0000

    leetcode

---
+++ leetcode/find-all-duplicates-in-an-array.cpp
@@ -0,0 +1,21 @@
+// 442. Find All Duplicates in an Array
+// https://leetcode.com/problems/find-all-duplicates-in-an-array/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// Based on Editorial's Approach 4: Mark Visited Elements in the Input Array itself
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector<int> ans;
+        for (int& num: nums) {
+            if (nums[abs(num) - 1] < 0)
+                ans.emplace_back(abs(num));
+            nums[abs(num) - 1] *= -1;
+        }
+        return ans;
+    }
+};
