1a301515688b7bbde49bd7fe15652cca824db962
SGrade <sgrade@users.noreply.github.com>
2023-10-14 13:19:04 +0000

    leetcode

--- leetcode/minimum-height-trees.cpp
+++ leetcode/minimum-height-trees.cpp
@@ -6,10 +6,17 @@
 using namespace std;
 
 
-// TLE
+// VERY SLOW
+// Based on Editorial's Approach 1: Topological Sorting
 class Solution {
 public:
     vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
+        if (n < 2) {
+            for (int i = 0; i < n; ++i)
+                ans.emplace_back(i);
+            return ans;
+        }
+
         this->n = n;
         adj.resize(n);
         for (const vector<int>& e: edges) {
@@ -17,43 +24,37 @@ public:
             adj[e[1]].emplace_back(e[0]);
         }
 
-        heights;
-        for (int i = 0; i < n; ++i)
-            bfs(i);
-        
-        sort(heights.begin(), heights.end());
-        int max_height = heights[0].first;
-        vector<int> ans = {heights[0].second};
-        for (size_t i = 1; i < heights.size() && heights[i].first == max_height; ++i) {
-            ans.emplace_back(heights[i].second);
+        queue<int> leaves;
+        for (int i = 0; i < n; ++i) {
+            if (adj[i].size() == 1)
+                leaves.emplace(i);
+        }
+
+        int remaining_nodes = n;
+        while (remaining_nodes > 2) {
+            remaining_nodes -= leaves.size();
+            queue<int> new_leaves;
+            while (!leaves.empty()) {
+                int leaf = leaves.front();
+                leaves.pop();
+                int neighbor = adj[leaf][0];
+                vector<int>& neighbors = adj[neighbor];
+                // https://en.wikipedia.org/wiki/Erase%E2%80%93remove_idiom
+                neighbors.erase(remove(neighbors.begin(), neighbors.end(), leaf), neighbors.end());
+                if (neighbors.size() == 1)
+                    new_leaves.emplace(neighbor);
+            }
+            leaves = new_leaves;
+        }
+
+        while (!leaves.empty()) {
+            ans.emplace_back(leaves.front());
+            leaves.pop();
         }
         return ans;
     }
 private:
     int n;
+    vector<int> ans;
     vector<vector<int>> adj;
-    vector<pair<int, int>> heights;
-    void bfs(int root) {
-        int height = 0;
-        // node, height
-        queue<pair<int, int>> q;
-        q.emplace(root, 0);
-        vector<bool> visited(n);
-        while (!q.empty()) {
-            int node = q.front().first;
-            int current_height = q.front().second + 1;
-            q.pop();
-            visited[node] = true;
-            height = max(height, current_height);
-            if (adj[node].empty()) {
-                continue;
-            }
-            for (const int& neighbor: adj[node]) {
-                if (visited[neighbor])
-                    continue;
-                q.emplace(neighbor, current_height);
-            }
-        }
-        heights.emplace_back(height, root);
-    }
 };
