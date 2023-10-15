0b7a8565a9c78e52e472828b92db15291ce3b83f
SGrade <sgrade@users.noreply.github.com>
2023-10-15 21:44:15 +0000

    leetcode

--- leetcode/minimum-height-trees.cpp
+++ leetcode/minimum-height-trees.cpp
@@ -6,45 +6,47 @@
 using namespace std;
 
 
-// VERY SLOW
 // Based on Editorial's Approach 1: Topological Sorting
+// Optimized with ideas from Sample 87 ms submission
 class Solution {
 public:
     vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
+        vector<int> ans;
         if (n < 2) {
             for (int i = 0; i < n; ++i)
                 ans.emplace_back(i);
             return ans;
         }
 
-        this->n = n;
-        adj.resize(n);
+        vector<vector<int>> adj(n);
+        vector<int> indegree(n);
         for (const vector<int>& e: edges) {
             adj[e[0]].emplace_back(e[1]);
             adj[e[1]].emplace_back(e[0]);
+            ++indegree[e[0]];
+            ++indegree[e[1]];
         }
 
         queue<int> leaves;
         for (int i = 0; i < n; ++i) {
-            if (adj[i].size() == 1)
+            if (indegree[i] == 1)
                 leaves.emplace(i);
         }
 
         int remaining_nodes = n;
         while (remaining_nodes > 2) {
-            remaining_nodes -= leaves.size();
-            queue<int> new_leaves;
-            while (!leaves.empty()) {
+            int k = leaves.size();
+            remaining_nodes -= k;
+            for (int i = 0; i < k; ++i) {
                 int leaf = leaves.front();
                 leaves.pop();
-                int neighbor = adj[leaf][0];
-                vector<int>& neighbors = adj[neighbor];
-                // https://en.wikipedia.org/wiki/Erase%E2%80%93remove_idiom
-                neighbors.erase(remove(neighbors.begin(), neighbors.end(), leaf), neighbors.end());
-                if (neighbors.size() == 1)
-                    new_leaves.emplace(neighbor);
+                --indegree[leaf];
+                for (const int& neighbor: adj[leaf]) {
+                    --indegree[neighbor];
+                    if (indegree[neighbor] == 1)
+                        leaves.emplace(neighbor);
+                }
             }
-            leaves = new_leaves;
         }
 
         while (!leaves.empty()) {
@@ -53,8 +55,4 @@ public:
         }
         return ans;
     }
-private:
-    int n;
-    vector<int> ans;
-    vector<vector<int>> adj;
 };
