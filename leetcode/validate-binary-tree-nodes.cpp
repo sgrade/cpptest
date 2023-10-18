909c26c3cedbbe06f7fbcb7ea1f7576d29c1312e
SGrade <sgrade@users.noreply.github.com>
2023-10-18 11:48:08 +0000

    leetcode

--- leetcode/validate-binary-tree-nodes.cpp
+++ leetcode/validate-binary-tree-nodes.cpp
@@ -6,46 +6,57 @@
 using namespace std;
 
 
-// Based on Editorial's Approach 1: Depth First Search (DFS)
+// Based on Editorial's Approach 3: Union Find
+class UnionFind {
+public:
+    UnionFind (int size): parent(size), rank(size) {
+        iota(parent.begin(), parent.end(), 0);
+        components = size;
+    }
+    int Find (int x) {
+        if (x != parent[x])
+            parent[x] = Find(parent[x]);
+        return parent[x];
+    }
+    bool UnionSet (int node, int child) {
+        int root_node = Find(node); 
+        int root_child = Find(child);
+        if (root_child != child || root_node == child)
+            return false;
+        --components;
+        if (rank[root_child] < rank[root_node])
+            parent[root_child] = root_node;
+        else if (rank[root_node] < rank[root_child])
+            parent[root_node] = root_child;
+        else {
+            parent[root_child] = root_node;
+            ++rank[root_child];
+        }
+        return true;
+    }
+    bool Connected (int x, int y) {
+        return Find(x) == Find(y);
+    }
+    vector<int> parent, rank;
+    int components;
+};
+
 class Solution {
 public:
     bool validateBinaryTreeNodes(int n, vector<int>& leftChild, vector<int>& rightChild) {
-        this->n = n;
-        int root = FindRoot(leftChild, rightChild);
-        if (root == -1)
-            return false;
-        
-        vector<bool> visited(n);
-        visited[root] = true;
-        stack<int> dfs;
-        dfs.emplace(root);
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
 
-        while(!dfs.empty()) {
-            int node = dfs.top();
-            dfs.pop();
+        UnionFind dsu(n);
+        for (int node = 0; node < n; ++node) {
             vector<int> children = {leftChild[node], rightChild[node]};
             for (const int& child: children) {
-                if (child != -1) {
-                    if (visited[child])
-                        return false;
-                    dfs.emplace(child);
-                    visited[child] = true;
-                }
+                if (child == -1)
+                    continue;
+                if (!dsu.UnionSet(node, child))
+                    return false;
             }
         }
-
-        return find(visited.begin(), visited.end(), false) == visited.end();
-    }
-
-private:
-    int n;
-    int FindRoot(vector<int>& left, vector<int>& right) {
-        unordered_set<int> children;
-        children.insert(left.begin(), left.end());
-        children.insert(right.begin(), right.end());
-        for (int i = 0; i < n; ++i)
-            if (children.find(i) == children.end())
-                return i;
-        return -1;
+        return dsu.components == 1;
     }
 };
