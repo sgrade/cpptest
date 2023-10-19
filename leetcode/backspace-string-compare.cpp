49b7d48384029f865be8a7b4f2d72ae17dca3334
SGrade <sgrade@users.noreply.github.com>
2023-10-19 17:48:22 +0000

    leetcode

--- leetcode/backspace-string-compare.cpp
+++ leetcode/backspace-string-compare.cpp
@@ -9,42 +9,19 @@ using namespace std;
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
-        int i = s.size() - 1;
-        int j = t.size() - 1;
-        int cnt_s = 0, cnt_t = 0;
-        
-        while (i >= 0 || j >= 0) {
-            while (i >= 0) {
-                if (s[i] == '#') {
-                    ++cnt_s;
-                    --i;
-                }
-                else if (cnt_s > 0) {
-                    --cnt_s;
-                    --i;
-                }
-                else break;
+        return FirstPass(s) == FirstPass(t);
+    }
+private:
+    string FirstPass (string& s) {
+        string ans;
+        for (const char& ch: s) {
+            if (ch != '#')
+                ans += ch;
+            else {
+                if (!ans.empty())
+                    ans.pop_back();
             }
-            while (j >= 0) {
-                if (t[j] == '#') {
-                    ++cnt_t;
-                    --j;
-                }
-                else if (cnt_t > 0) {
-                    --cnt_t;
-                    --j;
-                }
-                else break;
-            }
-            if (i >= 0 && j >= 0 && s[i] != t[j]) {
-                return false;
-            }
-            if ((i >= 0) != (j >= 0)) {
-                return false;
-            }
-            --i;
-            --j;
         }
-        return true;
+        return ans;
     }
 };
