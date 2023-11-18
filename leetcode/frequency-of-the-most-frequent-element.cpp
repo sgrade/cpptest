dcb29e081363df1dc199023948dc9ba88b8a3748
SGrade <sgrade@users.noreply.github.com>
2023-11-18 20:49:59 +0000

    leetcode

--- leetcode/frequency-of-the-most-frequent-element.cpp
+++ leetcode/frequency-of-the-most-frequent-element.cpp
@@ -6,23 +6,20 @@
 using namespace std;
 
 
-// NOT FINISHED
+// Based on Editorial's Approach 1: Sliding Window
 class Solution {
 public:
     int maxFrequency(vector<int>& nums, int k) {
         int n = nums.size();
-        sort(nums.begin(), nums.end(), greater<int>());
-        vector<long> d(n, 0L);
-        for (int i = 1; i < n; ++i)
-            d[i] = d[i - 1] + nums[i - 1] - nums[i];
-        for (int i = 1; i < n; ++i)
-            d[i] = d[i - 1] + d[i];
-        int ans = 1, left = 0, right = 1, current_d;
+        sort(nums.begin(), nums.end());
+        int left = 0, ans = 1;
+        long sum = nums[left], target;
         for (int right = 1; right < n; ++right) {
-            current_d = d[right] - d[left];
-            while (left < right && current_d > k) {
+            target = nums[right];
+            sum += target;
+            while ((right - left + 1) * target - sum > k) {
+                sum -= nums[left];
                 ++left;
-                current_d = d[right] - d[left];
             }
             ans = max(ans, right - left + 1);
         }
