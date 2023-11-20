b767ec2db7f4c57d9935f304b3d37a99b0842cdc
SGrade <sgrade@users.noreply.github.com>
2023-11-20 12:59:45 +0000

    leetcode

--- leetcode/minimum-amount-of-time-to-collect-garbage.cpp
+++ leetcode/minimum-amount-of-time-to-collect-garbage.cpp
@@ -10,28 +10,23 @@ class Solution {
 public:
     int garbageCollection(vector<string>& garbage, vector<int>& travel) {
         int n = garbage.size();
+        int ans = 0;
 
-        map<char, int> counter = {{'M', 0}, {'P', 0}, {'G', 0}};
         map<char, int> last = {{'M', 0}, {'P', 0}, {'G', 0}};
         for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < garbage[i].size(); ++j) {
-                char unit = garbage[i][j];
-                // Travel
+            // Travel
+            for (const char& unit: garbage[i])
                 last[unit] = i;
-                // Collect the garbage unit
-                ++counter[unit];
-            }
+            // Collect the garbage
+            ans += garbage[i].size();
         }
 
-        int ans = 0;
         // Total travel
         for (int i = 1; i < n - 1; ++i)
             travel[i] += travel[i - 1];
-        for (const auto& [unit, last_idx]: last)
-            ans += last[unit] == 0 ? 0 : travel[last_idx - 1];
-        // Total collect garbage
-        for (const auto& [_, count]: counter)
-            ans += count;
+        for (const auto& [unit, last_house_idx]: last)
+            ans += last[unit] == 0 ? 0 : travel[last_house_idx - 1];
+
         return ans;
     }
 };
