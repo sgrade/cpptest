84cf7fa84592446d456a88e44e72536955fd1fe4
SGrade <sgrade@users.noreply.github.com>
2023-12-05 17:30:45 +0000

    leetcode

---
+++ leetcode/count-of-matches-in-tournament.cpp
@@ -0,0 +1,14 @@
+// 1688. Count of Matches in Tournament
+// https://leetcode.com/problems/count-of-matches-in-tournament/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        return n - 1;
+    }
+};
