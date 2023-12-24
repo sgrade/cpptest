23e9654721fcb06d65a8ea946c6e77c62124b6c7
SGrade <sgrade@users.noreply.github.com>
2023-12-24 16:26:16 +0000

    leetcode

--- leetcode/minimum-changes-to-make-alternating-binary-string.cpp
+++ leetcode/minimum-changes-to-make-alternating-binary-string.cpp
@@ -10,14 +10,13 @@ class Solution {
 public:
     int minOperations(string s) {
         pair<char, char> x = {'0', '1'};
-        int ans_zero = 0, ans_one = 0;
+        int ans_zero = 0, ans_one;
         for (const char& ch: s) {
             if (ch != x.first)
                 ++ans_zero;
-            if (ch != x.second)
-                ++ans_one;
             swap(x.first, x.second);
         }
+        ans_one = s.size() - ans_zero;
         return min(ans_zero, ans_one);
     }
 };
