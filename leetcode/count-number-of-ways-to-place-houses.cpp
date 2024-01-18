ddfd1cbb554f5ce07f59f77d293382342cc770dc
SGrade <sgrade@users.noreply.github.com>
2024-01-18 10:51:20 +0000

    leetcode

--- leetcode/count-number-of-ways-to-place-houses.cpp
+++ leetcode/count-number-of-ways-to-place-houses.cpp
@@ -6,24 +6,27 @@
 using namespace std;
 
 
-// TLE
 class Solution {
 public:
     int countHousePlacements(int n) {
+        dp.resize(n + 1, vector<int>(2, - 1));
         long long one_side_count = (count(n, false) + count(n, true)) % MOD;
         return (one_side_count * one_side_count) % MOD;
     }
 
 private:
     long long MOD = 1e9 + 7;
+    vector<vector<int>> dp;
     long long count(int n, bool placed) {
         if (n == 1)
             return 1;
+        if (dp[n][placed] != -1)
+            return dp[n][placed];
         // the adjacent cell should be empty
         if (placed)
-            return count(n - 1, false);
+            return dp[n][1] = count(n - 1, false);
         // the adjacent cell can be left empty or house can be placed there
         else
-            return (count(n - 1, false) + count(n - 1, true)) % MOD;
+            return dp[n][0] = (count(n - 1, false) + count(n - 1, true)) % MOD;
     }
 };
