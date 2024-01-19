56a4041621cb4bd10aeb42021942328b7c25d88e
SGrade <sgrade@users.noreply.github.com>
2024-01-19 21:42:18 +0000

    leetcode

--- leetcode/minimum-falling-path-sum.cpp
+++ leetcode/minimum-falling-path-sum.cpp
@@ -12,10 +12,12 @@ public:
         int rows = matrix.size(), cols = matrix[0].size();
         for (int row = 1; row < rows; ++row) {
             for (int col = 0; col < cols; ++col) {
-                int left = col == 0 ? INT_MAX : matrix[row - 1][col - 1];
-                int mid = matrix[row - 1][col];
-                int right = col == cols - 1 ? INT_MAX : matrix[row - 1][col + 1];
-                matrix[row][col] += min({left, mid, right});
+                int x = matrix[row - 1][col];
+                if (col > 0)
+                    x = min(x, matrix[row - 1][col - 1]);
+                if (col < cols - 1)
+                    x = min(x, matrix[row - 1][col + 1]);
+                matrix[row][col] += x;
             }
         }
         return *min_element(matrix[rows - 1].begin(), matrix[rows - 1].end());
