005084f6fc8b7e2baa0b18415057d525c852d238
SGrade <sgrade@users.noreply.github.com>
2024-01-21 12:05:38 +0000

    leetcode

--- leetcode/sum-of-subarray-minimums.cpp
+++ leetcode/sum-of-subarray-minimums.cpp
@@ -6,28 +6,33 @@
 using namespace std;
 
 
-// Based on Editorial's Approach 1: Monotonic Stack
+// Based on Editorial's Approach 2: Monotonic Stack + Dynamic Programming
 class Solution {
 public:
     int sumSubarrayMins(vector<int>& arr) {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
         stack<int> st;
-        long ans = 0;
         int n = arr.size();
-        for (int i = 0; i <= n; ++i) {
-            while (!st.empty() && (i == n || arr[st.top()] >= arr[i])) {
-                int mid = st.top();
+        vector<int> dp(n);
+        for (int i = 0; i < n; ++i) {
+            while (!st.empty() && arr[st.top()] >= arr[i])
                 st.pop();
-                int left_boundary = st.empty() ? -1 : st.top();
-                int right_boundary = i;
-                long count = (mid - left_boundary) * (right_boundary - mid) % MOD;
-                ans += (count * arr[mid]) % MOD;
-                ans %= MOD;
+            if (!st.empty()) {
+                int previous_smaller = st.top();
+                dp[i] = dp[previous_smaller] + (i - previous_smaller) * arr[i];
             }
+            else
+                dp[i] = (i + 1) * arr[i];
             st.emplace(i);
-        }    
+        }
+        long ans = 0;
+        for (const int& cnt: dp) {
+            ans += cnt;
+            ans %= MOD;
+        }
         return ans;
     }
 private:
     int MOD = 1e9 + 7;
-
 };
