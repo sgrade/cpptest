3e99c18a76c03d8501f303dc39ac93625b8ac627
SGrade <sgrade@users.noreply.github.com>
2024-01-22 10:49:06 +0000

    leetcode

--- leetcode/set-mismatch.cpp
+++ leetcode/set-mismatch.cpp
@@ -6,21 +6,27 @@
 using namespace std;
 
 
+// Optimized with ideas from a sample solution
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
         ios::sync_with_stdio(false);
         cin.tie(nullptr);
-        sort(nums.begin(), nums.end());
+        int duplicate;
         vector<int> ans(2);
-        unordered_map<int, int> cntr;
-        for (const int& num: nums)
-            ++cntr[num];
-        for (int i = 1; i <= nums.size(); ++i) {
-            if (cntr.find(i) == cntr.end())
-                ans[1] = i;
-            if (cntr[i] == 2)
-                ans[0] = i;
+        for (const int& num: nums) {
+            int idx = abs(num) - 1;
+            if (nums[idx] < 0)
+                duplicate = abs(num);
+            else
+                nums[idx] = -nums[idx];
+        }
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] > 0) {
+                ans[0] = duplicate;
+                ans[1] = i + 1;
+                break;
+            }
         }
         return ans;
     }
