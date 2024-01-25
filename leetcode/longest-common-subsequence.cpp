8922d4fa5729f83de4d5a10f2b87a13a97549295
SGrade <sgrade@users.noreply.github.com>
2024-01-25 18:02:29 +0000

    leetcode

--- leetcode/longest-common-subsequence.cpp
+++ leetcode/longest-common-subsequence.cpp
@@ -6,20 +6,26 @@
 using namespace std;
 
 
-// Based on Editorial's Approach 3: Dynamic Programming
+// Based on Editorial's Approach 4: Dynamic Programming with Space Optimization
 class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) {
         int n1 = text1.size(), n2 = text2.size();
-        vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1));
-        for (int i1 = n1 - 1; i1 >= 0; --i1) {
-            for (int i2 = n2 - 1; i2 >= 0; --i2) {
-                if (text1[i1] == text2[i2])
-                    dp[i1][i2] = 1 + dp[i1 + 1][i2 + 1];
-                else
-                    dp[i1][i2] = max(dp[i1 + 1][i2], dp[i1][i2 + 1]);
-            }
+        if (n2 < n1) {
+            swap(n1, n2);
+            swap(text1, text2);
         }
-        return dp[0][0];
+        vector<int> prev(n1 + 1);
+        for (int i2 = n2 - 1; i2 >= 0; --i2) {
+            vector<int> current(n1 + 1);
+            for (int i1 = n1 - 1; i1 >= 0; --i1) {
+                if (text1[i1] == text2[i2])
+                    current[i1] = 1 + prev[i1 + 1];
+                else
+                    current[i1] = max(prev[i1], current[i1 + 1]);
+            }
+            prev = current;
+        }
+        return prev[0];
     }
 };
