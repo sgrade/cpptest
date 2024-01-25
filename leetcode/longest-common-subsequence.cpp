e9795bdfd2df4d591d43ae0fde5a3357fdc434ef
SGrade <sgrade@users.noreply.github.com>
2024-01-25 17:53:01 +0000

    leetcode

--- leetcode/longest-common-subsequence.cpp
+++ leetcode/longest-common-subsequence.cpp
@@ -6,27 +6,20 @@
 using namespace std;
 
 
-// TLE
+// Based on Editorial's Approach 3: Dynamic Programming
 class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) {
-        n1 = text1.size(), n2 = text2.size();
-        n = max(n1, n2);
-        int ans = dfs(text1, text2, 0, 0, 0);
-        return ans;
-    }
-private:
-    int n1, n2, n;
-    vector<int> dp;
-    int dfs (const string& text1, const string& text2, int i1, int i2, int ans) {
-        if (i1 == n1 || i2 == n2)
-            return ans;
-        int pick1_pick2 = 0;
-        if (text1[i1] == text2[i2])
-            pick1_pick2 = dfs(text1, text2, i1 + 1, i2 + 1, ans + 1);
-        int pick1_skip2 = dfs(text1, text2, i1, i2 + 1, ans);
-        int skip1_pick2 = dfs(text1, text2, i1 + 1, i2, ans);
-        ans = max({pick1_pick2, pick1_skip2, skip1_pick2});
-        return ans;
+        int n1 = text1.size(), n2 = text2.size();
+        vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1));
+        for (int i1 = n1 - 1; i1 >= 0; --i1) {
+            for (int i2 = n2 - 1; i2 >= 0; --i2) {
+                if (text1[i1] == text2[i2])
+                    dp[i1][i2] = 1 + dp[i1 + 1][i2 + 1];
+                else
+                    dp[i1][i2] = max(dp[i1 + 1][i2], dp[i1][i2 + 1]);
+            }
+        }
+        return dp[0][0];
     }
 };
