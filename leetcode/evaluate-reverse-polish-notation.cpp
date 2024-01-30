6a41c98f0cc060c7bb15905dd6a934496ec078fb
SGrade <sgrade@users.noreply.github.com>
2024-01-30 20:05:46 +0000

    leetcode

--- leetcode/evaluate-reverse-polish-notation.cpp
+++ leetcode/evaluate-reverse-polish-notation.cpp
@@ -9,11 +9,13 @@ using namespace std;
 class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
         stack<int> nums;
         int x, num1, num2;
-        for (int i = 0; i < tokens.size(); ++i) {
-            if (isdigit(tokens[i][tokens[i].size() - 1])) {
-                x = stoi(tokens[i]);
+        for (const string& s: tokens) {
+            if (isdigit(s.back())) {
+                x = stoi(s);
                 nums.emplace(x);
             }
             else {
@@ -21,7 +23,7 @@ public:
                 nums.pop();
                 num1 = nums.top();
                 nums.pop();
-                x = ops(tokens[i], num1, num2);
+                x = ops(s, num1, num2);
                 nums.emplace(x);
             }
         }
@@ -30,18 +32,14 @@ public:
     
     long ops(const string &op, const int &num1, const int &num2) {
         long output;
-        if (op == "+") {
+        if (op == "+")
             output = num1 + num2;
-        }
-        else if (op == "-") {
+        else if (op == "-")
             output = num1 - num2;
-        }
-        else if (op == "*") {
+        else if (op == "*")
             output = 1L * num1 * num2;
-        }
-        else {
+        else
             output = num1 / num2;
-        }
         return output;
     }
 };
