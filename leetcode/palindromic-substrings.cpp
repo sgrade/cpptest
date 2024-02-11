02fe276390f63587d5876f41b7557c058dcf1ce2
SGrade <sgrade@users.noreply.github.com>
2024-02-11 18:10:29 +0000

    leetcode

--- leetcode/palindromic-substrings.cpp
+++ leetcode/palindromic-substrings.cpp
@@ -6,25 +6,24 @@
 using namespace std;
 
 
-// Based on Editorial's Approach #1: Check All Substrings
+// Based on Editorial's Approach #2: Dynamic Programming
 class Solution {
 public:
     int countSubstrings(string s) {
-        int ans = 0;
-        for (int left = 0; left < s.size(); ++left)
-            for (int right = left; right < s.size(); ++right) {
-                ans += isPalindrome(s, left, right);
-            }
-        return ans;
-    }
-private:
-    bool isPalindrome(const string& s, int left, int right) {
-        while (left < right) {
-            if (s[left] != s[right])
-                return false;
-            ++left;
-            --right;
+        int n = s.size(), ans = n;
+        vector<vector<bool>> dp(n, vector<bool>(n));
+        for (int i = 0; i < n; ++i)
+            dp[i][i] = true;
+        for (int i = 0; i < n - 1; ++i) {
+            dp[i][i + 1] = s[i] == s[i + 1];
+            ans += dp[i][i + 1];
         }
-        return true;
+        for (int len = 3; len <= n; ++len) {
+            for (int i = 0, j = i + len - 1; j < n; ++i, ++j) {
+                dp[i][j] = dp[i + 1][j - 1] && (s[i] == s[j]);
+                ans += dp[i][j];
+            }
+        }
+        return ans;
     }
 };
