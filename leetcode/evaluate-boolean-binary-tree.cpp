d390d1f62511e91b344106bd1781e0f79914c0c1
SGrade <sgrade@users.noreply.github.com>
2024-02-25 18:11:43 +0000

    leetcode

---
+++ leetcode/evaluate-boolean-binary-tree.cpp
@@ -0,0 +1,28 @@
+// 2331. Evaluate Boolean Binary Tree
+// https://leetcode.com/problems/evaluate-boolean-binary-tree/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    bool evaluateTree(TreeNode* root) {
+        if (root->left == nullptr && root->right == nullptr)
+            return root->val;
+        bool left = evaluateTree(root->left);
+        bool right = evaluateTree(root->right);
+        return root->val == 2 ? left || right : left && right;
+    }
+};
