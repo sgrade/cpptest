fd12c9231efa349c5b090507ed312384189503e2
SGrade <sgrade@users.noreply.github.com>
2024-02-26 12:35:39 +0000

    leetcode

--- leetcode/same-tree.cpp
+++ leetcode/same-tree.cpp
@@ -19,16 +19,13 @@ struct TreeNode {
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if (p == nullptr && q == nullptr) {
-            return true;
-        }
-        if (p == nullptr || q == nullptr) {
-            return false;
-        }
-        if (p->val != q->val) {
-            return false;
-        }
-        return isSameTree(p->right, q->right) && 
-            isSameTree(p->left, q->left);
+        return dfs(p, q);
+    }
+private:
+    bool dfs(TreeNode* p, TreeNode* q) {
+        if (p == nullptr && q == nullptr) return true;
+        if (p == nullptr || q == nullptr) return false;
+        if (p->val != q->val) return false;
+        return dfs(p->left, q->left) && dfs(p->right, q->right);
     }
 };
