7ea32a46dfd6cf77d5b78bbd6d8801949317b5ca
SGrade <sgrade@users.noreply.github.com>
2024-03-05 19:01:54 +0000

    leetcode

---
+++ leetcode/minimum-length-of-string-after-deleting-similar-ends.cpp
@@ -0,0 +1,30 @@
+// 1750. Minimum Length of String After Deleting Similar Ends
+// https://leetcode.com/problems/minimum-length-of-string-after-deleting-similar-ends/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+class Solution {
+public:
+    int minimumLength(string s) {
+        if (s.size() == 1)
+            return 1;
+        int left = 0, right = s.size() - 1;
+        while (left < right && s[left] == s[right]) {
+            char ch = s[left];
+            while (left < right && s[left] == ch)
+                ++left;
+            while (left < right && s[right] == ch)
+                --right;
+        }
+        if (left == right) {
+            if (s[left - 1] == s[right])
+                return 0;
+            else
+                return 1;
+        }
+        return right - left + 1;
+    }
+};
