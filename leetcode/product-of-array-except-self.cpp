af5733f67c56b35e2c5a34d15ae9562f0e0bed10
SGrade <sgrade@users.noreply.github.com>
2024-03-15 12:46:14 +0000

    leetcode

--- leetcode/product-of-array-except-self.cpp
+++ leetcode/product-of-array-except-self.cpp
@@ -9,23 +9,26 @@ using namespace std;
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+
         int n = nums.size();
         vector<int> left(n), right(n);
 
         left[0] = nums[0];
         for (int i = 1; i < n; ++i) left[i] = left[i-1] * nums[i];
-        
+
         right[n-1] = nums[n-1];
         for (int i = n-2; i >=0; --i) right[i] = nums[i] * right[i+1];
-        
+
         vector<int> ans(n);
         ans[0] = right[1];
         ans[n-1] = left[n-2];
-        
+
         for (int i = 1; i < n-1; ++i) {
             ans[i] = left[i-1] * right[i+1];
         }
-        
+
         return ans;
     }
 };
