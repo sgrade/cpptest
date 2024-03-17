db771ceef6e0832b062dd2e44e728dfec13a6f10
SGrade <sgrade@users.noreply.github.com>
2024-03-17 20:07:33 +0000

    leetcode

--- leetcode/insert-interval.cpp
+++ leetcode/insert-interval.cpp
@@ -6,50 +6,33 @@
 using namespace std;
 
 
+// Based on Editorial's Approach 2: Binary Search
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        if (intervals.size() == 0 || newInterval[0] > intervals.back()[1]) {
-            intervals.emplace_back(newInterval);
-            return intervals;
-        }
-        
-        
-        int left = newInterval[0], right = newInterval[1];
-        
-        vector<vector<int>>::iterator start_it, end_it;
-        
-        end_it = find_if(intervals.begin(), intervals.end(), [right](const vector<int> &v){
-            return v[1] >= right;
-        });
-        
-        if (end_it != intervals.end()) {
-            start_it = end_it;
-            if (right >= (*end_it)[0]) {
-                right = (*end_it)[1];
-                ++end_it;
-            }
-        }
-        else {
-            start_it = intervals.begin() + intervals.size() - 1;
-        }
-        
-        while (start_it != intervals.begin() && (*start_it)[0] > left) {
-            --start_it;
-        }
-        
-        if (left > (*start_it)[1]) {
-            ++start_it;
-        }
-        else {
-            left = min(left, (*start_it)[0]);
-        }
-        
-        int pos = distance(intervals.begin(), start_it);
-        intervals.erase(start_it, end_it);
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
 
-        intervals.insert(intervals.begin() + pos, vector<int>{left, right});
-        
-        return intervals;
+        if (intervals.empty())
+            return {newInterval};
+        int n = intervals.size(), start = newInterval[0], left = 0, right = n - 1, mid;
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            if (intervals[mid][0] < start)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        intervals.insert(intervals.begin() + left, newInterval);
+
+        vector<vector<int>> ans;
+        ans.emplace_back(intervals[0]);
+        for (int i = 1; i < n + 1; ++i) {
+            if (ans.back()[1] < intervals[i][0])
+                ans.emplace_back(intervals[i]);
+            else
+                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
+        }
+        return ans;
     }
 };
