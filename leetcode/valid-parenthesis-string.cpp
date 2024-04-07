68950934cca58e50fd17f48d46eae5a9f3ab4907
SGrade <sgrade@users.noreply.github.com>
2024-04-07 17:43:07 +0000

    leetcode

--- leetcode/valid-parenthesis-string.cpp
+++ leetcode/valid-parenthesis-string.cpp
@@ -6,43 +6,28 @@
 using namespace std;
 
 
+// Optimized with ideas from Editorial's Approach 4: Two Pointer
 class Solution {
 public:
     bool checkValidString(string s) {
         int balance = 0, stars = 0;
         for (int i = 0; i < s.size(); ++i) {
-            if (s[i] == '*') {
-                ++stars;
-                continue;
-            }
-            if (s[i] == '(')
+            if (s[i] == '(' || s[i] == '*')
                 ++balance;
             else
                 --balance;
-            if (balance < 0) {
-                if (stars <= 0)
-                    return false;
-                ++balance;
-                --stars;
-            }
+            if (balance < 0)
+                return false;
         }
 
         balance = 0, stars = 0;
         for (int i = s.size() - 1; i >= 0; --i) {
-            if (s[i] == '*') {
-                ++stars;
-                continue;
-            }
-            if (s[i] == ')')
+            if (s[i] == ')' || s[i] == '*')
                 ++balance;
             else
                 --balance;
-            if (balance < 0) {
-                if (stars <= 0)
-                    return false;
-                ++balance;
-                --stars;
-            }
+            if (balance < 0)
+                return false;
         }
 
         return true;
