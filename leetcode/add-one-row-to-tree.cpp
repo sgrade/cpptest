26be644c10389d2e0c68b78f04cd83a4639c0228
SGrade <sgrade@users.noreply.github.com>
2024-04-16 16:42:43 +0000

    leetcode

--- leetcode/add-one-row-to-tree.cpp
+++ leetcode/add-one-row-to-tree.cpp
@@ -6,17 +6,16 @@
 using namespace std;
 
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
 class Solution {
 public:
     TreeNode* addOneRow(TreeNode* root, int val, int depth) {
