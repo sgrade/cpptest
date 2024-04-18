1313fa7346e32796234da502f0fdfcf6670dd666
SGrade <sgrade@users.noreply.github.com>
2024-04-18 16:15:22 +0000

    leetcode

--- leetcode/island-perimeter.cpp
+++ leetcode/island-perimeter.cpp
@@ -15,12 +15,10 @@ public:
             for (int col = 0; col < cols; ++col) {
                 if (grid[row][col] == 1) {
                     ans += 4;
-                    if (row > 0 && grid[row - 1][col] == 1) {
+                    if (row > 0 && grid[row - 1][col] == 1)
                         ans -= 2;
-                    }
-                    if (col > 0 && grid[row][col - 1] == 1) {
+                    if (col > 0 && grid[row][col - 1] == 1)
                         ans -= 2;
-                    }
                 }
             }
         }
