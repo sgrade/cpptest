c58f81ef763ef1989dae348db7a2aa8b532e78aa
SGrade <sgrade@users.noreply.github.com>
2024-04-23 12:34:11 +0000

    leetcode

--- leetcode/course-schedule.cpp
+++ leetcode/course-schedule.cpp
@@ -6,56 +6,35 @@
 using namespace std;
 
 
-// Based on Approach 2: Postorder DFS (Depth-First Search)
-// https://leetcode.com/problems/course-schedule/solution/
-
+// Based on Editorial's Approach 1: Topological Sort Using Kahn's Algorithm
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        map<int, vector<int>> adj;
-        for (vector<int>& v: prerequisites) {
-            adj[v[1]].push_back(v[0]);
+        vector<int> indegree(numCourses);
+        vector<vector<int>> adj(numCourses);
+        for (const vector<int>& p: prerequisites) {
+            adj[p[1]].emplace_back(p[0]);
+            ++indegree[p[0]];
         }
-        
-        vector<bool> checked(numCourses);
-        vector<bool> path(numCourses);
-        
+
+        queue<int> q;
         for (int i = 0; i < numCourses; ++i) {
-            if (isCyclic(i, adj, checked, path)) {
-                return false;
+            if (indegree[i] == 0)
+                q.emplace(i);
+        }
+
+        int nodes_visited = 0;
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            ++nodes_visited;
+            for (const int& neighbor: adj[node]) {
+                --indegree[neighbor];
+                if (indegree[neighbor] == 0)
+                    q.emplace(neighbor);
             }
         }
-        return true;
-    }
-    
-    bool isCyclic(int& curCourse, map<int, vector<int>>& adj, vector<bool>& checked, vector<bool>& path) {
-        
-        // this course has been checked, so no cycle with it
-        if (checked[curCourse]) return false;
-        // found a marked course in the path - cycle
-        if (path[curCourse]) return true;
-        
-        // the course is in the path
-        path[curCourse] = true;
-        
-        bool ret = false;
-        for (int& child: adj[curCourse]) {
-            ret = isCyclic(child, adj, checked, path);
-            if (ret) break;
-        }
-        
-        // after visiting all children, we don't need it in the path
-        path[curCourse] = false;
-        
-        // all the children in the downstream visited -> 
-        // the course check is completed
-        checked[curCourse] = true;
-        
-        return ret;
+
+        return nodes_visited == numCourses;
     }
 };
-
-
-int main() {
-    return 0;
-}
