f42979d7bf73e6d803a1709682b0030987a43975
SGrade <sgrade@users.noreply.github.com>
2024-04-24 12:21:36 +0000

    leetcode

--- leetcode/n-th-tribonacci-number.cpp
+++ leetcode/n-th-tribonacci-number.cpp
@@ -9,11 +9,17 @@ using namespace std;
 class Solution {
 public:
     int tribonacci(int n) {
-        vector<int> t(max(3, n + 1));
-        t[1] = 1;
+        if (n < 3) {
+            if (n == 0) return 0;
+            if (n == 1 || n == 2) return 1;
+        }
+        vector<int> t(4);
         t[2] = 1;
-        for (int i = 3; i <= n; ++i) 
-            t[i] = t[i - 3] + t[i - 2] + t[i - 1];
-        return t[n];
+        t[3] = 1;
+        for (int i = 3; i <= n; ++i) {
+            rotate(t.begin(), t.begin() + 1, t.end());
+            t[3] = t[0] + t[1] + t[2];
+        }
+        return t.back();
     }
 };
