3ae80eb6846038d69075de3dee2134494824dd0e
SGrade <sgrade@users.noreply.github.com>
2024-04-28 16:42:15 +0000

    leetcode

--- leetcode/sum-of-distances-in-tree.cpp
+++ leetcode/sum-of-distances-in-tree.cpp
@@ -33,19 +33,19 @@ private:
     vector<vector<int>> graph;
     void dfs (int node, int parent) {
         for (const int& child: graph[node]) {
-            if (child != parent) {
-                dfs(child, node);
-                count[node] += count[child];
-                ans[node] += ans[child] + count[child];
-            }
+            if (child == parent)
+                continue;
+            dfs(child, node);
+            count[node] += count[child];
+            ans[node] += ans[child] + count[child];
         }
     }
     void dfs2 (int node, int parent) {
         for (const int& child: graph[node]) {
-            if (child != parent) {
-                ans[child] = ans[node] - count[child] + n - count[child];
-                dfs2(child, node);
-            }
+            if (child == parent)
+                continue;
+            ans[child] = ans[node] - count[child] + n - count[child];
+            dfs2(child, node);
         }
     }
 };
