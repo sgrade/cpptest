c4f6dcd2962fe03a5d36d55441da1dc19aae6d05
SGrade <sgrade@users.noreply.github.com>
2024-04-30 20:46:52 +0000

    leetcode

--- leetcode/number-of-wonderful-substrings.cpp
+++ leetcode/number-of-wonderful-substrings.cpp
@@ -7,15 +7,16 @@ using namespace std;
 
 
 // Based on Editorial's Approach: Count Parity Prefixes
+// Optimized with ideas from the fastest solutions
 class Solution {
 public:
     long long wonderfulSubstrings(string word) {
         int n = word.size();
         long long ans = 0LL;
 
-        unordered_map<int, int> freq;
+        vector<int> freq(1024);
         // Empty prefix
-        freq.emplace(0, 1);
+        freq[0] = 1;
         int mask = 0;
         for (int i = 0; i < n; ++i) {
             int bit_idx = word[i] - 'a';
@@ -23,11 +24,8 @@ public:
             mask ^= (1 << bit_idx);
             ans += freq[mask];
             ++freq[mask];
-            for (int char_idx = 0; char_idx < 10; ++char_idx) {
-                int target = mask ^ (1 << char_idx);
-                if (freq.find(target) != freq.end())
-                    ans += freq[target];
-            }
+            for (char ch = 'a'; ch <= 'j'; ++ch)
+                ans += freq[mask ^ (1 << (ch - 'a'))];
         }
         return ans;
     }
