470192cd25ee67c4270c8cf80d41ea308754b977
SGrade <sgrade@users.noreply.github.com>
2024-05-21 17:35:19 +0000

    leetcode

--- leetcode/subsets.cpp
+++ leetcode/subsets.cpp
@@ -9,24 +9,22 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>> ans;
-        vector<int> cur;
-        for (int k = 0; k < n + 1; ++k) {
-            backtrack(0, cur, nums, ans, k, n);
-        }
-        return ans;                
+        n = nums.size();
+        GetSubset(nums, 0, {});
+        return vector<vector<int>>(st.begin(), st.end());
     }
-    
-    void backtrack(int first, vector<int>& cur, const vector<int>& nums, vector<vector<int>>& ans, const int& k, const int& n) {
-        if (cur.size() == k) {
-            ans.push_back(cur);
+
+private:
+    int n;
+    set<vector<int>> st;
+    void GetSubset (vector<int>& nums, int idx, vector<int> current) {
+        if (idx == n) {
+            st.emplace(current);
             return;
         }
-        for (int i = first; i < n; ++i) {
-            cur.push_back(nums[i]);
-            backtrack(i + 1, cur, nums, ans, k, n);
-            cur.pop_back();
-        }
+        current.emplace_back(nums[idx]);
+        GetSubset(nums, idx + 1, current);
+        current.pop_back();
+        GetSubset(nums, idx + 1, current);
     }
 };
