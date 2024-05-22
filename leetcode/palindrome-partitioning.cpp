95d93cb21a9c870b2c907a1719bfaf2dbdc7dbac
SGrade <sgrade@users.noreply.github.com>
2024-05-22 16:54:26 +0000

    leetcode

--- leetcode/palindrome-partitioning.cpp
+++ leetcode/palindrome-partitioning.cpp
@@ -6,30 +6,31 @@
 using namespace std;
 
 
+// Based on Editorial's Approach 2: Backtracking with Dynamic Programming
 class Solution {
 public:
     vector<vector<string>> partition(string s) {
-        int n = s.size();
-        vector<vector<bool>> dp(n, vector<bool>(n));
-        vector<string> current_ans;
-        dfs(s, 0, current_ans, dp);
+        n = s.size();
+        dp.resize(n, vector<bool>(n));
+        vector<string> current;
+        int left = 0, right = 0;
+        dfs (s, 0, current);
         return ans;
     }
-    
-    void dfs(string &s, int left, vector<string> &current_ans, vector<vector<bool>> &dp) {
-        if (left >= s.size()) {
-            ans.emplace_back(current_ans);
-        }
-        for (int right = left; right < s.size(); ++right) {
-            if (s[left] == s[right] && (right - left < 3 || dp[left + 1][right - 1])) {
+private:
+    int n;
+    vector<vector<string>> ans;
+    vector<vector<bool>> dp;
+    void dfs (const string& s, int left, vector<string>& current) {
+        if (left >= n)
+            ans.emplace_back(current);
+        for (int right = left; right < n; ++right) {
+            if (s[left] == s[right] && (right - left <= 2 || dp[left + 1][right - 1])) {
                 dp[left][right] = true;
-                current_ans.emplace_back(s.substr(left, right - left + 1));
-                dfs(s, right + 1, current_ans, dp);
-                current_ans.pop_back();
+                current.emplace_back(s.substr(left, right - left + 1));
+                dfs(s, right + 1, current);
+                current.pop_back();
             }
         }
     }
-        
-private:
-    vector<vector<string>> ans;
 };
