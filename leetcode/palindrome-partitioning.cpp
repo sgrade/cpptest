a7fbf74b61b3ba5531f7899d692932828b832c92
SGrade <sgrade@users.noreply.github.com>
2024-05-23 16:44:23 +0000

    leetcode

--- leetcode/palindrome-partitioning.cpp
+++ leetcode/palindrome-partitioning.cpp
@@ -6,31 +6,37 @@
 using namespace std;
 
 
-// Based on Editorial's Approach 2: Backtracking with Dynamic Programming
+// Based on a fast sample solution
 class Solution {
 public:
     vector<vector<string>> partition(string s) {
         n = s.size();
-        dp.resize(n, vector<bool>(n));
         vector<string> current;
-        int left = 0, right = 0;
         dfs (s, 0, current);
         return ans;
     }
+
 private:
     int n;
     vector<vector<string>> ans;
-    vector<vector<bool>> dp;
+
     void dfs (const string& s, int left, vector<string>& current) {
-        if (left >= n)
+        if (left == n)
             ans.emplace_back(current);
         for (int right = left; right < n; ++right) {
-            if (s[left] == s[right] && (right - left <= 2 || dp[left + 1][right - 1])) {
-                dp[left][right] = true;
-                current.emplace_back(s.substr(left, right - left + 1));
+            current.emplace_back(s.substr(left, right - left + 1));
+            if (IsPalindrome(current.back()))
                 dfs(s, right + 1, current);
-                current.pop_back();
-            }
+            current.pop_back();
         }
+    }
+
+    bool IsPalindrome(const string& s) {
+        int n = s.size();
+        for (int i = 0; i < n; ++i) {
+            if (s[i] != s[n - 1 - i])
+                return false;
+        }
+        return true;
     }
 };
