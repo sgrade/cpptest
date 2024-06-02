c956be919feaf22bee1ba51483bee6d6e4d57f32
SGrade <sgrade@users.noreply.github.com>
2024-06-02 16:46:00 +0000

    leetcode

---
+++ leetcode/reverse-string.cpp
@@ -0,0 +1,14 @@
+// 344. Reverse String
+// https://leetcode.com/problems/reverse-string/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        reverse(s.begin(), s.end());
+    }
+};
