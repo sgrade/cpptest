bdae0b22b2b3e1492a592f88212aec3fd6d2f358
SGrade <sgrade@users.noreply.github.com>
2024-06-04 19:19:19 +0000

    leetcode

--- leetcode/freedom-trail.cpp
+++ leetcode/freedom-trail.cpp
@@ -1,47 +1,57 @@
-// 1289. Minimum Falling Path Sum II
+// 514. Freedom Trail
 // https://leetcode.com/problems/freedom-trail/
 
 #include <bits/stdc++.h>
 
 using namespace std;
 
-
-// Key idea is from Editorial's Approach 4: Space-Optimized Bottom-Up Dynamic Programming
+// Based on Editorial's Approach 4: Shortest Path
 class Solution {
 public:
-    int minFallingPathSum(vector<vector<int>>& grid) {
-        int n = grid.size();
-        pair<int, int> prev_val(0, 0);
-        pair<int, int> prev_idx(-1, -1);
+    int findRotateSteps(string ring, string key) {
+        n_ring = ring.size(), n_key = key.size();
 
-        for (int row = 0; row < n; ++row) {
-            pair<int, int> cur_val (20000, 20000);
-            pair<int, int> cur_idx(-1, -1);
+        unordered_map<char, vector<int>> char_indexes;
+        for (int i = 0 ;i < n_ring; ++i)
+            char_indexes[ring[i]].emplace_back(i);
 
-            for (int col = 0; col < n; ++col) {
-                int x = grid[row][col];
-                if (col != prev_idx.first)
-                    x += prev_val.first;
-                else {
-                    x += prev_val.second;
-                }
+        // <total_steps, <ring_idx, key_idx>>
+        priority_queue<
+            pair<int, pair<int, int>>,
+            vector<pair<int, pair<int, int>>>,
+            greater<pair<int, pair<int, int>>>
+        > pq;
+        pq.emplace(0, pair<int, int>(0, 0));
 
-                if (x <= cur_val.first) {
-                    cur_val.second = cur_val.first;
-                    cur_idx.second = cur_idx.first;
-                    cur_val.first = x;
-                    cur_idx.first = col;
-                }
-                else if (x <= cur_val.second) {
-                    cur_val.second = x;
-                    cur_idx.second = col;
-                }
+        unordered_set<string> visited;
+
+        int total_steps = 0;
+        while (!pq.empty()) {
+            total_steps = pq.top().first;
+            const auto [ring_idx, key_idx] = pq.top().second;
+            pq.pop();
+            if (key_idx == n_key)
+                break;
+
+            string current_state = to_string(ring_idx) + "-" + to_string(key_idx);
+            if (visited.find(current_state) != visited.end())
+                continue;
+            visited.emplace(current_state);
+
+            for (const int& next_idx: char_indexes[key[key_idx]]) {
+                int min_steps = getMinSteps (ring_idx, next_idx);
+                pq.emplace(total_steps + min_steps, pair<int, int>(next_idx, key_idx + 1));
             }
-
-            prev_val = cur_val;
-            prev_idx = cur_idx;
         }
 
-        return prev_val.first;
+        return total_steps + n_key;
+    }
+
+private:
+    int n_ring, n_key;
+    int getMinSteps (const int& ring_idx, const int& next_idx) {
+        int steps = abs(ring_idx - next_idx);
+        int steps_other_direction = n_ring - steps;
+        return min(steps, steps_other_direction);
     }
 };
