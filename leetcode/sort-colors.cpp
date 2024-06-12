ee12a1f0e41efa328666754fdd1f8aa40f441766
SGrade <sgrade@users.noreply.github.com>
2024-06-12 15:54:41 +0000

    leetcode

--- leetcode/sort-colors.cpp
+++ leetcode/sort-colors.cpp
@@ -9,26 +9,13 @@ using namespace std;
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int left = 0, mid, right = nums.size() - 1;
-        
-        while (left < nums.size() && nums[left] == 0) ++left;
-        while (right >= 0 && nums[right] == 2) --right;
-        
-        while (left < right) 
-        {
-            if (nums[left] == 1) 
-            {
-                mid = left + 1;
-                while (mid <= right && nums[mid] == 1) ++mid;
-                if (mid > right) break;
-                swap (nums[left], nums[mid]);
-            }
-            else if (nums[left] == 2)
-            {
-                swap (nums[left], nums[right]); 
-                --right;
-            }
-            if (nums[left] == 0) ++left;
+        vector<int> counter(3);
+        for (const int& num: nums)
+            ++counter[num];
+        int idx = 0;
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < counter[i]; ++j)
+                nums[idx++] = i;
         }
     }
 };
