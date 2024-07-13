1990ff1d261ef2187dabf25f6c8a3e8225f34530
Roman K <sgrade@users.noreply.github.com>
2024-07-13 18:55:44 +0000

    leetcode

--- leetcode/maximum-score-from-removing-substrings.cpp
+++ leetcode/maximum-score-from-removing-substrings.cpp
@@ -6,44 +6,58 @@
 using namespace std;
 
 
+// Used the idea from the Editorial's Approach 3: Greedy Way (Counting)
 class Solution {
 public:
     int maximumGain(string s, int x, int y) {
         ios::sync_with_stdio(false);
         cin.tie(nullptr);
-
-        int ans = 0;
+       
+        n = s.size();
+        ans = 0;
         string pattern, anti_pattern;
         if (x > y) {
             pattern = "ab";
-            anti_pattern = "ba";
+            this->x = x;
+            this->y = y;
         }
         else {
             pattern = "ba";
-            anti_pattern = "ab";
-            swap (x, y);
+            this->x = y;
+            this->y = x;
         }
-
-        int pos = 0;
-        while (pos != string::npos) {
-            pos = s.find(pattern, pos);
-            if (pos == string::npos)
-                break;
-            s.erase(pos, 2);
-            ans += x;
-            pos = max(0, pos - 1);
-        }
-
-        pos = 0;
-        while (pos != string::npos) {
-            pos = s.find(anti_pattern, pos);
-            if (pos == string::npos)
-                break;
-            s.erase(pos, 2);
-            ans += y;
-            pos = max(0, pos - 1);
-        }
+        
+        traverse (s, pattern);
+        // swap (pattern[0], pattern[1]);
+        // traverse (s, pattern, y);
 
         return ans;
+    }
+
+private:
+    int n, x, y, ans = 0;
+    void traverse (const string& s, const string& pattern) {
+        char ch1 = pattern[0], ch2 = pattern[1];
+        int cnt_ch1 = 0, cnt_ch2 = 0;
+        for (int i = 0; i < n; ++i) {
+            if (s[i] == ch1) {
+                ++cnt_ch1;
+            }
+            else if (s[i] == ch2) {
+                if (cnt_ch1 > 0) {
+                    --cnt_ch1;
+                    ans += x;
+                }
+                else {
+                    ++cnt_ch2;
+                }
+            }
+            else {
+                ans += min(cnt_ch1, cnt_ch2) * y;
+                cnt_ch1 = 0;
+                cnt_ch2 = 0;
+            }
+        }
+        ans += min(cnt_ch1, cnt_ch2) * y;
     }
 };
