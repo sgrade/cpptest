a56ffd68cca0ccae68650402c2c93c728622c5b7
SGrade <sgrade@users.noreply.github.com>
2024-07-28 20:07:25 +0000

    leetcode

--- leetcode/second-minimum-time-to-reach-destination.cpp
+++ leetcode/second-minimum-time-to-reach-destination.cpp
@@ -6,8 +6,7 @@
 using namespace std;
 
 
-// Wrong Answer
-// 69 / 76 testcases passed
+// Fixed wrong answer with some ideas from Editorial's Approach 2: Breadth First Search
 class Solution {
 public:
     int secondMinimum(int n, vector<vector<int>>& edges, int time, int change) {
@@ -20,38 +19,35 @@ public:
             adj[edge[1]].emplace_back(edge[0]);
         }
 
-        int ans = 0;
+        int ans = -1;
         int current_time = 0;
         queue<int> q;
-        vector<int> visited (n + 1);
+        vector<pair<int, int>> distance (n + 1, {-1, -1});
         q.emplace(1);
-        visited[1] = 1;
         while (!q.empty()) {
             bool green = (current_time / change) % 2 == 0;
             if (green)
                 current_time += time;
-            else {
-                int rem = current_time % change;
-                current_time += (change - rem) + time;
-            }
+            else
+                current_time = change * (current_time / change + 1) + time;
             int nodes = q.size();
             for (int i = 0; i < nodes; ++i) {
                 int node = q.front();
                 q.pop();
                 for (const int& neighbor: adj[node]) {
-                    if (neighbor == n) {
-                        if (ans == 0 || current_time != ans)
-                            ans = current_time;
-                        else
-                            return current_time;
+                    if (distance[neighbor].first == -1) {
+                        distance[neighbor].first = current_time;
+                        q.emplace(neighbor);
                     }
-                    if (visited[neighbor] == 2)
-                        continue;
-                    q.emplace(neighbor);
-                    ++visited[neighbor];
+                    else if (distance[neighbor].second == -1 && distance[neighbor].first != current_time) {
+                        if (neighbor == n)
+                            return current_time;
+                        distance[neighbor].second = current_time;
+                        q.emplace(neighbor);
+                    }
                 }
             }
         }
-        return ans;
+        return -1;
     }
 };
