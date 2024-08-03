04ee5de8832277e7b0fa81b54a059e278c2899e3
RomAn <sgrade@users.noreply.github.com>
2024-08-03 17:22:01 +0000

    leetcode

--- leetcode/longest-repeating-substring.cpp
+++ leetcode/longest-repeating-substring.cpp
@@ -6,18 +6,23 @@
 using namespace std;
 
 
+// Based on Approach 5: Dynamic Programming
 class Solution {
 public:
     int longestRepeatingSubstring(string s) {
-        int n = s.size();
-        for (int len = n - 1; len > 0; --len) {
-            for (int i = 0; i < n - len; ++i) {
-                string candidate = s.substr(i, len);
-                if (s.find(candidate, i + 1) != string::npos) {
-                    return len;
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+
+        int n = s.size(), ans = 0;
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1));
+        for (int i = 1; i <= n; ++i) {
+            for (int j = i + 1; j <= n; ++j) {
+                if (s[i - 1] == s[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    ans = max (ans, dp[i][j]);
                 }
             }
         }
-        return 0;
+        return ans;
     }
 };
