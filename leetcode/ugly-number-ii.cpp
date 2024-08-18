11c7953688d0994fab57fe09cdce8f615d6e172b
RomAn <sgrade@users.noreply.github.com>
2024-08-18 21:20:12 +0700

    leetcode

--- leetcode/ugly-number-ii.cpp
+++ leetcode/ugly-number-ii.cpp
@@ -6,29 +6,32 @@
 using namespace std;
 
 
-// Key ideas are from https://leetcode.com/problems/ugly-number-ii/solutions/69364/my-16ms-c-dp-solution-with-short-explanation/
+// Optimized with Editorial's Approach 3: Dynamic Programming (DP)
 class Solution {
 public:
     int nthUglyNumber(int n) {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        
         int t2 = 0, t3 = 0, t5 = 0;
-        int t2_by_2, t3_by_3, t5_by_5;
-        vector<int> ans(n);
-        ans[0] = 1;
+        int t2_by_2 = 2, t3_by_3 = 3, t5_by_5 = 5;
+        vector<int> dp(n);
+        dp[0] = 1;
         for (int i = 1; i < n; ++i) {
-            t2_by_2 = ans[t2] * 2;
-            t3_by_3 = ans[t3] * 3;
-            t5_by_5 = ans[t5] * 5;
-            ans[i] = min({t2_by_2, t3_by_3, t5_by_5});
-            if (ans[i] == t2_by_2) {
+            dp[i] = min({t2_by_2, t3_by_3, t5_by_5});
+            if (dp[i] == t2_by_2) {
                 ++t2;
+                t2_by_2 = dp[t2] * 2;
             }
-            if (ans[i] == t3_by_3) {
+            if (dp[i] == t3_by_3) {
                 ++t3;
+                t3_by_3 = dp[t3] * 3;
             }
-            if (ans[i] == t5_by_5) {
+            if (dp[i] == t5_by_5) {
                 ++t5;
+                t5_by_5 = dp[t5] * 5;
             }
         }
-        return ans.back();
+        return dp.back();
     }
 };
