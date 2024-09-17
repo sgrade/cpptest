ce7800aca77d7b8563747e5b56a3886e3d4c270c
SGrade <sgrade@users.noreply.github.com>
2024-09-17 10:24:34 +0000

    templates: reuse stringstream

--- templates/strings/tokenizing_iss.cpp
+++ templates/strings/tokenizing_iss.cpp
@@ -6,12 +6,13 @@ using namespace std;
 int main() {
 
     string s = "1,2,3,null,null,4,5";
-    
+
     istringstream iss(s);
     string tmp;
     while (getline(iss, tmp, ',')) {
         cout << tmp << '\n';
     }
+    cout << endl;
 
     /* Output:
     1
@@ -22,6 +23,14 @@ int main() {
     4
     5
     */
+
+    cout << "Reuse the stringstream: " << endl;
+    string s2 = "1 2 3";
+    iss.clear();
+    iss.str(s2);
+    while (iss >> tmp)
+        cout << tmp << ' ';
+    cout << endl;
 
     return 0;
 }
