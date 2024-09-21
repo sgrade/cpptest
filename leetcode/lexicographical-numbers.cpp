50092807a20a47400d880c00bbb9f268c067165a
SGrade <sgrade@users.noreply.github.com>
2024-09-21 16:04:32 +0000

    leetcode

--- leetcode/lexicographical-numbers.cpp
+++ leetcode/lexicographical-numbers.cpp
@@ -6,31 +6,25 @@
 using namespace std;
 
 
-// Based on Editorial's Approach 1: DFS Approach
+// Based on Editorial's Approach 2: Iterative Approach
 class Solution {
 public:
     vector<int> lexicalOrder(int n) {
         ios::sync_with_stdio(false);
         cin.tie(nullptr);
 
-        this->n = n;
-        for (int start = 1; start < 10; ++start)
-            dfs(start);
-        return ans;
-    }
-
-private:
-    int n;
-    vector<int> ans;
-    void dfs (int start) {
-        if (start > n)
-            return;
-        ans.emplace_back(start);
-        for (int next_digit = 0; next_digit < 10; ++next_digit) {
-            int next_num = start * 10 + next_digit;
-            if (next_num > n)
-                break;
-            dfs(next_num);
+        vector<int> ans;
+        int x = 1;
+        for (int i = 0; i < n; ++i){
+            ans.emplace_back(x);
+            if (x * 10 <= n) {
+                x *= 10;
+                continue;
+            }
+            while (x % 10 == 9 || x >= n)
+                x /= 10;
+            x += 1;
         }
+        return ans;
     }
 };
