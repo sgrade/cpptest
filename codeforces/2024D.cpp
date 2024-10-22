fe60bb9747c17e8b221586231f55c57371fbaf34
SGrade <sgrade@users.noreply.github.com>
2024-10-22 22:15:24 +0000

    codeforces

--- codeforces/2024D.cpp
+++ codeforces/2024D.cpp
@@ -1,8 +1,10 @@
 // D. Skipping
+// https://codeforces.com/contest/2024/problem/D
 
 #include <bits/stdc++.h>
 
 using namespace std;
+using ll = long long;
 
 
 int main() {
@@ -18,39 +20,39 @@ int main() {
         int n;
         cin >> n;
 
-        vector<int> a(n + 1), b(n + 1);
+        vector<ll> a(n + 1), b(n + 1);
         for (int i = 1; i <= n; ++i)
             cin >> a[i];
         for (int i = 1; i <= n; ++i)
             cin >> b[i];
 
         // Editorial - https://codeforces.com/blog/entry/135341
-        // Based on https://codeforces.com/blog/entry/135341
-        vector<int> seen(n + 1, 1e9 + 1);
-        seen[1] = 0;
-        int i = 2, cur_max = 1;
-        priority_queue<pair<int, int>> pq;
+        vector<ll> penalty(n + 1, 2e18 + 1);
+        penalty[1] = 0;
+
+        int i = 2, max_i = 1;
+        priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<>> pq;
         if (b[1] > 1)
-            pq.emplace(-1 * a[1], b[1]);
+            pq.emplace(a[1], b[1]);
         while (!pq.empty()) {
-            auto cur = pq.top();
+            auto [points, b_i] = pq.top();
             pq.pop();
-            if (cur.second > cur_max) {
-                cur_max = cur.second;
-                while (i <= cur_max) {
-                    seen[i] = -1 * cur.first;
-                    if (b[i] > cur_max)
-                        pq.emplace(-1 * (seen[i] + a[i]), b[i]);
+            if (b_i > max_i) {
+                max_i = b_i;
+                while (i <= max_i) {
+                    penalty[i] = points;
+                    if (b[i] > max_i)
+                        pq.emplace(penalty[i] + a[i], b[i]);
                     ++i;
                 }
             }
         }
 
-        int ans = 0;
+        ll ans = 0;
         for (int i = 1; i <= n; ++i)
             a[i] += a[i - 1];
         for (int i = 1; i <= n; ++i)
-            ans = max(ans, a[i] - seen[i]);
+            ans = max(ans, a[i] - penalty[i]);
         cout << ans << endl;
     }
 
