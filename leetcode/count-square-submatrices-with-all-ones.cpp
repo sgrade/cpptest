6574f9b6d012b2d55518f4dba6af38191c4218d1
SGrade <sgrade@users.noreply.github.com>
2024-10-27 14:22:06 +0000

    leetcode

--- leetcode/count-square-submatrices-with-all-ones.cpp
+++ leetcode/count-square-submatrices-with-all-ones.cpp
@@ -6,32 +6,28 @@
 using namespace std;
 
 
-// Based on Editorial's Approach 1: Top-Down Dynamic Programming
+// Based on Editorial's Approach 2: Bottom-up Approach
 class Solution {
 public:
     int countSquares(vector<vector<int>>& matrix) {
         ios::sync_with_stdio(false);
         cin.tie(nullptr);
-        
-        rows = matrix.size(), cols = matrix[0].size();
-        dp.resize(rows, vector<int>(cols, -1));
+
+        int rows = matrix.size(), cols = matrix[0].size();
+        int ans = 0;
+        vector<vector<int>> dp (rows + 1, vector<int>(cols + 1));
         for (int row = 0; row < rows; ++row) {
-            for (int col = 0; col < cols; ++col)
-                ans += Get(row, col, matrix);
+            for (int col = 0; col < cols; ++col) {
+                if (matrix[row][col] == 0)
+                    continue;
+                dp[row + 1][col + 1] = 1 + min ({
+                    dp[row][col],
+                    dp[row][col + 1],
+                    dp[row + 1][col]
+                });
+                ans += dp[row + 1][col + 1];
+            }
         }
         return ans;
-    }
-private:
-    int ans = 0, rows, cols;
-    vector<vector<int>> dp;
-    int Get (int row, int col, const vector<vector<int>>& matrix) {
-        if (row >= rows || col >= cols || matrix[row][col] == 0)
-            return 0;
-        if (dp[row][col] != -1)
-            return dp[row][col];
-        int right = Get (row, col + 1, matrix);
-        int below = Get (row + 1, col, matrix);
-        int diag = Get (row + 1, col + 1, matrix);
-        return dp[row][col] = 1 + min({right, below, diag});
     }
 };
