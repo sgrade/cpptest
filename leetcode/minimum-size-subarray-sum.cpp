6a4715932fa6d771f2d29a649e42dfda5372e0ea
SGrade <sgrade@users.noreply.github.com>
2024-11-17 21:17:31 +0000

    leetcode

--- leetcode/minimum-size-subarray-sum.cpp
+++ leetcode/minimum-size-subarray-sum.cpp
@@ -6,18 +6,21 @@
 using namespace std;
 
 
-// Optimization of the solution is based on Leetcode's Approach #4 Using 2 pointers
+// Based on Editorial's Approach: Sliding Window
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+
+        int left = 0, right, sum = 0;
         int ans = numeric_limits<int>::max();
-        int sm = 0;
-        int left = 0;
-        for (int right = 0; right < nums.size(); ++right) {
-            sm += nums[right];
-            while (sm >= target) {
+        for (right = 0; right < nums.size(); ++right) {
+            sum += nums[right];
+            while (sum >= target) {
                 ans = min(ans, right - left + 1);
-                sm -= nums[left++];
+                sum -= nums[left];
+                ++left;
             }
         }
         return ans == numeric_limits<int>::max() ? 0 : ans;
