96a630033007aaad43a3f28c652bd57df6095bac
SGrade <sgrade@users.noreply.github.com>
2024-12-01 10:15:30 +0000

    leetcode

--- leetcode/find-the-celebrity.cpp
+++ leetcode/find-the-celebrity.cpp
@@ -12,37 +12,36 @@ using namespace std;
 class Solution {
 public:
     int findCelebrity(int n) {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+
         int celebrity_candidate = 0;
         bool current;
         for (int i = 0; i < n; ++i) {
-            if (KnowsWithCache(celebrity_candidate, i)) {
+            if (knowsWithCache(celebrity_candidate, i))
                 celebrity_candidate = i;
-            }
         }
-        if (IsCelebrity(celebrity_candidate, n)) {
+        if (isCelebrity(celebrity_candidate, n))
             return celebrity_candidate;
-        }
         return -1;
     }
-    
+
 private:
     map<pair<int, int>, bool> cache;
-    
-    bool KnowsWithCache (const int &a, const int &b) {
+
+    bool knowsWithCache (const int &a, const int &b) {
         pair<int, int> p = {a, b};
-        if (cache.find(p) == cache.end()) {
+        if (cache.find(p) == cache.end())
             cache[p] = knows(a, b);
-        }
         return cache[p];
     }
-    
-    bool IsCelebrity (const int &candidate_celebrity, const int &n) {
+
+    bool isCelebrity (const int &candidate_celebrity, const int &n) {
         for (int i = 0; i < n; ++i) {
             if (i == candidate_celebrity)
                 continue;
-            if (KnowsWithCache(candidate_celebrity, i) || !KnowsWithCache(i, candidate_celebrity)) {
+            if (knowsWithCache(candidate_celebrity, i) || !knowsWithCache(i, candidate_celebrity))
                 return false;
-            }
         }
         return true;
     }
