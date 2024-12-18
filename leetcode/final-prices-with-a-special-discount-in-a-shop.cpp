ab894d7303f1f3123dd078e91cb8e16226e8c0bf
SGrade <sgrade@users.noreply.github.com>
2024-12-18 11:15:22 +0000

    leetcode

---
+++ leetcode/final-prices-with-a-special-discount-in-a-shop.cpp
@@ -0,0 +1,25 @@
+// 1475. Final Prices With a Special Discount in a Shop
+// https://leetcode.com/problems/final-prices-with-a-special-discount-in-a-shop/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+class Solution {
+public:
+    vector<int> finalPrices(vector<int>& prices) {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+
+        for (int i = 0; i < prices.size(); ++i) {
+            for (int j = i + 1; j < prices.size(); ++j) {
+                if (prices[j] <= prices[i]) {
+                    prices[i] -= prices[j];
+                    break;
+                }
+            }
+        }
+        return prices;
+    }
+};
