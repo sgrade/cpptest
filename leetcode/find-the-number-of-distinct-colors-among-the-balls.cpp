5dc1598f33adc2a8e35c56d85a458989a0c1b6a9
SGrade <sgrade@users.noreply.github.com>
2025-02-07 11:40:29 +0000

    leetcode

--- leetcode/find-the-number-of-distinct-colors-among-the-balls.cpp
+++ leetcode/find-the-number-of-distinct-colors-among-the-balls.cpp
@@ -12,28 +12,21 @@ public:
     vector<int> queryResults(int limit, vector<vector<int>>& queries) {
         ios::sync_with_stdio(false);
         cin.tie(nullptr);
-        
+
         int n = queries.size();
         vector<int> balls(limit + 1), ans(n);
-        balls[queries[0][0]] = queries[0][1], ans[0] = 1;
         unordered_map<int, int> colors;
-        ++colors[queries[0][1]];
 
-        for (int i = 1; i < n; ++i) {
+        for (int i = 0; i < n; ++i) {
             int ball = queries[i][0], color = queries[i][1];
-            if (balls[ball] == color) {
-                ans[i] = ans[i - 1];
-                continue;
-            }
-
-            int prev_color = balls[ball];
-            if (prev_color != 0) {
+            if (balls[ball] != 0) {
+                int prev_color = balls[ball];
                 --colors[prev_color];
                 if (colors[prev_color] == 0)
                     colors.erase(prev_color);
             }
-            ++colors[color];
             balls[ball] = color;
+            ++colors[color];
             ans[i] = colors.size();
         }
 
