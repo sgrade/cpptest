651e749ff65d732d542aa4f28cef562db1ac429d
SGrade <sgrade@users.noreply.github.com>
2025-02-12 09:44:23 +0000

    leetcode

--- leetcode/max-sum-of-a-pair-with-equal-sum-of-digits.cpp
+++ leetcode/max-sum-of-a-pair-with-equal-sum-of-digits.cpp
@@ -11,24 +11,22 @@ public:
     int maximumSum(vector<int>& nums) {
         ios::sync_with_stdio(false);
         cin.tie(nullptr);
-        
-        int n = nums.size(), ans = -1;
-        unordered_map<int, multiset<int>> counter;
+
+        unordered_map<int, vector<int>> counter;
         for (const int& num: nums) {
             int x = num, sum = 0;
             while (x) {
                 sum += x % 10;
                 x /= 10;
             }
-            counter[sum].emplace(num);
+            counter[sum].emplace_back(num);
         }
 
-        for (const auto& [_, st]: counter) {
-            if (st.size() > 1) {
-                multiset<int>::reverse_iterator rit = st.rbegin();
-                int current_ans = *rit;
-                ++rit;
-                current_ans += *rit;
+        int ans = -1;
+        for (auto& [_, v]: counter) {
+            if (v.size() > 1) {
+                sort(v.begin(), v.end());
+                int current_ans = v[v.size() - 1] + v[v.size() - 2];
                 ans = max(ans, current_ans);
             }
         }
