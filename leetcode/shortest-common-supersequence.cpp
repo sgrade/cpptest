f87753a09af0e9a9fbea514bd3596b504977af37
SGrade <sgrade@users.noreply.github.com>
2025-02-28 10:18:08 +0000

    leetcode

--- leetcode/shortest-common-supersequence.cpp
+++ leetcode/shortest-common-supersequence.cpp
@@ -6,32 +6,56 @@
 using namespace std;
 
 
-// MLE
-// Approach 3: Bottom-Up Dynamic Programming
+// Based on Editorial's Approach 4: Most Optimal - Space Optimized Dynamic Programming
 class Solution {
-public:
-    string shortestCommonSupersequence(string str1, string str2) {
-        ios::sync_with_stdio(false);
-        cin.tie(nullptr);
+    public:
+        string shortestCommonSupersequence(string str1, string str2) {
+            ios::sync_with_stdio(false);
+            cin.tie(nullptr);
 
-        int n1 = str1.size(), n2 = str2.size();
-        vector<string> prev_row(n2 + 1);
-        for (int i2 = 0; i2 <= n2; ++i2)
-            prev_row[i2] = str2.substr(0, i2);
-        for (int i1 = 1; i1 <= n1; ++i1) {
-            vector<string> cur_row(n2 + 1);
-            cur_row[0] = str1.substr(0, i1);
-            for (int i2 = 1; i2 <= n2; ++i2) {
-                if (str1[i1 - 1] == str2[i2 - 1])
-                    cur_row[i2] = prev_row[i2 - 1] + str1[i1 - 1];
-                else {
-                    string s1 = prev_row[i2];
-                    string s2 = cur_row[i2 - 1];
-                    cur_row[i2] = (s1.size() < s2.size()) ? s1 + str1[i1 - 1] : s2 + str2[i2 - 1];
+            int n1 = str1.size(), n2 = str2.size();
+            vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1));
+            for (int i1 = 0; i1 <= n1; ++i1)
+                dp[i1][0] = i1;
+            for (int i2 = 0; i2 <= n2; ++i2)
+                dp[0][i2] = i2;
+            for (int i1 = 1; i1 <= n1; ++i1) {
+                for (int i2 = 1; i2 <= n2; ++i2) {
+                    if (str1[i1 - 1] == str2[i2 - 1])
+                        dp[i1][i2] = dp[i1 - 1][i2 - 1] + 1;
+                    else
+                        dp[i1][i2] = min(dp[i1 - 1][i2], dp[i1][i2 - 1]) + 1;
                 }
             }
-            prev_row = cur_row;
+
+            string supersequence = "";
+            int i1 = n1, i2 = n2;
+            while (i1 > 0 && i2 > 0) {
+                if (str1[i1 - 1] == str2[i2 - 1]) {
+                    supersequence += str1[i1 - 1];
+                    --i1;
+                    --i2;
+                }
+                else if (dp[i1 - 1][i2] < dp[i1][i2 - 1]) {
+                    supersequence += str1[i1 - 1];
+                    --i1;
+                }
+                else {
+                    supersequence += str2[i2 - 1];
+                    --i2;
+                }
+            }
+
+            while (i1 > 0) {
+                supersequence += str1[i1 - 1];
+                --i1;
+            }
+            while (i2 > 0) {
+                supersequence += str2[i2 - 1];
+                --i2;
+            }
+
+            reverse(supersequence.begin(), supersequence.end());
+            return supersequence;
         }
-        return prev_row[n2];
-    }
-};
+    };
