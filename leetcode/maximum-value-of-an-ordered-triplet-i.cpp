c60cf99f554c7f86d62eb11a3f05de020735939b
SGrade <sgrade@users.noreply.github.com>
2025-04-02 16:07:26 +0000

    leetcode

--- leetcode/maximum-value-of-an-ordered-triplet-i.cpp
+++ leetcode/maximum-value-of-an-ordered-triplet-i.cpp
@@ -6,21 +6,19 @@
 using namespace std;
 
 
+// Based on Editorial's Approach 4: Greedy
 class Solution {
     public:
         long long maximumTripletValue(vector<int>& nums) {
             ios::sync_with_stdio(false);
             cin.tie(nullptr);
 
-            int n = nums.size();
+            int n = nums.size(), max_left = 0, max_diff = 0;
             long long ans = 0;
-            for (int i = 0; i < n - 2; ++i) {
-                for (int j = i + 1; j < n - 1; ++j) {
-                    for (int k = j + 1; k < n; ++k) {
-                        long long current_ans = 1LL * (nums[i] - nums[j]) * nums[k];
-                        ans = max(ans, current_ans);
-                    }
-                }
+            for (int k = 0; k < n; ++k) {
+                ans = max(ans, 1LL * max_diff * nums[k]);
+                max_diff = max(max_diff, max_left - nums[k]);
+                max_left = max(max_left, nums[k]);
             }
             return ans;
         }
