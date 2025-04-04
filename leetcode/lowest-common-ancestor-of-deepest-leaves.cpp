eb1db5a30069bf71ab93420b64379817f428b4a9
SGrade <sgrade@users.noreply.github.com>
2025-04-04 15:27:08 +0000

    leetcode

---
+++ leetcode/lowest-common-ancestor-of-deepest-leaves.cpp
@@ -0,0 +1,39 @@
+// 1123. Lowest Common Ancestor of Deepest Leaves
+// https://leetcode.com/problems/lowest-common-ancestor-of-deepest-leaves/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// Based on Editorial's Approach 1: Recursion
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+ class Solution {
+    public:
+        TreeNode* lcaDeepestLeaves(TreeNode* root) {
+            ios::sync_with_stdio(false);
+            cin.tie(nullptr);
+
+            return dfs(root).first;
+        }
+    private:
+        pair<TreeNode*, int> dfs(TreeNode* root) {
+            if (root == nullptr)
+                return {root, 0};
+            pair<TreeNode*, int> left = dfs(root->left), right = dfs(root->right);
+            if (left.second > right.second)
+                return {left.first, left.second + 1};
+            if (left.second < right.second)
+                return {right.first, right.second + 1};
+            return {root, left.second + 1};
+        }
+    };
