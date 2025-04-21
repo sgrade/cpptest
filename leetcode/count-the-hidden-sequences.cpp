d99494568f31ad9b4f7568cac8a2de49c481cd84
SGrade <sgrade@users.noreply.github.com>
2025-04-21 14:45:23 +0000

    leetcode

--- leetcode/count-the-hidden-sequences.cpp
+++ leetcode/count-the-hidden-sequences.cpp
@@ -6,23 +6,19 @@
 using namespace std;
 
 
+// Optimized with the Editorial
 class Solution {
     public:
         int numberOfArrays(vector<int>& differences, int lower, int upper) {
-            long long cur = lower, mn = lower, mx = lower;
+            int cur = lower, mn = lower, mx = lower, max_diff = upper - lower;
             for (const int& d: differences) {
                 cur += d;
                 mn = min(mn, cur);
                 mx = max(mx, cur);
+                if (mx - mn > max_diff)
+                    return 0;
             }
 
-            int ans = 1;
-            if (mn < lower)
-                ans -= lower - mn;
-            if (mx > upper)
-                ans -= mx - upper;
-            else
-                ans += upper - mx;
-            return max(0, ans);
+            return max_diff - (mx - mn) + 1;
         }
     };
