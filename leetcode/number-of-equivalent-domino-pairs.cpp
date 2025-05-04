bee0e26868a7cd90dadae7b6b55cfeaade7e26ff
SGrade <sgrade@users.noreply.github.com>
2025-05-04 11:44:40 +0000

    leetcode

--- leetcode/number-of-equivalent-domino-pairs.cpp
+++ leetcode/number-of-equivalent-domino-pairs.cpp
@@ -1,37 +1,27 @@
 // 1128. Number of Equivalent Domino Pairs
 // https://leetcode.com/problems/number-of-equivalent-domino-pairs/
 
-#include <vector>
-#include <map>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 
 class Solution {
-public:
-    int numEquivDominoPairs(vector<vector<int>>& dominoes) {
-        for (int i = 0; i < dominoes.size(); ++i) {
-            if (dominoes[i][0] > dominoes[i][1]) {
-                swap(dominoes[i][0], dominoes[i][1]);
+    public:
+        int numEquivDominoPairs(vector<vector<int>>& dominoes) {
+            ios::sync_with_stdio(false);
+            cin.tie(nullptr);
+
+            unordered_map<int, int> counter;
+            for (vector<int>& v: dominoes) {
+                if (v[0] > v[1])
+                    swap(v[0], v[1]);
+                ++counter[v[0] * 1e5 + v[1]];
             }
+
+            int ans = 0;
+            for (const auto& [_, cnt]: counter)
+                ans += cnt * (cnt - 1) / 2;
+            return ans;
         }
-        
-        map<vector<int>, int> mp;
-        for (int i = 0; i < dominoes.size(); ++i) {
-            ++mp[dominoes[i]];
-        }
-        
-        long long ans = 0;
-        for (auto &p: mp) {
-            if (p.second > 1) {
-                int c = 0;
-                while(--p.second) {
-                    c += p.second;
-                }
-                ans += c;
-            }
-        }
-        
-        return ans;
-    }
-};
+    };
