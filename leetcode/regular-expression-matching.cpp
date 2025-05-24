5d877bba6da8acdd67eff6c3570be8250b2244f6
SGrade <sgrade@users.noreply.github.com>
2025-05-24 13:45:25 +0000

    leetcode

--- leetcode/regular-expression-matching.cpp
+++ leetcode/regular-expression-matching.cpp
@@ -6,32 +6,39 @@
 using namespace std;
 
 
-// Based on Editorial's Approach 1: Recursion
-// TLE
 class Solution {
 public:
     bool isMatch(string s, string p) {
         ios::sync_with_stdio(false);
         cin.tie(nullptr);
 
-        if (p.size() == 0)
-            return s.size() == 0;
-        bool first_match = (
-            s.size() > 0 &&
-            (p[0] == s[0] || p[0] == '.')
-        );
+        ss = s.size(), ps = p.size();
+        memo.resize(ss + 1, vector<int>(ps + 1, -1));
+        return dp(0, 0, s, p);
+    }
 
-        if (p.size() >= 2 && p[1] == '*') {
-            return (
-                isMatch(s, p.substr(2)) ||
-                (first_match && isMatch(s.substr(1), p))
-            );
-        }
+private:
+    int ss, ps;
+    vector<vector<int>> memo;
+    bool dp (int s_idx , int p_idx, string s, string p) {
+        if (memo[s_idx][p_idx] != -1)
+            return memo[s_idx][p_idx];
+
+        bool ans;
+        if (p_idx == ps)
+            ans = s_idx == ss;
         else {
-            return (
-                first_match &&
-                isMatch(s.substr(1), p.substr(1))
+            bool first_match = (
+                s_idx < ss &&
+                (p[p_idx] == s[s_idx] || p[p_idx] == '.')
             );
+            if (p_idx + 1 < ps && p[p_idx + 1] == '*')
+                ans = dp(s_idx, p_idx + 2, s, p) || (first_match && dp(s_idx + 1, p_idx, s, p));
+            else
+                ans = first_match && dp(s_idx + 1, p_idx + 1, s, p);
         }
+
+        memo[s_idx][p_idx] = ans;
+        return ans;
     }
 };
