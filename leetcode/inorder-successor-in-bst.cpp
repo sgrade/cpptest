a4dd7ecb439a93f3f4f68c5e2acfb57ebc270b66
SGrade <sgrade@users.noreply.github.com>
2025-07-08 14:06:59 +0000

    leetcode

--- leetcode/inorder-successor-in-bst.cpp
+++ leetcode/inorder-successor-in-bst.cpp
@@ -1,59 +1,36 @@
 // 285. Inorder Successor in BST
 // https://leetcode.com/problems/inorder-successor-in-bst/
-// Based on Leetcode's Approach 1: Without using BST properties
 
 #include <bits/stdc++.h>
 
 using namespace std;
 
 
+// Based on the Editorial's Approach 2: Using BST properties
+
 // Definition for a binary tree node.
- struct TreeNode {
-     int val;
-     TreeNode *left;
-     TreeNode *right;
-     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- };
- 
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
 class Solution {
 public:
     TreeNode* inorderSuccessor(TreeNode* root, TreeNode* p) {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
         
-        // Left child exists
-        if (p->right != nullptr) {
-            TreeNode* leftmost = p->right;
-            while (leftmost->left != nullptr) {
-                leftmost = leftmost->left;
+        TreeNode* ans = nullptr;
+        while (root != nullptr) {
+            if (p->val >= root->val)
+                root = root->right;
+            else {
+                ans = root;
+                root = root->left;
             }
-            ans = leftmost;
         }
-        
-        // Left child doesn't exist, so ans is one of the ancestors
-        else {
-            FindInorderAncestor(root, p);
-        }
-        
         return ans;
-    }
-
-private:
-    TreeNode* prev = nullptr;
-    TreeNode* ans = nullptr;
-    
-    void FindInorderAncestor(TreeNode* node, TreeNode* p) {
-        if (node == nullptr) {
-            return;
-        }
-        
-        FindInorderAncestor(node->left, p);
-        
-        if (prev == p && ans == nullptr) {
-            ans = node;
-            return;
-        }
-        
-        prev = node;
-        
-        FindInorderAncestor(node->right, p);
     }
 };
