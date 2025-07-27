f8113e0214c18909f69ee88e7e3d816bb587afe5
sgrade <sgrade@users.noreply.github.com>
2025-07-27 09:02:10 +0000

    leetcode

--- leetcode/count-hills-and-valleys-in-an-array.cpp
+++ leetcode/count-hills-and-valleys-in-an-array.cpp
@@ -10,36 +10,29 @@ class Solution {
 public:
     int countHillValley(vector<int>& nums) {
         int ans = 0, n = nums.size();
+        int prev = nums[0]; // Track the last different value
 
         for (int i = 1; i < n - 1; ++i) {
-            // hill
-            if (nums[i - 1] < nums[i]){
-                if (nums[i] < nums[i + 1])
-                    continue;
-                if (nums[i] > nums[i + 1])
-                    ++ans;
-                else {
-                    while (i < n - 1 && nums[i] == nums[i + 1])
-                        ++i;
-                    if (i < n - 1 && nums[i] > nums[i + 1])
-                        ++ans;
-                }
-            }
+            int next = nums[i + 1];
 
-            // valley
-            if (nums[i - 1] > nums[i]) {
-                if (nums[i] > nums[i + 1])
-                    continue;
-                if (nums[i] < nums[i + 1])
-                    ++ans;
-                else {
-                    while (i < n - 1 && nums[i] == nums[i + 1])
-                        ++i;
-                    if (i < n - 1 && nums[i] < nums[i + 1])
-                        ++ans;
-                }
+            // Skip if current equals next (find the actual next different value)
+            int j = i + 1;
+            while (j < n && nums[j] == nums[i])
+                ++j;
+            if (j == n) break; // No more different values
+            next = nums[j];
 
-            }
+            // Check if current position forms a hill or valley
+            if ((prev < nums[i] && nums[i] > next) ||
+                (prev > nums[i] && nums[i] < next))
+                    ++ans;
+
+            // Update prev only when we move to a different value
+            if (nums[i] != prev)
+                prev = nums[i];
+
+            // Skip duplicate values
+            i = j - 1; // -1 because loop will increment
         }
 
         return ans;
