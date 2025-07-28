ca6d35ffdffebbff7e040c28bd3f67cd615c5e0a
sgrade <sgrade@users.noreply.github.com>
2025-07-28 15:17:03 +0000

    leetcode

--- leetcode/convert-a-number-to-hexadecimal.cpp
+++ leetcode/convert-a-number-to-hexadecimal.cpp
@@ -6,22 +6,21 @@
 using namespace std;
 
 
-// Based on https://leetcode.com/problems/convert-a-number-to-hexadecimal/solutions/437513/c-beats-100-no-bit-manipulation-with-explanation
 class Solution {
 public:
     string toHex(int num) {
         if (num == 0) return "0";
-        
+
         unsigned int x = num;
         const char chars[] = "0123456789abcdef";
         string ans;
         ans.reserve(8); // Max 8 hex digits for 32-bit int
-        
+
         while (x) {
-            ans = chars[x & 15] + ans;  // Prepend to avoid reverse()
-            x >>= 4;
+            ans = chars[x & 15] + ans;  // Instead of x % 16
+            x >>= 4;    // Instead of x /= 16
         }
-        
+
         return ans;
     }
 };
