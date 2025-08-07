17561f7c258794d82338530d56231e9964031a2b
sgrade <sgrade@users.noreply.github.com>
2025-08-07 18:45:24 +0000

    leetcode

--- leetcode/find-the-maximum-number-of-fruits-collected.cpp
+++ leetcode/find-the-maximum-number-of-fruits-collected.cpp
@@ -12,13 +12,17 @@ public:
     int maxCollectedFruits(vector<vector<int>>& fruits) {
         int n = fruits.size();
         int ans = 0;
-        for (int i = 0; i < n; ++i) ans += fruits[i][i];
+        for (int i = 0; i < n; ++i) {
+            ans += fruits[i][i];
+        }
         ans += traverse(fruits, n);
 
         // Flip along main diagonal
-        for (int row = 0; row < n; ++row)
-            for (int col = 0; col < row; ++col)
+        for (int row = 0; row < n; ++row) {
+            for (int col = 0; col < row; ++col) {
                 swap(fruits[row][col], fruits[col][row]);
+            }
+        }
 
         ans += traverse(fruits, n);
         return ans;
@@ -33,10 +37,15 @@ private:
             int start_col = max(n - 1 - row, row + 1);
             for (int col = start_col; col < n; ++col) {
                 int prev_best = dp[col];
-                if (col - 1 >= 0) prev_best = max(prev_best, dp[col - 1]);
-                if (col + 1 < n) prev_best = max(prev_best, dp[col + 1]);
-                if (prev_best != numeric_limits<int>::min())
+                if (col - 1 >= 0) {
+                    prev_best = max(prev_best, dp[col - 1]);
+                }
+                if (col + 1 < n) {
+                    prev_best = max(prev_best, dp[col + 1]);
+                }
+                if (prev_best != numeric_limits<int>::min()) {
                     new_dp[col] = prev_best + fruits[row][col];
+                }
             }
             dp.swap(new_dp);
         }
