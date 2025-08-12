70375a73e83f5cf846878285d8a06bc632394e5c
sgrade <sgrade@users.noreply.github.com>
2025-08-12 08:03:42 +0000

    leetcode

--- leetcode/ways-to-express-an-integer-as-sum-of-powers.cpp
+++ leetcode/ways-to-express-an-integer-as-sum-of-powers.cpp
@@ -5,22 +5,22 @@
 #include <vector>
 #include <cmath>
 
-// Based on Editorial's Approach: Dynamic Programming
+// Based on Editorial's Approach: Dynamic Programming; Space Optimization
 class Solution {
 public:
     int numberOfWays(int n, int x) {
         constexpr int kMod = 1e9 + 7;
-        std::vector<std::vector<int64_t>> dp(n + 1, std::vector<int64_t>(n + 1));
-        dp[0][0] = 1;
+        std::vector<int64_t> dp(n + 1);
+        dp[0] = 1;
         for (int i = 1; i <= n; ++i) {
             int64_t cur = pow(i, x);
-            for (int j = 0; j <= n; ++j) {
-                dp[i][j] = dp[i - 1][j];
-                if (j >= cur) {
-                    dp[i][j] = (dp[i][j] + dp[i - 1][j - cur]) % kMod;
-                }
+            if (cur > n)
+                break;
+            for (int j = n; j >= cur; --j) {
+                dp[j] = (dp[j] + dp[j - cur]) % kMod;
             }
         }
-        return dp[n][n];
+        return dp[n];
     }
 };
+
