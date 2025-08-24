6699bd36139dc1ee47ddc71937f1921674c5c285
SGrade <sgrade@users.noreply.github.com>
2025-08-24 16:23:16 +0000

    leetcode

--- leetcode/before-and-after-puzzle.cpp
+++ leetcode/before-and-after-puzzle.cpp
@@ -1,41 +1,45 @@
 // 1181. Before and After Puzzle
 // https://leetcode.com/problems/before-and-after-puzzle/
 
-#include <vector>
-#include <string>
-#include <unordered_set>
-#include <algorithm>
 
-using namespace std;
+#include <algorithm>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
 
 
 // Based on Editorial's Approach: Hash Table + Sorting
 class Solution {
-public:
-    vector<string> beforeAndAfterPuzzles(vector<string>& phrases) {
-        vector<pair<string, string>> words;
-        for (const string& phrase: phrases) {
-            size_t pos = phrase.find(' ');
-            string first = phrase.substr(0, pos);
-            pos = phrase.find_last_of(' ');
-            string last = pos == string::npos? phrase : phrase.substr(pos + 1);
-            words.emplace_back(first, last);
+ public:
+    std::vector<std::string> beforeAndAfterPuzzles(std::vector<std::string>& phrases) {
+        int n = phrases.size();
+        std::vector<std::string> first_words(n), last_words(n);
+        std::unordered_multimap<std::string, int> last_to_idx;
+        for (int i = 0; i < n; ++i) {
+            const std::string& phrase = phrases[i];
+            size_t pos_first = phrase.find(' ');
+            first_words[i] = phrase.substr(0, pos_first);
+            size_t pos_last = phrase.find_last_of(' ');
+            last_words[i] = (pos_last == std::string::npos) ? phrase : phrase.substr(pos_last + 1);
+            last_to_idx.emplace(last_words[i], i);
         }
-        
-        unordered_set<string> output;
-        for (int i = 0; i < phrases.size(); ++i) {
-            for (int j = 0; j < phrases.size(); ++j) {
+
+        std::unordered_set<std::string> output;
+        for (int i = 0; i < n; ++i) {
+            auto range = last_to_idx.equal_range(first_words[i]);
+            for (auto it = range.first; it != range.second; ++it) {
+                int j = it->second;
                 if (i == j) continue;
-                if (words[i].first == words[j].second) {
-                    size_t pos = words[i].first.size();
-                    string current = phrases[j] + phrases[i].substr(pos);
-                    output.emplace(current);
-                }
+                size_t pos = first_words[i].size();
+                std::string current = phrases[j] + phrases[i].substr(pos);
+                output.emplace(current);
             }
         }
 
-        vector<string> ans(output.begin(), output.end());
-        sort(ans.begin(), ans.end());
+        std::vector<std::string> ans(output.begin(), output.end());
+        std::sort(ans.begin(), ans.end());
         return ans;
     }
 };
