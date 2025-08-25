eebb6c55c0d7ca843da58e11bad88ff28359925b
SGrade <sgrade@users.noreply.github.com>
2025-08-25 16:03:08 +0000

    leetcode

--- leetcode/diagonal-traverse.cpp
+++ leetcode/diagonal-traverse.cpp
@@ -1,3 +1,4 @@
+
 // 498. Diagonal Traverse
 // https://leetcode.com/problems/diagonal-traverse/
 
@@ -5,62 +6,27 @@
 #include <algorithm>
 
 using std::vector;
-using std::min;
-using std::max;
-
 
 class Solution {
 public:
     vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
+        int rows = mat.size(), cols = mat[0].size();
         vector<int> ans;
-
-        int rows = mat.size(), cols = mat[0].size(), total_elements = rows * cols, diag = 1;
-        
-        bool up_and_right = true;
-        int r = 0, c = 0;
-        int row_modifier, col_modifier;
-
-        while (total_elements) {
-
-            int diag_elements;
-            if (diag <= min(rows, cols))
-                diag_elements = diag;
-            else if (diag > min(rows, cols) && diag <= max(rows, cols))
-                diag_elements = min(rows, cols);
-            else
-                diag_elements = (max(rows, cols) + min(rows, cols)) - diag;
-            
-            int elements_placed = 0;
-            while (true) {
-                ans.emplace_back(mat[r][c]);
-                ++elements_placed;
-                if (elements_placed < diag_elements) {
-                    r += row_modifier;
-                    c += col_modifier;
-                }
-                else {
-                    if (up_and_right) {
-                        up_and_right = false;
-                        if (c == cols - 1) ++r;
-                        ++c;
-                        c = min(c, cols - 1);
-                        row_modifier = 1, col_modifier = -1;
-                    }
-                    else {
-                        up_and_right = true;
-                        if (r == rows - 1) ++c;
-                        ++r;
-                        r = min(r, rows - 1);
-                        row_modifier = -1, col_modifier = 1;
-                    }
-                    total_elements -= elements_placed;
-                    break;
-                }
+        for (int d = 0; d < rows + cols - 1; ++d) {
+            vector<int> diagonal;
+            int r = d < cols ? 0 : d - cols + 1;
+            int c = d < cols ? d : cols - 1;
+            while (r < rows && c >= 0) {
+                diagonal.emplace_back(mat[r][c]);
+                ++r;
+                --c;
             }
-
-            ++diag;
+            if (d % 2 == 0) {
+                std::reverse(diagonal.begin(), diagonal.end());
+            }
+            ans.insert(ans.end(), diagonal.begin(), diagonal.end());
         }
-
         return ans;
     }
 };
+
