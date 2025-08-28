cbb8c007f0157d4d61b2ad831711bb7689dd6de5
SGrade <sgrade@users.noreply.github.com>
2025-08-28 14:41:56 +0000

    leetcode

--- leetcode/sort-matrix-by-diagonals.cpp
+++ leetcode/sort-matrix-by-diagonals.cpp
@@ -15,23 +15,17 @@ public:
     vector<vector<int>> sortMatrix(vector<vector<int>>& grid) {
         int rows = grid.size(), cols = grid[0].size();
         for (int d = 1; d < rows + cols; ++d) {
-            vector<int> d_nums;
             int row = d < rows ? rows - d : 0;
             int col = d < rows ? 0 : d - rows;
-            int r = row, c = col;
-            while (r < rows && c < cols) {
-                d_nums.emplace_back(grid[r++][c++]);
+            int n = std::min(rows - row, cols - col);
+            vector<int> d_nums(n);
+            for (int i = 0; i < n; ++i) {
+                d_nums[i] = (grid[row + i][col + i]);
             }
-            if (d <= rows) {
-                sort(d_nums.begin(), d_nums.end(), greater<int>());
-            }
-            else {
-                sort(d_nums.begin(), d_nums.end());
-            }
-            r = row, c = col;
-            int i = 0;
-            while (i < d_nums.size()) {
-                grid[r++][c++] = d_nums[i++];
+            auto cmp = (d <= rows) ? [](int a, int b) { return a > b; } : [](int a, int b) { return a < b; };
+            sort(d_nums.begin(), d_nums.end(), cmp);
+            for (int i = 0; i < n; ++i) {
+                grid[row + i][col + i] = d_nums[i];
             }
         }
         return grid;
