6eae24c1cb0c669eeb57fee7ce64ceba8de83918
SGrade <sgrade@users.noreply.github.com>
2025-08-29 10:03:35 +0000

    leetcode

---
+++ leetcode/alice-and-bob-playing-flower-game.cpp
@@ -0,0 +1,9 @@
+// 3021. Alice and Bob Playing Flower Game
+// https://leetcode.com/problems/alice-and-bob-playing-flower-game/
+
+class Solution {
+public:
+    long long flowerGame(int n, int m) {
+        return (long long) m * n / 2;
+    }
+};
