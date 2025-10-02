f61d8861b0c7c317c44d8f0953f21d7b4f953e23
SGrade <sgrade@users.noreply.github.com>
2025-10-02 17:44:46 +0000

    leetcode

---
+++ leetcode/water-bottles-ii.cpp
@@ -0,0 +1,15 @@
+// 3100. Water Bottles II
+// https://leetcode.com/problems/water-bottles-ii/
+
+
+class Solution {
+public:
+    int maxBottlesDrunk(int numBottles, int numExchange) {
+        int ans = numBottles;
+        for (int empty = numBottles; empty >= numExchange; ++numExchange) {
+            ++ans;
+            empty -= numExchange - 1;
+        }
+        return ans;
+    }
+};
