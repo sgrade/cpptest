54fb9ab95e6fb61288b7bd19d177ff94fc4cf921
SGrade <sgrade@users.noreply.github.com>
2025-10-07 10:23:24 +0000

    leetcode

--- leetcode/avoid-flood-in-the-city.cpp
+++ leetcode/avoid-flood-in-the-city.cpp
@@ -3,44 +3,61 @@
 
 #include <vector>
 #include <unordered_map>
+#include <set>
 
 using std::vector;
 using std::unordered_map;
+using std::set;
 
 
-// Wrong answer
 class Solution {
 public:
     vector<int> avoidFlood(vector<int>& rains) {
         int n = rains.size();
-        unordered_map<int, int> lakes;
-        bool flood = false;
+
+        set<int> dry_days;
+        for (int i = 0; i < n; ++i) {
+            if (rains[i] == 0) {
+                dry_days.emplace(i);
+            }
+        }
+
+        vector<int> ans(n, 1);
+        unordered_map<int, int> full_lakes;
         for (int i = 0; i < n; ++i) {
             int lake = rains[i];
+            
             if (lake == 0) {
-                if (lakes.empty()) {
-                    rains[i] = 1;
-                    continue;
-                }
-                auto [lake_to_dry, idx] = *lakes.begin();
-                rains[i] = lake_to_dry;
-                rains[idx] = -1;
-                lakes.erase(lakes.begin());
+                continue;
             }
-            if (lakes.count(lake)) {
-                flood = true;
-                break;
+
+            // lake != 0
+            if (full_lakes.find(lake) == full_lakes.end()) {
+                full_lakes[lake] = i;
+                ans[i] = -1;
+                continue;
+            } 
+            
+            // else - need to dry the lake
+            if (dry_days.empty()) {
+                return {};
             }
-            if (lake != 0) {
-                lakes[lake] = i;
+            int day_when_lake_filled = full_lakes[lake];
+            auto dry_day_it = dry_days.lower_bound(day_when_lake_filled);
+            if (dry_day_it == dry_days.end()) {
+                return {};
             }
+            int dry_day = *dry_day_it;
+            if (dry_day > i) {
+                return {};
+            }
+            ans[dry_day] = lake;
+            full_lakes[lake] = i;
+            ans[i] = -1;
+            dry_days.erase(dry_day_it);
+            ans[i] = -1;
         }
-        if (flood) {
-            return {};
-        }
-        for (const auto& [full_lake, idx]: lakes) {
-            rains[idx] = -1;
-        }
-        return rains;
+
+        return ans;
     }
 };
