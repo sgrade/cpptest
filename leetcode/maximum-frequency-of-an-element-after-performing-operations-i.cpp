c4efdd576024aebc61c34f8bf5ee98b130bf87e5
SGrade <sgrade@users.noreply.github.com>
2025-10-21 18:04:10 +0000

    leetcode

---
+++ leetcode/maximum-frequency-of-an-element-after-performing-operations-i.cpp
@@ -0,0 +1,35 @@
+// 3346. Maximum Frequency of an Element After Performing Operations I
+// https://leetcode.com/problems/maximum-frequency-of-an-element-after-performing-operations-i/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// Based on Editorial's Approach: Sort + Enumerate + Binary Search
+class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k, int numOperations) {
+        unordered_map<int, int> counter;
+        for (const int num: nums) {
+            ++counter[num];
+        }
+
+        int ans = 0;
+        sort(nums.begin(), nums.end());
+        for (int num = nums[0]; num <= nums[nums.size() - 1]; ++num) {
+            int left = num - k, right = num + k;
+            vector<int>::iterator left_it = lower_bound(nums.begin(), nums.end(), left);
+            vector<int>::iterator right_it = upper_bound(nums.begin(), nums.end(), right);
+            int can_change = distance(left_it, right_it);
+            int cur_ans = 0;
+            if (counter.count(num)) {
+                cur_ans = min(can_change, counter[num] + numOperations);
+            } else {
+                cur_ans = min(can_change, numOperations);
+            }
+            ans = max(ans, cur_ans);
+        }
+        return ans;
+    }
+};
