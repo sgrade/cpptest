4f12c0a79f291c524c34094181d9ed1949de21fe
SGrade <sgrade@users.noreply.github.com>
2025-10-25 12:21:20 +0000

    leetcode

--- leetcode/calculate-money-in-leetcode-bank.cpp
+++ leetcode/calculate-money-in-leetcode-bank.cpp
@@ -1,16 +1,19 @@
 // 1716. Calculate Money in Leetcode Bank
 // https://leetcode.com/problems/calculate-money-in-leetcode-bank/
 
-#include <bits/stdc++.h>
-
-using namespace std;
-
 
 class Solution {
 public:
     int totalMoney(int n) {
-        int weeks = n / 7;
-        int rem = n % 7;
-        return (56 + 7 * (weeks - 1)) * weeks / 2 + (1 + weeks + weeks + rem) * rem / 2;
+        int weeks = n / 7, first_week = 28, diff_each_week = 7;
+        // Ariphmetic sum
+        int full_weeks = weeks * (2 * first_week + (weeks - 1) * diff_each_week) / 2;
+
+        int last_monday = 1 + weeks, last_week = 0;
+        for (int day = 0; day < n % 7; ++day) {
+            last_week += last_monday + day;
+        }
+
+        return full_weeks + last_week;
     }
 };
