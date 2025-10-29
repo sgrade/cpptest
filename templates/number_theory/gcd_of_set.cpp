06dea6b435cf63b0b510edf5ea77b330db4d8d11
SGrade <sgrade@users.noreply.github.com>
2025-10-29 16:32:50 +0000

    templates

---
+++ templates/number_theory/gcd_of_set.cpp
@@ -0,0 +1,19 @@
+/*
+The key mathematical property is the associativity of GCD:
+gcd(a, b, c, d, ...) = gcd(gcd(gcd(a, b), c), d), ...)
+This means you can compute the GCD of multiple numbers by repeatedly applying the two-argument GCD function, 
+and the order doesn't matter!
+*/
+
+long long GcdOfSet(const unordered_set<long long>& st) {
+    auto it = st.begin();
+    long long gcd = *it;
+    ++it;
+    for (; it != st.end(); ++it) {
+        gcd = gcd == 1 ? 1 : std::gcd(gcd, *it);
+        if (gcd == 1) {
+            break;
+        }
+    }
+    return gcd;
+}
