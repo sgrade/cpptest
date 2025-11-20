7819d9206de43893d1ac8c0df19a32c0120b8a6d
SGrade <sgrade@users.noreply.github.com>
2025-11-20 15:34:15 +0000

    leetcode

--- leetcode/set-intersection-size-at-least-two.cpp
+++ leetcode/set-intersection-size-at-least-two.cpp
@@ -10,13 +10,8 @@ class Solution {
  public:
   int intersectionSizeTwo(vector<vector<int>>& intervals) {
     sort(intervals.begin(), intervals.end(),
-         [](vector<int>& v1, vector<int>& v2) {
-           if (v1[0] < v2[0]) {
-             return true;
-           } else if (v1[0] == v2[0]) {
-             return v1[1] > v2[1];
-           }
-           return false;
+         [](const vector<int>& v1, const vector<int>& v2) {
+           return v1[0] != v2[0] ? v1[0] < v2[0] : v1[1] > v2[1];
          });
 
     vector<int> to_pick_from_interval(intervals.size(), 2);
