c37b380474e2d00e7091e28be8c4fe182ffdefe0
SGrade <sgrade@users.noreply.github.com>
2025-11-20 14:11:48 +0000

    leetcode

--- leetcode/keep-multiplying-found-values-by-two.cpp
+++ leetcode/keep-multiplying-found-values-by-two.cpp
@@ -1,19 +1,40 @@
 // 2154. Keep Multiplying Found Values by Two
 // https://leetcode.com/problems/keep-multiplying-found-values-by-two/
 
-#include <unordered_set>
+#include <algorithm>
 #include <vector>
 
-using std::unordered_set;
 using std::vector;
 
 class Solution {
  public:
-  int findFinalValue(vector<int>& nums, int original) {
-    unordered_set<int> st(nums.begin(), nums.end());
-    while (st.count(original)) {
-      original *= 2;
+  int intersectionSizeTwo(vector<vector<int>>& intervals) {
+    sort(intervals.begin(), intervals.end(),
+         [](vector<int>& v1, vector<int>& v2) {
+           if (v1[0] < v2[0]) {
+             return true;
+           } else if (v1[0] == v2[0]) {
+             return v1[1] > v2[1];
+           }
+           return false;
+         });
+
+    vector<int> to_pick_from_interval(intervals.size(), 2);
+    int min_len = 0;
+
+    for (int i = intervals.size() - 1; i >= 0; --i) {
+      int cur_start = intervals[i][0], to_pick = to_pick_from_interval[i];
+
+      for (int point = cur_start; point < cur_start + to_pick; ++point) {
+        for (int j = 0; j <= i; ++j) {
+          int cur_end = intervals[j][1];
+          if (to_pick_from_interval[j] && point <= cur_end) {
+            to_pick_from_interval[j]--;
+          }
+        }
+        min_len++;
+      }
     }
-    return original;
+    return min_len;
   }
 };
