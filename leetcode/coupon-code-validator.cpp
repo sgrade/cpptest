7071ff9359640538064ec342dbe91f4be18643bc
SGrade <sgrade@users.noreply.github.com>
2025-12-13 14:16:42 +0000

    leetcode

--- leetcode/coupon-code-validator.cpp
+++ leetcode/coupon-code-validator.cpp
@@ -1,50 +1,51 @@
 // 3606. Coupon Code Validator
 // https://leetcode.com/problems/coupon-code-validator/
 
-#include <bits/stdc++.h>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
 
-using namespace std;
-
+using std::map;
+using std::set;
+using std::string;
+using std::vector;
 
 class Solution {
-public:
-    vector<string> validateCoupons(vector<string>& code, vector<string>& businessLine, vector<bool>& isActive) {
-        ios::sync_with_stdio(false);
-        cin.tie(nullptr);
-
-        int n = code.size();
-        for (int i = 0; i < n; ++i) {
-            if (!isActive[i])
-                continue;
-            if (code[i].empty())
-                continue;
-            string category = businessLine[i];
-            if (find(categories.begin(), categories.end(), category) == categories.end())
-                continue;
-            bool is_valid = true;
-            for (const char& ch: code[i]) {
-                if (isalnum(ch) || ch == '_')
-                    continue;
-                else {
-                    is_valid = false;
-                    break;
-                }
-            }
-            if (is_valid)
-                coupons[category].emplace_back(code[i]);
+ public:
+  vector<string> validateCoupons(vector<string>& code,
+                                 vector<string>& businessLine,
+                                 vector<bool>& isActive) {
+    for (size_t i = 0; i < code.size(); ++i) {
+      if (!isActive[i]) continue;
+      if (code[i].empty()) continue;
+      string category = businessLine[i];
+      if (find(categories.begin(), categories.end(), category) ==
+          categories.end())
+        continue;
+      bool is_valid = true;
+      for (const char& ch : code[i]) {
+        if (isalnum(ch) || ch == '_')
+          continue;
+        else {
+          is_valid = false;
+          break;
         }
-
-        vector<string> ans;
-        for (auto& [category, coups]: coupons) {
-            if (coups.empty())
-                continue;
-            sort(coups.begin(), coups.end());
-            ans.insert(ans.end(), coups.begin(), coups.end());
-        }
-        return ans;
+      }
+      if (is_valid) coupons[category].emplace_back(code[i]);
     }
 
-private:
-    map<string, vector<string>> coupons;
-    set<string> categories = {"electronics", "grocery", "pharmacy", "restaurant"};
+    vector<string> ans;
+    for (auto& [category, coups] : coupons) {
+      if (coups.empty()) continue;
+      sort(coups.begin(), coups.end());
+      ans.insert(ans.end(), coups.begin(), coups.end());
+    }
+    return ans;
+  }
+
+ private:
+  map<string, vector<string>> coupons;
+  set<string> categories = {"electronics", "grocery", "pharmacy", "restaurant"};
 };
