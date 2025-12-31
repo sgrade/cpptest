4d81e5cb7164058675927fc24821e37f761d21e5
sgrade <sgrade@users.noreply.github.com>
2025-12-31 18:06:23 +0000

    dsu implementation is aligned with C++ style guide

--- templates/graph_pheory/union_find.cpp
+++ templates/graph_pheory/union_find.cpp
@@ -2,33 +2,31 @@
 
 using namespace std;
 
-
 class UnionFind {
-public:
-    UnionFind (int size): parent(size), rank(size) {
-        iota(parent.begin(), parent.end(), 0);
+ public:
+  explicit UnionFind(int size) : parent_(size), rank_(size) {
+    iota(parent_.begin(), parent_.end(), 0);
+  }
+  int Find(int x) {
+    if (x != parent_[x]) parent_[x] = Find(parent_[x]);
+    return parent_[x];
+  }
+  void UnionSet(int x, int y) {
+    int root_x = Find(x);
+    int root_y = Find(y);
+    if (root_x == root_y) return;
+    if (rank_[root_x] < rank_[root_y]) {
+      parent_[root_x] = root_y;
+    } else if (rank_[root_y] < rank_[root_x]) {
+      parent_[root_y] = root_x;
+    } else {
+      parent_[root_y] = root_x;
+      ++rank_[root_x];
     }
-    int Find (int x) {
-        if (x != parent[x])
-            parent[x] = Find(parent[x]);
-        return parent[x];
-    }
-    void UnionSet (int x, int y) {
-        int root_x = Find(x); 
-        int root_y = Find(y);
-        if (root_x == root_y)
-            return;
-        if (rank[root_x] < rank[root_y])
-            parent[root_x] = root_y;
-        else if (rank[root_y] < rank[root_x])
-            parent[root_y] = root_x;
-        else {
-            parent[root_y] = root_x;
-            ++rank[root_x];
-        }
-    }
-    bool Connected (int x, int y) {
-        return Find(x) == Find(y);
-    }
-    vector<int> parent, rank;
+  }
+  bool Connected(int x, int y) { return Find(x) == Find(y); }
+
+ private:
+  vector<int> parent_;
+  vector<int> rank_;
 };
