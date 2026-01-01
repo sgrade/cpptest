16cf8034d5e2720387f58a9b2b2e14a7fddf4cad
sgrade <sgrade@users.noreply.github.com>
2026-01-01 16:23:39 +0000

    leetcode

--- leetcode/plus-one.cpp
+++ leetcode/plus-one.cpp
@@ -1,26 +1,22 @@
 // 66. Plus One
 // https://leetcode.com/problems/plus-one/
 
-#include <bits/stdc++.h>
+#include <vector>
 
 using namespace std;
 
-
 class Solution {
-public:
-    vector<int> plusOne(vector<int>& digits) {
-        int carry = 1;
-        // Note: if carry == 0, the rest of the array won't change
-        // So we stop iterating
-        // Idea is from one of the leetcode's submissions
-        for (int i = digits.size() - 1; i > -1 && carry > 0; --i) {
-            carry += digits[i];
-            digits[i] = carry % 10;
-            carry /= 10;
-        }
-        if (carry > 0) {
-            digits.insert(digits.begin(), carry);
-        }
+ public:
+  vector<int> plusOne(vector<int>& digits) {
+    for (int i = digits.size() - 1; i >= 0; i--) {
+      digits[i]++;
+      if (digits[i] == 10) {
+        digits[i] = 0;
+      } else {
         return digits;
+      }
     }
+    digits.insert(digits.begin(), 1);
+    return digits;
+  }
 };
