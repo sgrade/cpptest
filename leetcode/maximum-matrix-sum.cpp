c05df6e69cf110f1f78e7f5733df8c6262d5b563
sgrade <sgrade@users.noreply.github.com>
2026-01-05 08:42:02 +0000

    leetcode

--- leetcode/maximum-matrix-sum.cpp
+++ leetcode/maximum-matrix-sum.cpp
@@ -1,30 +1,27 @@
 // 1975. Maximum Matrix Sum
 // https://leetcode.com/problems/maximum-matrix-sum/
 
-#include <bits/stdc++.h>
+#include <algorithm>
+#include <climits>
+#include <vector>
 
 using namespace std;
 
-
-// Based on Editorial's Approach: Journey From Minus to Plus
 class Solution {
-public:
-    long long maxMatrixSum(vector<vector<int>>& matrix) {
-        ios::sync_with_stdio(false);
-        cin.tie(nullptr);
-
-        long long sum = 0;
-        int mn = INT_MAX, negatives = 0;
-        for (const auto& row: matrix) {
-            for (const int& val: row) {
-                sum += abs(val);
-                if (val < 0)
-                    ++negatives;
-                mn = min(mn, abs(val));
-            }
-        }
-        if (negatives % 2 != 0)
-            sum -= 2 * mn;
-        return sum;
+ public:
+  long long maxMatrixSum(vector<vector<int>>& matrix) {
+    long long total = 0;
+    int min_abs = INT_MAX;
+    int negatives = 0;
+    for (const vector<int>& row : matrix) {
+      for (const int num : row) {
+        int cur_abs = abs(num);
+        total += cur_abs;
+        min_abs = min(min_abs, cur_abs);
+        if (num < 0) ++negatives;
+      }
     }
+    if (negatives % 2 != 0) total -= 2 * min_abs;
+    return total;
+  }
 };
