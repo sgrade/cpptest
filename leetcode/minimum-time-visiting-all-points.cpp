28b7ea63fd56c29aedae90c2d98ef68ad68db003
sgrade <sgrade@users.noreply.github.com>
2026-01-12 10:29:44 +0000

    leetcode

--- leetcode/minimum-time-visiting-all-points.cpp
+++ leetcode/minimum-time-visiting-all-points.cpp
@@ -1,21 +1,20 @@
 // 1266. Minimum Time Visiting All Points
 // https://leetcode.com/problems/minimum-time-visiting-all-points/
 
-#include <bits/stdc++.h>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 
-
 class Solution {
-public:
-    int minTimeToVisitAllPoints(vector<vector<int>>& points) {
-        int ans = 0;
-        for (int i = 1; i < points.size(); ++i) {
-            int d0 = abs(points[i][0] - points[i - 1][0]);
-            int d1 = abs(points[i][1] - points[i - 1][1]);
-            if (d0 < d1) swap(d0, d1);
-            ans += d0;
-        }
-        return ans;
+ public:
+  int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+    int ans = 0;
+    for (size_t i = 1; i < points.size(); ++i) {
+      int d0 = abs(points[i][0] - points[i - 1][0]);
+      int d1 = abs(points[i][1] - points[i - 1][1]);
+      ans += max(d0, d1);
     }
+    return ans;
+  }
 };
