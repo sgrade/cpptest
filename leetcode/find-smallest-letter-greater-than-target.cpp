7478a22d5dfa183efb22843e66f9c7ea24aea376
sgrade <sgrade@users.noreply.github.com>
2026-01-31 15:34:44 +0000

    leetcode

--- leetcode/find-smallest-letter-greater-than-target.cpp
+++ leetcode/find-smallest-letter-greater-than-target.cpp
@@ -5,22 +5,10 @@
 
 using namespace std;
 
-
 class Solution {
-public:
-    char nextGreatestLetter(vector<char>& letters, char target) {
-        int left = 0, right = letters.size() - 1, mid;
-        while (left < right) {
-            mid = left + (right - left) / 2;
-            if (letters[mid] <= target) {
-                left = mid + 1;
-            }
-            else {
-                right = mid;
-            }
-        }
-        if (left == letters.size() - 1 && letters.back() <= target)
-            return letters.front();
-        return letters[left];
-    }
+ public:
+  char nextGreatestLetter(vector<char>& letters, char target) {
+    auto it = upper_bound(letters.begin(), letters.end(), target);
+    return it == letters.end() ? letters.front() : *it;
+  }
 };
