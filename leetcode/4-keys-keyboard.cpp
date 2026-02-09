f3ccdbc20c3bbbb8f2d17855e6dbef87d2218134
sgrade <sgrade@users.noreply.github.com>
2026-02-09 09:55:48 +0000

    leetcode

---
+++ leetcode/4-keys-keyboard.cpp
@@ -0,0 +1,22 @@
+// 651. 4 Keys Keyboard
+// https://leetcode.com/problems/4-keys-keyboard/
+
+#include <numeric>
+#include <vector>
+
+using namespace std;
+
+// Based on Editorial's A Dynamic Programming Approach
+class Solution {
+ public:
+  int maxA(int n) {
+    vector<int> dp(n + 1);
+    iota(dp.begin(), dp.end(), 0);
+    for (int i = 0; i <= n - 3; ++i) {
+      for (int j = i + 3; j <= min(n, i + 6); ++j) {
+        dp[j] = max(dp[j], (j - i - 1) * dp[i]);
+      }
+    }
+    return dp[n];
+  }
+};
