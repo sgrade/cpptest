3cc74eb6af806ed2f3894991fa6fe7bd9525bff5
sgrade <sgrade@users.noreply.github.com>
2026-07-28 10:17:34 +0000

    leetcode

--- leetcode/smallest-palindromic-rearrangement-i.cpp
+++ leetcode/smallest-palindromic-rearrangement-i.cpp
@@ -2,7 +2,6 @@
 // https://leetcode.com/problems/smallest-palindromic-rearrangement-i/
 
 #include <string>
-#include <vector>
 
 using namespace std;
 
@@ -10,29 +9,28 @@ class Solution {
  public:
   string smallestPalindrome(string s) {
     int n = s.size();
-    vector<int> counter(26);
-    for (int i = 0; i < n; ++i) {
-      int idx = s[i] - 'a';
-      counter[idx]++;
+    int cnt[26] = {};
+    for (char c : s) {
+      ++cnt[c - 'a'];
     }
 
+    string ans(n, ' ');
+    int left = 0;
     int mid = -1;
-    string ans = "";
-    for (int i = 0; i < 26; i++) {
-      if (counter[i] % 2 != 0) {
+    for (int i = 0; i < 26; ++i) {
+      if (cnt[i] & 1) {
         mid = i;
       }
-      int cnt = counter[i] / 2;
+      int half = cnt[i] / 2;
       char ch = 'a' + i;
-      for (int j = 0; j < cnt; ++j) {
-        ans += ch;
+      for (int j = 0; j < half; ++j) {
+        ans[left] = ch;
+        ans[n - 1 - left] = ch;
+        ++left;
       }
     }
     if (mid != -1) {
-      ans += 'a' + mid;
-    }
-    for (int j = n / 2 - 1; j >= 0; j--) {
-      ans += ans[j];
+      ans[n / 2] = 'a' + mid;
     }
     return ans;
   }
